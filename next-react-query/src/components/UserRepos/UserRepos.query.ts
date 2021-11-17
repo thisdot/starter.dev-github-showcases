74d3bf8ddc8aefe542c376953b4dfe193f0a886b
Dane Grant <danecando@gmail.com>
2021-11-17 15:08:14 -0500

    Adds sort order by updated at to repo list

--- next-react-query/src/components/UserRepos/UserRepos.query.ts
+++ next-react-query/src/components/UserRepos/UserRepos.query.ts
@@ -1,9 +1,9 @@
 import { gql } from 'graphql-request';
 
 export const USER_REPOS_QUERY = gql`
-  query UserRepos($username: String!) {
+  query UserRepos($username: String!, $orderBy: RepositoryOrder) {
     user(login: $username) {
-      repositories(first: 25) {
+      repositories(first: 20, orderBy: $orderBy) {
         nodes {
           id
           name
