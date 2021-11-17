2872a4283e0c8905b3ddfb239ed24808a2ca93f2
Dane Grant <danecando@gmail.com>
2021-11-17 09:06:10 -0500

    Removes overview tab

--- next-react-query/src/components/ProfileNav/tabList.tsx
+++ next-react-query/src/components/ProfileNav/tabList.tsx
@@ -1,12 +1,7 @@
 import type { TabItem } from '../TabNavigation';
-import { BookOpenIcon } from '@heroicons/react/outline';
 import { GitRepoIcon } from '@components/Icons';
 
 export const tabList: TabItem[] = [
-  {
-    title: 'Overview',
-    Icon: BookOpenIcon,
-  },
   {
     title: 'Repositories',
     path: '',
