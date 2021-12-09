0831092e56435e92ef30987826d6afc7f7a4b259
morgnism <morgan@border.dev>
2021-12-09 10:24:16 -0700

    fix: code review and apollo version

--- angular-apollo-tailwind/src/app/home/home.component.ts
+++ angular-apollo-tailwind/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CurrentUser, CurrentUserData, CURRENT_USER_QUERY } from '../gql';
 
 @Component({
@@ -8,14 +8,10 @@ import { CurrentUser, CurrentUserData, CURRENT_USER_QUERY } from '../gql';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  user$: Observable<CurrentUser | null> = of(null);
+export class HomeComponent {
+  user$: Observable<CurrentUser> = this.apollo
+    .watchQuery<CurrentUserData>({ query: CURRENT_USER_QUERY })
+    .valueChanges.pipe(map((res) => res.data.viewer));
 
   constructor(private apollo: Apollo) {}
-
-  ngOnInit() {
-    this.user$ = this.apollo
-      .watchQuery<CurrentUserData>({ query: CURRENT_USER_QUERY })
-      .valueChanges.pipe(map((res) => res.data.viewer));
-  }
 }
