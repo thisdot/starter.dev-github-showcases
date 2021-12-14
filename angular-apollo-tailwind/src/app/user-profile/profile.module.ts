0e5db873cf2013465e8688bd26f4412a63b4cb51
WillHutt <whutt1994@gmail.com>
2021-12-14 09:32:59 -0800

    feat: adds profile header tabs

---
+++ angular-apollo-tailwind/src/app/user-profile/profile.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { IconsModule } from '../components/icons/icons.module';
+
+@NgModule({
+	declarations: [],
+	imports: [CommonModule, RouterModule, IconsModule],
+})
+export class ReposModule {}
