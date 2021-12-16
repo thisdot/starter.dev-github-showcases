023cb1a2d86a00b35f02f529828ebca4afabfb76
Morgan Worrell <morgan@thisdot.co>
2021-12-16 09:16:42 -0700

    feat: added accessible label colors (#55)
    * feat: added accessible label colors

--- angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
+++ angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
@@ -11,7 +11,18 @@ export class PullRequestsListComponent {
 
   readonly baseDate = new Date();
 
-  getColor(color: string) {
-    return { 'background-color': `#${color}` };
+  colorMap(color: string): string {
+    switch (color) {
+      case 'D4C5F9': // violet
+        return '#ddd6fe';
+      case '310D85': // dark purple
+        return '#c084fc';
+      case '6ED842': // bright yellow green
+        return '#bef264';
+      case 'ED1DB5': // hot pink
+        return '#f9a8d4';
+      default:
+        return `#${color}`;
+    }
   }
 }
