0c399ebaa90fd90af3384e3e37c1d5bbd3514f56
morgnism <morgan@border.dev>
2021-12-15 22:14:17 -0700

    feat: added accessible label colors

--- angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
+++ angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
@@ -14,4 +14,19 @@ export class PullRequestsListComponent {
   getColor(color: string) {
     return { 'background-color': `#${color}` };
   }
+
+  colorMap(color: string): string {
+    switch (color) {
+      case 'D4C5F9': // violet
+        return '#ddd6fe';
+      case '310D85': // dark purple
+        return '#c084fc';
+      case '6ED842': // bright yellow green
+        return '#bef264';
+      case 'ED1DB5': // hot pink
+        return '#f9a8d4';
+      default:
+        return '';
+    }
+  }
 }
