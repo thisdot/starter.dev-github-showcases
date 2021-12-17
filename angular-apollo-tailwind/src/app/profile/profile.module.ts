bc3a8b5e03c087bf4fcb9925683425702c008408
WillHutt <whutt1994@gmail.com>
2021-12-17 11:49:32 -0800

    fix: resolves name changes

--- angular-apollo-tailwind/src/app/profile/profile.module.ts
+++ angular-apollo-tailwind/src/app/profile/profile.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { ProfileNavComponent } from './profile-nav/profile-nav.component';
+import { IconsModule } from '../components/icons/icons.module';
 
 @NgModule({
   declarations: [ProfileComponent, ProfileNavComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IconsModule],
   exports: [ProfileComponent],
 })
 export class ProfileModule {}
