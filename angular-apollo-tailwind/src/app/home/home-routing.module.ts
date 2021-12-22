07404a13954f9ed74bb499f2bc5ffad12d49d279
morgnism <morgan@border.dev>
2021-12-22 10:31:43 -0700

    fix: oauth redirect route

--- angular-apollo-tailwind/src/app/home/home-routing.module.ts
+++ angular-apollo-tailwind/src/app/home/home-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth.guard';
-import { RedirectComponent } from '../provider/redirect/redirect.component';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
@@ -11,10 +10,6 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     loadChildren: () =>
       import('../repos/repos.module').then((m) => m.ReposModule),
-  },
-  {
-    path: 'redirect',
-    component: RedirectComponent,
   },
 ];
 
