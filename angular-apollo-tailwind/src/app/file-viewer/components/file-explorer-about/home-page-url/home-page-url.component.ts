28d4a67a982dde47009b00d3b4de325db6a4b17e
Morgan Worrell <morgan@thisdot.co>
2021-12-29 20:40:06 -0700

    Feat/repo about topics (#83)
    * fix: link icon

--- angular-apollo-tailwind/src/app/file-viewer/components/file-explorer-about/home-page-url/home-page-url.component.ts
+++ angular-apollo-tailwind/src/app/file-viewer/components/file-explorer-about/home-page-url/home-page-url.component.ts
@@ -3,12 +3,20 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-home-page-url',
   template: `<div *ngIf="homepageUrl" class="linkContainer">
-    <hero-icon
-      name="link"
-      type="outline"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       class="linkIcon"
-      aria-hidden="true"
-    ></hero-icon>
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+      />
+    </svg>
     <a [href]="homepageUrl" class="link" target="_blank">
       {{ homepageUrl }}
     </a>
