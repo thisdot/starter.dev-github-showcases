aabff1abde46a479f3689d6f618a9ac793a0ae89
Chris Trześniewski <k.trzesniewski@gmail.com>
2022-01-12 15:55:01 +0100

    Fix/UI bugs (#96)
    * fix circular deps
    * fix mage overflow in readme

--- angular-apollo-tailwind/src/app/file-viewer/repo-read-me/read-me/read-me.component.ts
+++ angular-apollo-tailwind/src/app/file-viewer/repo-read-me/read-me/read-me.component.ts
@@ -4,6 +4,7 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-read-me',
   template: `<span [innerHTML]="text | markdown"></span>`,
   encapsulation: ViewEncapsulation.ShadowDom,
+  styleUrls: ['./read-me.component.css'],
 })
 export class ReadMeComponent {
   @Input() text = '';
