8c9bbf739536ac54ac69a73b93feb7f435f46d9a
Dane Grant <danecando@gmail.com>
2022-01-20 08:38:17 -0500

    Fix leaking client_secret to the client (#97)

--- starter-dev-backend/lib/signin-url.js
+++ starter-dev-backend/lib/signin-url.js
@@ -6,7 +6,6 @@ export default () => {
 
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID ?? '',
-    client_secret: process.env.GITHUB_CLIENT_SECRET ?? '',
   });
 
   return {
