d3eb804bcad646d0d3f166cf582fd151b82ed49a
LindaT <linda@thisdot.co>
2022-02-01 17:15:35 -0600

    feat/104 set up services and got initial routing working

--- angular-ngrx-scss/src/app/auth/auth.module.ts
+++ angular-ngrx-scss/src/app/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
-  imports: [CommonModule],
+  declarations: [AuthComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
--- angular-ngrx-scss/src/environments/environment.prod.ts
+++ angular-ngrx-scss/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiUrl: '/api',
   production: true,
 };
