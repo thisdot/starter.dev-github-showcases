8cd60b93d0b43af6efa14f592c99463af0ae0385
LindaT <linda@thisdot.co>
2022-02-01 15:59:59 -0600

    104 - set up login routing and initial components

--- angular-ngrx-scss/src/app/app-routing.module.ts
+++ angular-ngrx-scss/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signin',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'redirect',
+    redirectTo: 'signin/redirect',
+  },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ angular-ngrx-scss/src/app/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [CommonModule],
+})
+export class AuthModule {}
