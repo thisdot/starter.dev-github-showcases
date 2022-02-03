2027c5606be5b5f693c3499af92412f3c0b9f3eb
LindaT <linda@thisdot.co>
2022-02-02 18:43:49 -0600

    test: 104 deleted unused test files; added tests to service files

--- angular-ngrx-scss/src/app/auth/services/auth.guard.spec.ts
+++ angular-ngrx-scss/src/app/auth/services/auth.guard.spec.ts
@@ -1,16 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { TokenService } from './token.service';
 
 describe('AuthGuard', () => {
+  let routerSpy: jasmine.SpyObj<Router>;
+  let authService: AuthService;
+  let tokenService: jasmine.SpyObj<TokenService>;
+  let httpClient: jasmine.SpyObj<HttpClient>;
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
+    routerSpy = jasmine.createSpyObj('Router', ['createUrlTree']);
+    tokenService = jasmine.createSpyObj('TokenService', ['getToken']);
+    authService = new AuthService(httpClient, tokenService);
+    guard = new AuthGuard(authService, routerSpy);
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
+  describe('if user is successfully authenticated', () => {
+    beforeEach(() => {
+      tokenService.getToken.and.returnValue('1234');
+    });
+
+    it('grants route access to home page', () => {
+      const canActivate = guard.canActivate();
+
+      expect(canActivate).toBeTrue();
+    });
+  });
+
+  describe('if user is not authenticated', () => {
+    beforeEach(() => {
+      tokenService.getToken.and.returnValue(null);
+    });
+
+    it('routes the user to the sign in page', () => {
+      const routeSpy = routerSpy.createUrlTree as jasmine.Spy;
+
+      guard.canActivate();
+      const navArgs = routeSpy.calls.first().args[0];
+      expect(navArgs)
+        .withContext('should navigate to sign in page')
+        .toContain('/signin');
+    });
   });
 });
--- angular-ngrx-scss/src/app/auth/services/auth.service.spec.ts
+++ angular-ngrx-scss/src/app/auth/services/auth.service.spec.ts
@@ -1,16 +1,76 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { environment } from 'src/environments/environment';
+import { SignInResponse, AuthResponse } from '../interfaces/auth';
 import { AuthService } from './auth.service';
+import { TokenService } from './token.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let tokenService: jasmine.SpyObj<TokenService>;
+  let authService: AuthService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    tokenService = jasmine.createSpyObj('TokenService', ['getToken']);
+    authService = TestBed.inject(AuthService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should redirect the user to the sign in link', () => {
+    const expectedSignInResponse = {
+      redirectUrl: `${environment.apiUrl}/auth/signin`,
+    };
+
+    authService.signIn().subscribe((res) => {
+      expect(res).toEqual(expectedSignInResponse as SignInResponse);
+    });
+
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: expectedSignInResponse.redirectUrl,
+    });
+
+    req.flush(expectedSignInResponse);
+  });
+
+  it('should get an access token', () => {
+    const expectedAuthResponse = {
+      access_token: '12345',
+      bearer: '',
+      scope: '',
+    };
+    const expectedPostUrl = `${environment.apiUrl}/auth/signin/callback`;
+
+    authService.getToken('code').subscribe((res) => {
+      expect(res).toEqual(expectedAuthResponse as AuthResponse);
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: expectedPostUrl,
+    });
+
+    req.flush(expectedAuthResponse);
+  });
+
+  it('checks if a user is authenticated', () => {
+    const token = 'hello';
+    tokenService.getToken.and.returnValue(token);
+
+    const isAuthenticated = authService.isAuthenticated();
+    expect(isAuthenticated).toBeTrue();
+  });
+
+  it('checks if a user is not authenticated', () => {
+    tokenService.getToken.and.returnValue(null);
+
+    const isNotAuthenticated = authService.isAuthenticated();
+    expect(isNotAuthenticated).toBeFalse();
   });
 });
