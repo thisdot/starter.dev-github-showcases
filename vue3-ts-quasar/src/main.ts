11c00a2089511014cd83e724db4cae54f1e79552
Jerry Hogan <jerry@enyata.com>
2022-02-15 18:42:19 +0100

    Vue3 with quasar

--- vue3-ts-quasar/src/main.ts
+++ vue3-ts-quasar/src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(Quasar, quasarUserOptions)
+  .use(store)
+  .use(router)
+  .mount("#app");
---
+++ vue3-ts-quasar/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pluginOptions: {
+    quasar: {
+      importStrategy: "kebab",
+      rtlSupport: true,
+    },
+  },
+  transpileDependencies: ["quasar"],
+};
