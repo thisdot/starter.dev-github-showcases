3db90c0009413b24a772f9bacace2e0293c94335
LindaT <linda@thisdot.co>
2022-02-15 11:14:46 -0600

    fix: 105 added token interceptor

--- angular-ngrx-scss/src/app/app.module.ts
+++ angular-ngrx-scss/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { reducers } from './state';
 import { UserEffects } from './state/user';
+import { TokenInterceptor } from './auth/services/token.interceptor';
 
 @NgModule({
   imports: [
@@ -25,7 +26,13 @@ import { UserEffects } from './state/user';
     EffectsModule.forRoot([UserEffects]),
   ],
   declarations: [AppComponent],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
