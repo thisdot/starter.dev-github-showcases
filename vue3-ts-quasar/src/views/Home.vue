c50bfd84c65ba85ba3d137e0c6bb12b4ddd54b86
AllanJeremy <aj.dev254@gmail.com>
2022-02-16 19:08:45 +0300

    [Vue3 + TS + Quasar] cleanup: Cleaned up the main app component as well as the home page component

--- vue3-ts-quasar/src/views/Home.vue
+++ vue3-ts-quasar/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>This is home</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
