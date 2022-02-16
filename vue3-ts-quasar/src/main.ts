87283d0c721170c38875ac7fef8b0b6fe69bf62f
AllanJeremy <aj.dev254@gmail.com>
2022-02-16 12:55:46 +0300

    dependency: replaced vuex with pinia as the preferred state management solution (#123)

--- vue3-ts-quasar/src/main.ts
+++ vue3-ts-quasar/src/main.ts
@@ -1,12 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
 
-createApp(App)
-  .use(Quasar, quasarUserOptions)
-  .use(store)
-  .use(router)
-  .mount("#app");
+createApp(App).use(Quasar, quasarUserOptions).use(router).mount("#app");
