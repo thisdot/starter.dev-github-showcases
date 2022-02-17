2d5a98c62304d6fe0720e7d4fca209387b73f352
Jerry Hogan <jerry@enyata.com>
2022-02-17 22:11:22 +0100

    UserProfileCard in progress. Added 5 of 13 items

--- vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.stories.js
+++ vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.stories.js
@@ -3,7 +3,12 @@ import UserProfileCard from '.';
 export default {
   title: 'Component/UserProfileCard',
   component: UserProfileCard,
-  argTypes: {},
+  argTypes: {
+    avatarUrl: {},
+    name: {},
+    login: {},
+    bio: {},
+  },
 };
 
 const Template = (args) => ({
@@ -17,3 +22,10 @@ const Template = (args) => ({
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  avatarUrl: 'https://avatars.githubusercontent.com/u/28502531?v=4',
+  name: 'Jerry Hogan',
+  login: 'hdjerry',
+  bio: 'I am a Front-End Developer with about 3 years plus of experience and with a very good knowledge of JavaScript, Vue, React, Tailwind, Styled component',
+  // avatarUrl: 'https://avatars.githubusercontent.com/u/63736569?v=4',
+};
--- vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.vue
+++ vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>User Profile Card</div>
+  <div class="profile-card">
+    <div class="avatar_name_bio">
+      <div class="avatar">
+        <img :src="avatarUrl" alt="avatar" />
+      </div>
+      <div class="name_bio">
+        <span class="name">
+          {{ name }}
+        </span>
+        <span class="login">
+          {{ login }}
+        </span>
+      </div>
+    </div>
+    <p class="bio">
+      {{ bio }}
+    </p>
+    <div class="divider" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +31,14 @@ export default defineComponent({
       default: '',
     },
     name: {
+      type: String,
+      default: '',
+    },
+    login: {
+      type: String,
+      default: '',
+    },
+    bio: {
       type: String,
       default: '',
     },
