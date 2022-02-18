d77f2a268d902bc2909f82c4c6d2bc1458470bc6
AllanJeremy <aj.dev254@gmail.com>
2022-02-18 13:20:03 +0300

    [Vue3 + TS + Quasar]  feature: Completed NavHeader component as well as stories - Also removed unnecessary code in quasar.variables.scss that might have brought on confusion at a later stage
    - Wrapped the NavHeader component in a q-header. q-header doesn't work with storybook, so we have extracted it out of the NavHeader component

--- vue3-ts-quasar/src/App.vue
+++ vue3-ts-quasar/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <NavHeader />
+    <q-header>
+      <NavHeader />
+    </q-header>
     <q-page-container>
       <router-view></router-view>
     </q-page-container>
