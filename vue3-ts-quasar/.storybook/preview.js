3f4af0fe035d326ceecf5b3f0051c6edb56178f8
AllanJeremy <aj.dev254@gmail.com>
2022-02-18 10:14:10 +0300

    [Vue3 + TS + Quasar] config: Updated storybook config to work with quasar styling

--- vue3-ts-quasar/.storybook/preview.js
+++ vue3-ts-quasar/.storybook/preview.js
@@ -1,9 +1,22 @@
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/animate/fadeInUp.css';
+import '@quasar/extras/animate/fadeOutDown.css';
+import '@quasar/extras/animate/fadeInRight.css';
+import '@quasar/extras/animate/fadeOutRight.css';
+
+import 'quasar/dist/quasar.css';
+import { app } from '@storybook/vue3';
+import { Quasar } from 'quasar';
+
+app.use(Quasar, {});
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
