45986930e907fc3a844643c8410bcb908f563892
Jerry Hogan <jerry@enyata.com>
2022-02-19 09:50:17 +0100

    Added custom input

--- vue3-ts-quasar/src/components/SearchInput/SearchInput.vue
+++ vue3-ts-quasar/src/components/SearchInput/SearchInput.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="search_container">
     <form>
-      <input placeholder="Find a repository..." />
+      <AppInput v-model="search" placeholder="Find a repository..." />
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import AppInput from '../AppInput';
 
 export default defineComponent({
   name: 'SearchInput',
+  components: { AppInput },
+  setup() {
+    const search = ref('');
+
+    return { search };
+  },
 });
 </script>
 
