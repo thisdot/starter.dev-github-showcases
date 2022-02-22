3ca24003ac8ee35026a550dfb2cbb5f2f061048b
LindaT <linda@thisdot.co>
2022-02-21 18:28:02 -0600

    fix: 121 removed unnecessary changes from previous testing

--- angular-ngrx-scss/tools/generate-cert.sh
+++
@@ -1,5 +0,0 @@
-# generates a certs for https
-openssl req -x509 -out ./certs/private.crt -keyout ./certs/private.key \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=localhost' -extensions EXT -config <( \
-   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
