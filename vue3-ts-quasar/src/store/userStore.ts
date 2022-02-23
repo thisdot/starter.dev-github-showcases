4bf8774b68d8ae0e0aa0eeead0bbcce270292367
Allan N Jeremy <AllanJeremy@users.noreply.github.com>
2022-02-23 13:10:08 +0300

    Revert "Refactoring/better component loading - component aggregator (#162)" (#166)
    This reverts commit 92c2cf85cafbb95704f76353e7c378a657112f07.

--- vue3-ts-quasar/src/store/userStore.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const useUserStore = defineStore('userStore', {
-  state: () => ({
-    username: 'TestUsername',
-    profileImg: 'https://place-hold.it/64x64',
-  }),
-});
