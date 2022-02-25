87480087b6914faac0169f7ca712feac77ce5db8
morgnism <morgan@border.dev>
2022-02-25 14:21:07 -0700

    updated cert generation script

--- angular-apollo-tailwind/tools/generate-cert.sh
+++ angular-apollo-tailwind/tools/generate-cert.sh
@@ -1,5 +1,7 @@
 # generates a certs for https
+mkdir certs
 openssl req -x509 -out ./certs/private.crt -keyout ./certs/private.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+cd ..
