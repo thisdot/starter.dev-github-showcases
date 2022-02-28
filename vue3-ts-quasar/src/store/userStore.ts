10dd8ceecfabb9f6cbef2602c10bca011e4e6182
AllanJeremy <aj.dev254@gmail.com>
2022-02-28 16:50:53 +0300

    [Vue3 + TS + Quasar] enhancement: Added logged in getter in userStore

--- vue3-ts-quasar/src/store/userStore.ts
+++ vue3-ts-quasar/src/store/userStore.ts
@@ -1,8 +1,16 @@
 import { defineStore } from 'pinia';
 
+//
+import { useToken } from '@/composables';
+
+const { getAuthToken } = useToken();
+
 export const useUserStore = defineStore('userStore', {
   state: () => ({
     username: 'TestUsername',
     profileImg: 'https://place-hold.it/64x64',
   }),
+  getters: {
+    isLoggedIn: () => !!getAuthToken(),
+  },
 });
