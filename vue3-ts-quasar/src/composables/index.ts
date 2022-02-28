07d6662df1493e959fb717466f2a3c5e35142dd5
AllanJeremy <aj.dev254@gmail.com>
2022-02-28 16:45:39 +0300

    [Vue3 + TS + Quasar] feature: Added useNav composable Also added the new composables to the overall composables aggregator

--- vue3-ts-quasar/src/composables/index.ts
+++ vue3-ts-quasar/src/composables/index.ts
@@ -2,7 +2,14 @@
     This file is simply meant to be an aggregator of all composables.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
-import useApollo from './useApollo';
+import { useAuth, useToken } from './auth';
+import useNav from './useNav';
 
 //* EXPORTS
-export { useApollo };
+export {
+  // Auth stuff
+  useAuth,
+  useToken,
+  //
+  useNav,
+};
---
+++ vue3-ts-quasar/src/composables/useNav.ts
@@ -0,0 +1,46 @@
+import { useRouter } from 'vue-router';
+import { delay } from 'lodash';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default () => {
+  const $router = useRouter();
+
+  /** @param {String} linkPath the path the link is meant to redirect to. For example: `/auth` */
+  const linkIsActive = (linkPath: string) => {
+    return window.location.pathname === linkPath;
+  };
+
+  /** Redirect user the given link after the wait duration
+   * @param link The link to redirect to
+   * @param wait The amount of time to wait before redirecting in milliseconds
+   */
+  const goTo = (link: string, wait = 0) => {
+    if (window.location.pathname !== link) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      delay(() => {
+        void $router.push({
+          path: link,
+        });
+      }, wait);
+    }
+  };
+
+  /** Redirect user an external link after the wait duration
+   * @param link The link to redirect to
+   * @param wait The amount of time to wait before redirecting in milliseconds
+   */
+  const goToExternal = (link: string, wait = 0) => {
+    if (window.location.pathname !== link) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      delay(() => {
+        window.location.replace(link);
+      }, wait);
+    }
+  };
+
+  return {
+    goTo,
+    goToExternal,
+    linkIsActive,
+  };
+};
