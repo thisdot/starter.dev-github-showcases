aaf9658442bd29bb44705c06648bd4eccfddb507
AllanJeremy <aj.dev254@gmail.com>
2022-03-01 16:38:09 +0300

    [Vue3 + TS + Quasar] refactoring: Components & Pages using direct export syntax

--- vue3-ts-quasar/src/components/index.ts
+++ vue3-ts-quasar/src/components/index.ts
@@ -2,39 +2,18 @@
     This file is simply meant to be an aggregator of all components.
     This file does not implement any logic of its own and simply serves the purpose of importing components and exporting them out together so that they can be loaded from one file/directory.
 */
-import AppInput from './AppInput';
-import EmptyContainer from './EmptyContainer';
-import GistsPanel from './GistsPanel';
-import Loading from './Loading';
-import Logo from './Logo';
-import NavHeader from './NavHeader';
-import RepoCard from './RepoCard';
-import SearchDropdowns from './SearchDropdowns';
-import SearchInput from './SearchInput';
-import SearchFilter from './SearchFilter';
-import UserAvatar from './UserAvatar';
-import UserProfileCard from './UserProfileCard';
-import TabHeader from './TabHeader';
-import ProfilePageLayout from './ProfilePageLayout';
-import OrganizationPageLayout from './OrganizationPageLayout';
-
-// TODO: Consider reading the filesystem dynamically to fetch all vue components (children of the components/ directory)
-
-//
-export {
-  AppInput,
-  EmptyContainer,
-  GistsPanel,
-  Loading,
-  Logo,
-  NavHeader,
-  RepoCard,
-  SearchDropdowns,
-  SearchInput, //? Imported in order of what depends on what. SearchFilter depends on SearchInput
-  SearchFilter,
-  UserAvatar,
-  UserProfileCard,
-  TabHeader,
-  ProfilePageLayout,
-  OrganizationPageLayout,
-};
+export { default as AppInput } from './AppInput';
+export { default as EmptyContainer } from './EmptyContainer';
+export { default as GistsPanel } from './GistsPanel';
+export { default as Loading } from './Loading';
+export { default as Logo } from './Logo';
+export { default as NavHeader } from './NavHeader';
+export { default as RepoCard } from './RepoCard';
+export { default as SearchDropdowns } from './SearchDropdowns';
+export { default as SearchInput } from './SearchInput'; //? Imported in order of what depends on what. SearchFilter depends on SearchInput
+export { default as SearchFilter } from './SearchFilter';
+export { default as UserAvatar } from './UserAvatar';
+export { default as UserProfileCard } from './UserProfileCard';
+export { default as TabHeader } from './TabHeader';
+export { default as ProfilePageLayout } from './ProfilePageLayout';
+export { default as OrganizationPageLayout } from './OrganizationPageLayout';
