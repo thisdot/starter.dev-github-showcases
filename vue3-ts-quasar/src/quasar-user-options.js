22e9a8ec8b1264359c5cd0f5185c53e8c1b0bb89
AllanJeremy <aj.dev254@gmail.com>
2022-03-01 10:29:10 +0300

    [Vue3 + TS + Quasar] config: Added dialog, meta & notify quasar plugins

--- vue3-ts-quasar/src/quasar-user-options.js
+++ vue3-ts-quasar/src/quasar-user-options.js
@@ -2,8 +2,11 @@ import './styles/quasar.scss';
 import '@quasar/extras/material-icons/material-icons.css';
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
 
+//
+import { Dialog, Meta, Notify } from 'quasar';
+
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
-  plugins: {},
+  plugins: [Dialog, Meta, Notify],
 };
