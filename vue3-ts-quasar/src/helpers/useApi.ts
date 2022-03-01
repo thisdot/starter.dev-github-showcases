e9d58860e52764b18e14ed707bdab9993483caa3
AllanJeremy <aj.dev254@gmail.com>
2022-03-01 16:22:28 +0300

    [Vue3 + TS + Quasar] refactoring: Renamed api helper to useApi Also changed it to be a named  export

--- vue3-ts-quasar/src/helpers/useApi.ts
+++ vue3-ts-quasar/src/helpers/useApi.ts
@@ -5,11 +5,23 @@ import axios, {
 } from 'axios';
 import { EnvironmentConfig } from '../config';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (
+interface UseApi {
+  getRequest: <T>(
+    url: string,
+    additionalConfig: AxiosRequestConfig<unknown>,
+  ) => Promise<T>;
+
+  postRequest: <T>(
+    url: string,
+    requestData: Record<string, unknown> | null,
+    additionalConfig: AxiosRequestConfig<unknown>,
+  ) => Promise<T>;
+}
+
+export const useApi = (
   baseUrl = EnvironmentConfig.API_URL,
   additionalHeaders: AxiosRequestHeaders = {},
-) => {
+): UseApi => {
   axios.defaults.baseURL = baseUrl;
 
   // #region UNAUTHENTICATED REQUESTS
@@ -38,53 +50,18 @@ export default (
   const postRequest = async <T = Record<string, unknown> | null>(
     url: string,
     requestData = {},
+    additionalConfig: AxiosRequestConfig<unknown> = {},
   ) => {
     const response: AxiosResponse<T> = await axios.post(url, requestData, {
+      ...additionalConfig,
       headers: additionalHeaders,
     });
 
     return response.data;
   };
-
-  /** Make a standard unauthenticated patch request
-   * @param {String} url The url the API request should be sent to
-   * @param {Object} requestData Any data that needs to be passed in as part of the request
-   * @return {Promise<Object>} A promise that resolves to the API response if successful. If rejected, the promise returns details of what went wrong with the request.
-   */
-  const patchRequest = async <T = Record<string, unknown> | null>(
-    url: string,
-    requestData = {},
-  ) => {
-    const response: AxiosResponse<T> = await axios.patch(url, requestData, {
-      headers: additionalHeaders,
-    });
-
-    return response.data;
-  };
-
-  /** Make a standard unauthenticated delete request
-   * @param {String} url The url the API request should be se nt to
-   * @param {Object} requestParams Any request parameters passed in as part of the request
-   * @return {Promise<Object>} A promise that resolves to the API response if successful. If rejected, the promise returns details of what went wrong with the request.
-   */
-  const deleteRequest = async <T = Record<string, unknown> | null>(
-    url: string,
-    requestParams = {},
-  ) => {
-    const response: AxiosResponse<T> = await axios.delete(url, {
-      params: requestParams,
-      headers: additionalHeaders,
-    });
-
-    return response.data;
-  };
-
-  // #endregion UNAUTHENTICATED REQUESTS
 
   return {
     getRequest,
     postRequest,
-    patchRequest,
-    deleteRequest,
   };
 };
