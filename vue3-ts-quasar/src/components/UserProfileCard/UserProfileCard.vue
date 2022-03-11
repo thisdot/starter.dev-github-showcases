3265122afe3cf5628f489e3f018e9c1a8a888733
AllanJeremy <aj.dev254@gmail.com>
2022-03-11 17:57:18 +0300

    Vue3 + TS+ Quasar enhancement: Refactored UserProfileCard to only rely on username passed in TODO: Fix storybook

--- vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.vue
+++ vue3-ts-quasar/src/components/UserProfileCard/UserProfileCard.vue
@@ -1,208 +1,140 @@
 <template>
-  <div class="profile-card">
+  <!-- Loading -->
+  <div v-if="userLoading" class="q-mt-xl q-px-lg" style="width: 400px">
+    <div class="full-width text-center q-mb-md">
+      <q-skeleton
+        class="avatar q-mb-sm"
+        type="circle"
+        style="width: 18rem; height: 18rem"
+      ></q-skeleton>
+    </div>
+    <q-skeleton class="q-mb-xs" type="rect"></q-skeleton>
+    <q-skeleton class="q-mb-md" type="text"></q-skeleton>
+
+    <div class="row q-col-gutter-x-md q-mb-lg">
+      <div class="col-6">
+        <q-skeleton type="rect"></q-skeleton>
+      </div>
+      <div class="col-6">
+        <q-skeleton type="rect"></q-skeleton>
+      </div>
+    </div>
+    <q-skeleton class="q-mb-md" type="text"></q-skeleton>
+    <q-skeleton class="q-mb-md" type="text"></q-skeleton>
+  </div>
+
+  <!-- Actual content -->
+  <div v-else class="profile-card" v-bind="$attrs">
     <div class="avatar_name_bio">
+      <!-- TODO: Replace with q-avatar -->
       <div class="avatar">
-        <img :src="avatarUrl" alt="avatar" />
+        <img :src="userData.avatarUrl" alt="avatar" />
       </div>
       <div class="name_bio">
         <span class="name">
-          {{ name }}
+          {{ userData.name }}
         </span>
         <span class="login">
-          {{ login }}
+          {{ userData.login }}
         </span>
       </div>
     </div>
     <p class="bio">
-      {{ bio }}
+      {{ userData.bio }}
     </p>
     <div class="follows">
-      <a href="#" class="follower" v-if="follower">
-        <svg
-          text="muted"
-          aria-hidden="true"
-          height="16"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          data-view-component="true"
-          class="allSvg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"
-          ></path>
-        </svg>
+      <a href="#" class="follower" v-if="userData.followers.totalCount">
+        <q-icon class="q-mr-xs" name="fas fa-users"></q-icon>
         <span>
-          {{ follower }}
+          {{ userData.followers.totalCount }}
         </span>
         followers
       </a>
-      <span>.</span>
-      <a href="#" class="following" v-if="following">
+      <span>·</span>
+      <a href="#" class="following" v-if="userData.following.totalCount">
         <span>
-          {{ following }}
+          {{ userData.following.totalCount }}
         </span>
         followers
       </a>
     </div>
     <div class="others">
       <p>
-        <svg
-          aria-hidden="true"
-          height="16"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          data-view-component="true"
-          class="allSvg starIcon"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-          ></path>
-        </svg>
-        <span>{{ stars }}</span>
+        <q-icon class="q-mr-xs" name="far fa-star"></q-icon>
+
+        <span>{{
+          userData.starredRepositories?.totalCount?.toLocaleString() || 0
+        }}</span>
       </p>
-      <p>
-        <svg
-          class="allSvg loc"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          height="16"
-          aria-hidden="true"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
-          ></path>
-        </svg>
-        <span>{{ location }}</span>
+      <p v-if="userData.company">
+        <q-icon class="q-mr-xs" name="fas fa-map-marker-alt"></q-icon>
+        <span>{{ userData?.location }}</span>
       </p>
-      <p>
-        <svg
-          class="allSvg orgs"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          height="16"
-          aria-hidden="true"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M1.5 14.25c0 .138.112.25.25.25H4v-1.25a.75.75 0 01.75-.75h2.5a.75.75 0 01.75.75v1.25h2.25a.25.25 0 00.25-.25V1.75a.25.25 0 00-.25-.25h-8.5a.25.25 0 00-.25.25v12.5zM1.75 16A1.75 1.75 0 010 14.25V1.75C0 .784.784 0 1.75 0h8.5C11.216 0 12 .784 12 1.75v12.5c0 .085-.006.168-.018.25h2.268a.25.25 0 00.25-.25V8.285a.25.25 0 00-.111-.208l-1.055-.703a.75.75 0 11.832-1.248l1.055.703c.487.325.779.871.779 1.456v5.965A1.75 1.75 0 0114.25 16h-3.5a.75.75 0 01-.197-.026c-.099.017-.2.026-.303.026h-3a.75.75 0 01-.75-.75V14h-1v1.25a.75.75 0 01-.75.75h-3zM3 3.75A.75.75 0 013.75 3h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 3.75zM3.75 6a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM3 9.75A.75.75 0 013.75 9h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 9.75zM7.75 9a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM7 6.75A.75.75 0 017.75 6h.5a.75.75 0 010 1.5h-.5A.75.75 0 017 6.75zM7.75 3a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5z"
-          ></path>
-        </svg>
-        <span>{{ organization }}</span>
+      <p v-if="userData.company">
+        <q-icon class="q-mr-xs" name="far fa-building"></q-icon>
+        <span>{{ userData.company }}</span>
       </p>
-      <p>
-        <svg
-          aria-hidden="true"
-          height="16"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          data-view-component="true"
-          class="allSvg bioLink"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"
-          ></path>
-        </svg>
-        <a :href="bioLink">{{ bioLink }}</a>
+      <p v-if="userData.websiteUrl">
+        <q-icon class="q-mr-xs" name="fas fa-link"></q-icon>
+        <a :href="userData.websiteUrl" target="__blank">{{
+          userData.websiteUrl
+        }}</a>
       </p>
     </div>
-    <div class="divider" />
-    <h3 class="heading">Organizations</h3>
-    <div class="flex organization">
-      <a
-        v-for="(organization, i) in organizations"
-        :href="organization.url"
-        :key="i"
-      >
-        <img :src="organization.avatar" alt="some" class="avatar" />
-        <div class="info">
-          <p class="top">
-            <img
-              :src="organization.avatar"
-              alt="some"
-              class="avatar"
-              style="width: 42px; height: 42px"
-            />
-            <span>
-              {{ organization.name }}
-            </span>
-          </p>
-          <div class="divider" />
-        </div>
-      </a>
-    </div>
+
+    <template v-if="userData.organizations.nodes.length">
+      <q-separator></q-separator>
+      <h3 class="heading">Organizations</h3>
+      <div class="flex organization">
+        <a
+          v-for="organization in userData.organizations.nodes"
+          :href="organization.login"
+          :key="`org-${organization.login}`"
+        >
+          <img :src="organization.avatarUrl" alt="some" class="avatar" />
+          <div class="info">
+            <p class="top">
+              <!-- TODO: Replace with q-avatar -->
+              <img
+                :src="organization.avatarUrl"
+                alt="some"
+                class="avatar"
+                style="width: 42px; height: 42px"
+              />
+              <span>
+                {{ organization.login }}
+              </span>
+            </p>
+            <div class="divider" />
+          </div>
+        </a>
+      </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-
-interface Organization {
-  name: string;
-  url: string;
-  avatar: string;
-}
+import { defineComponent, defineProps } from 'vue';
 
 export default defineComponent({
   name: 'UserProfileCard',
-  props: {
-    avatarUrl: {
-      type: String,
-      default: '',
-    },
-    name: {
-      type: String,
-      default: '',
-    },
-    login: {
-      type: String,
-      default: '',
-    },
-    bio: {
-      type: String,
-      default: '',
-    },
-    organization: {
-      type: String,
-      default: '',
-    },
-    location: {
-      type: String,
-      default: '',
-    },
-    bioLink: {
-      type: String,
-      default: '',
-    },
-    stars: {
-      type: Number,
-      default: 0,
-    },
-    following: {
-      type: Number,
-      default: 0,
-    },
-    follower: {
-      type: Number,
-      default: 0,
-    },
-    organizations: {
-      type: Array as PropType<Organization[]>,
-      default: () => [],
-    },
-  },
 });
 </script>
 
+<script lang="ts" setup>
+import { Loading } from '@/components';
+import { useUser } from '@/composables';
+const { getUserProfile } = useUser();
+
+const props = defineProps({
+  username: String,
+});
+
+const { data: userData, loading: userLoading } = getUserProfile(props.username);
+</script>
+
 <style lang="scss" scoped>
+// TODO: Refactor all this
 @import '../../App.css';
 
 .profile-card {
