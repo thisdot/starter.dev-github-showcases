bab75af8472e89f8e5d11b36b3c9bc5f304f2212
AllanJeremy <aj.dev254@gmail.com>
2022-03-11 17:56:08 +0300

    Vue3 + TS+ Quasar enhancement: Added useUser composable

--- vue3-ts-quasar/src/composables/github/index.ts
+++ vue3-ts-quasar/src/composables/github/index.ts
@@ -3,3 +3,4 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 export { useRepository } from './useRepository';
+export { useUser } from './useUser';
