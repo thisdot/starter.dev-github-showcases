cc8f6fdae0ff1a16241388a327830c2ca6c571fd
Brett Zeidler <brettzeidler93@gmail.com>
2022-05-12 10:34:50 -0500

    feat: renamed e2e package, added two missing cypress config and commands

--- showcase-e2e-tests/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add("mockNextAuth", () => {
-  cy.intercept("/api/auth/session", { fixture: "auth/session.json" }).as(
-    "session"
-  );
-
-  cy.setCookie("next-auth.session-token", "your-valid-token");
-
-  Cypress.Cookies.preserveOnce("next-auth.session-token");
-});
