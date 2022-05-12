f397e7857276ce5ebe7c8cc120bcd655662b9e89
Ian Mungai <iansam74@gmail.com>
2022-05-12 16:43:39 +0300

    feat: add error handler interceptor

--- angular-apollo-tailwind/src/app/gql/graphql.module.ts
+++ angular-apollo-tailwind/src/app/gql/graphql.module.ts
@@ -4,11 +4,27 @@ import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from 'src/environments/environment';
 
+import { onError } from '@apollo/client/link/error';
+
 const uri = environment.graphApiUrl; // <-- add the URL of the GraphQL server here
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const http = httpLink.create({ uri });
+  const error = onError(({ networkError, graphQLErrors }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      );
+
+    if (networkError) console.log(`[Network error]: ${networkError.name}`);
+  });
+
+  const link = error.concat(http);
+
   return {
-    link: httpLink.create({ uri }),
+    link,
     cache: new InMemoryCache(),
     defaultOptions: {
       watchQuery: {
