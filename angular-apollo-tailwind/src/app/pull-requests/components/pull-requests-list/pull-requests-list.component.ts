8eb756e443c029af360fdfc2999c643ebf9ad629
Ian Mungai <iansam74@gmail.com>
2022-05-16 12:45:39 +0300

    fix: convert createdAt to date

--- angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
+++ angular-apollo-tailwind/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RepoPullRequest } from 'src/app/gql';
 
 @Component({
@@ -6,10 +6,18 @@ import { RepoPullRequest } from 'src/app/gql';
   templateUrl: './pull-requests-list.component.html',
   styleUrls: ['./pull-requests-list.component.css'],
 })
-export class PullRequestsListComponent {
+export class PullRequestsListComponent implements OnInit {
   @Input() pullRequests: RepoPullRequest[] = [];
 
   readonly baseDate = new Date();
+
+  ngOnInit(): void {
+    console.log(this.pullRequests);
+    this.pullRequests.map((pr) => {
+      pr.createdAt = new Date(pr.createdAt);
+      // pr.closedAt = pr.closedAt ? new Date(pr.closedAt) : null;
+    });
+  }
 
   colorMap(color: string): string {
     switch (color) {
