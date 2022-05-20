ed96feeba6edacf16a1f2069e0f363329784d445
Mark Shenouda <mark@thisdot.co>
2022-05-20 17:33:39 +0200

    feat: setup react context

--- cra-rxjs-styled-components/src/App.tsx
+++ cra-rxjs-styled-components/src/App.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router';
 import Header from './components/header';
+import StateProvider from './context/StateProvider'
 
 function App() {
   return (
-    <>
+    <StateProvider>
       <Header />
       <Outlet />
-    </>
+    </StateProvider>
   );
 }
 
