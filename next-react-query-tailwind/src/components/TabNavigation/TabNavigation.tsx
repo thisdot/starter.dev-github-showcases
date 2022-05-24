c3f614810fc28ce119b9b86b7f9b67199d959a6f
Ian Mungai <iansam74@gmail.com>
2022-05-24 20:26:11 +0300

    chore: add isCurrentTab return type as boolean

--- next-react-query-tailwind/src/components/TabNavigation/TabNavigation.tsx
+++ next-react-query-tailwind/src/components/TabNavigation/TabNavigation.tsx
@@ -27,7 +27,7 @@ function TabNavigation({
     .filter((queryPart) => typeof queryPart === 'string')
     .join('/');
 
-  const isCurrentTab = (path?: string) => {
+  const isCurrentTab = (path?: string): boolean => {
     const matchPath = path === '' ? basePath : `${basePath}/${path}`;
     return path === '' ? pathname === basePath : pathname.includes(matchPath);
   };
