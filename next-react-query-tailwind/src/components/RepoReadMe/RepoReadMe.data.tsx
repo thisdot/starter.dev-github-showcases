14604d5eb001cdd49d8caec7b018ca4288a707b9
Ian Mungai <iansam74@gmail.com>
2022-05-24 14:36:34 +0300

    fix: empty readme for non root code directory

--- next-react-query-tailwind/src/components/RepoReadMe/RepoReadMe.data.tsx
+++ next-react-query-tailwind/src/components/RepoReadMe/RepoReadMe.data.tsx
@@ -1,12 +1,14 @@
 import gqlClient from '@lib/gqlClient';
 import { useRepoReadMeQuery } from '@lib/github';
 import { useRepo } from '@context/RepoContext';
+import { useRouter } from 'next/router';
 import { parseQuery } from './parseQuery';
 import RepoReadMeView from './RepoReadMe.view';
 import Empty from './Empty';
 
 function RepoReadMe() {
   const repo = useRepo();
+  const { pathname } = useRouter();
   const { data, isLoading, error } = useRepoReadMeQuery(gqlClient, {
     owner: repo.owner,
     name: repo.name,
@@ -19,7 +21,11 @@ function RepoReadMe() {
 
   const readme = parseQuery(data);
 
-  return readme ? <RepoReadMeView readme={readme} /> : <Empty />;
+  return readme ? (
+    <RepoReadMeView readme={readme} />
+  ) : !pathname.includes('tree') ? (
+    <Empty />
+  ) : null;
 }
 
 export default RepoReadMe;
