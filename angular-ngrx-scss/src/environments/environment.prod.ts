79d423293a2a4b5d95a92f9fccb69b35152fd563
LindaT <linda@thisdot.co>
2022-05-25 18:26:54 -0500

    fix: testing fix in env variables

--- angular-ngrx-scss/src/environments/environment.prod.ts
+++ angular-ngrx-scss/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.starter.dev',
   githubUrl: 'https://api.github.com',
-  redirectUrl: 'https://angular-ngrx-scss.starter.dev/redirect',
+  redirectUrl: 'https://angular-ngrx-scss.starter.dev/signin/redirect',
 };
