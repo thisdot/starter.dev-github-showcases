6d234818d427466e0f07c9dcd7b21b00984337ec
LindaT <linda@thisdot.co>
2022-05-25 18:32:57 -0500

    fix: testing the redirect url with preview link

--- angular-ngrx-scss/src/environments/environment.prod.ts
+++ angular-ngrx-scss/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.starter.dev',
   githubUrl: 'https://api.github.com',
-  redirectUrl: 'https://angular-ngrx-scss.starter.dev/signin/redirect',
+  redirectUrl: 'https://pr-315.dbm2zhgk5abrj.amplifyapp.com/redirect',
 };
