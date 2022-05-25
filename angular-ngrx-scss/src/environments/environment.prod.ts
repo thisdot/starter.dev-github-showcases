61a8826dbde4a7edd5c205525b7b17b7fbc09b19
LindaT <linda@thisdot.co>
2022-05-25 18:46:03 -0500

    fix: reverted back to actual value

--- angular-ngrx-scss/src/environments/environment.prod.ts
+++ angular-ngrx-scss/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.starter.dev',
   githubUrl: 'https://api.github.com',
-  redirectUrl: 'https://pr-315.dbm2zhgk5abrj.amplifyapp.com/redirect',
+  redirectUrl: 'https://angular-ngrx-scss.starter.dev/redirect',
 };
