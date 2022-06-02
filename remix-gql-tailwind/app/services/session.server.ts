1dd8bb0874e4566ef15b4babf6505dff62abef10
WillHutt <whutt1994@gmail.com>
2022-06-02 14:41:20 -0700

    refactor: wip

--- remix-gql-tailwind/app/services/session.server.ts
+++ remix-gql-tailwind/app/services/session.server.ts
@@ -14,6 +14,36 @@ export let sessionStorage = createCookieSessionStorage({
 
 export function getSession(request: Request): Promise<Session> {
   return sessionStorage.getSession(request.headers.get('Cookie'));
+
+  // const session = sessionStorage.getSession(request.headers.get('Cookie'));
+  // return {
+  //   session,
+  //   getUser: async () => {
+  //     const session = await sessionStorage.getSession(
+  //       request.headers.get('Cookie')
+  //     );
+  //     // console.log(`SESSION YOS${session}`);
+
+  //     const valid = session.get(auth.sessionKey);
+  //     // console.log(`TESTING ${valid}`);
+  //     if (!valid) return { valid: null, accessToken: '' };
+
+  //     const { accessToken } = await auth.isAuthenticated(request, {
+  //       failureRedirect: '/login',
+  //     });
+  //     return { accessToken, valid };
+  //     // const token = getSessionId();
+  //     // if (!token) return null;
+
+  //     // finds a user and checks expiration timing to request a new link
+  //     // for us we will just use this to find the session.user
+  //     // return getUserFromSessionId(token).catch((error: unknown) => {
+  //     //   unsetSessionId();
+  //     //   console.error(`Failure getting user from session ID:`, error);
+  //     //   return null;
+  //     // });
+  //   },
+  // };
 }
 
 async function getUser(request: Request) {
