d2218765aa25e068375418326afd7a3787766104
Brett Zeidler <brettzeidler93@gmail.com>
2022-06-03 00:15:52 -0500

    test: add repo tree file explorer navigation tests and test IDs

--- starter-dev-e2e/cypress/integration/repo.spec.ts
+++ starter-dev-e2e/cypress/integration/repo.spec.ts
@@ -1,4 +1,8 @@
-import { hasOperationName, aliasQuery } from "../utils/graphql-test-utils";
+import {
+  hasOperationName,
+  aliasQuery,
+  hasExpression,
+} from "../utils/graphql-test-utils";
 
 //TODO: this can be refactored, probably using an enum/json structure to check against
 function handleGqlRequest(req): Boolean {
@@ -14,6 +18,39 @@ function handleGqlRequest(req): Boolean {
     aliasQuery(req, "RepoPage");
     req.reply({
       fixture: "repo/repoPage.graphql.json",
+    });
+    return true;
+  }
+
+  if (
+    hasOperationName(req, "RepoTree") &&
+    hasExpression(req, "main:angular-apollo-tailwind")
+  ) {
+    aliasQuery(req, "RepoTreeFolder1");
+    req.reply({
+      fixture: "repo/repoTree-folder1.graphql.json",
+    });
+    return true;
+  }
+
+  if (
+    hasOperationName(req, "RepoTree") &&
+    hasExpression(req, "main:angular-apollo-tailwind/src")
+  ) {
+    aliasQuery(req, "RepoTreeFolder2");
+    req.reply({
+      fixture: "repo/repoTree-folder2.graphql.json",
+    });
+    return true;
+  }
+
+  if (
+    hasOperationName(req, "RepoTree") &&
+    hasExpression(req, "main:angular-apollo-tailwind/src/app")
+  ) {
+    aliasQuery(req, "RepoTreeFolder3");
+    req.reply({
+      fixture: "repo/repoTree-folder3.graphql.json",
     });
     return true;
   }
@@ -53,7 +90,7 @@ function handleGqlRequest(req): Boolean {
   return false;
 }
 
-describe("Repository Page Tests", () => {
+describe("When there is proper repository page responses", () => {
   beforeEach(() => {
     cy.intercept("POST", "/graphql", (req) => {
       if (handleGqlRequest(req)) {
@@ -65,6 +102,7 @@ describe("Repository Page Tests", () => {
 
     cy.visit("/thisdot/starter.dev-github-showcases");
   });
+
   it("should display correct nav header names", () => {
     cy.wait("@gqlCurrentUserQuery")
       .wait("@gqlRepoPageQuery")
@@ -101,7 +139,38 @@ describe("Repository Page Tests", () => {
       .wait("@gqlRepoPullRequestsQuery");
   });
 
-  it("should be able to navigate down and up four folders deep", () => {
-    cy.get(`[data-testid="repo issues tab"]`).click();
+  it("should be able to navigate down and up three folders deep", () => {
+    cy.get(`[data-testid="file explorer list angular-apollo-tailwind"]`)
+      .click()
+      .get(
+        `[data-testid="file explorer nav root starter.dev-github-showcases"]`
+      )
+      .should("be.visible")
+      .get(`[data-testid="file explorer nav end angular-apollo-tailwind"]`)
+      .should("be.visible")
+      .get(`[data-testid="file explorer list src"]`)
+      .click()
+      .get(
+        `[data-testid="file explorer nav root starter.dev-github-showcases"]`
+      )
+      .should("be.visible")
+      .get(`[data-testid="file explorer nav crumb angular-apollo-tailwind"]`)
+      .should("be.visible")
+      .get(`[data-testid="file explorer nav end src"]`)
+      .should("be.visible")
+      .get(`[data-testid="file explorer list app"]`)
+      .click()
+      .get(`[data-testid="file explorer nav crumb src"]`)
+      .should("be.visible")
+      .get(`[data-testid="file explorer nav end app"]`)
+      .should("be.visible")
+      .get(`[data-testid="file explorer nav crumb src"]`)
+      .click()
+      .get(`[data-testid="file explorer nav crumb angular-apollo-tailwind"]`)
+      .click()
+      .get(
+        `[data-testid="file explorer nav root starter.dev-github-showcases"]`
+      )
+      .click();
   });
 });
