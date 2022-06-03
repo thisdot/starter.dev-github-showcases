fc52487312421e4effeb114b894b0365f1e490a3
Allan N Jeremy <AllanJeremy@users.noreply.github.com>
2022-06-03 16:36:59 +0300

    Vue3 + TS + Quasar - Get user top repos (#180)
    * [Vue3 + TS + Quasar] enhancement: Added useFormatter composable
    Contains method for formatting date into a friendly time as well as a method for uppercasing the first character of a string
    
    * [Vue3 + TS + Quasar] storybook: Added fontawesome to storybook
    This means storybooks can now show fontawesome icons
    
    * [Vue3 + TS + Quasar] dependency: Added dayjs
    
    * [Vue3 + TS + Quasar] refactoring: Removed most custom css from RepoCard component
    - About 150 lines less now and it's easier to understand if you know how to use quasar rather than introducing completely new styling that won't translate elsewhere
    
    - Updated secondary color in quasar variables
    
    * [Vue3 + TS + Quasar] enhancement: Added user top repositories query and related types
    
    * [Vue3 + TS + Quasar] feature: Added useRepository composable
    This can be used to fetch user repositories from the the github GraphQL API
    
    * [Vue3 + TS + Quasar] enhancement: Added missing composables to composable aggregator
    - Github composables
    - useFormatter composable
    
    * [Vue3 + TS + Quasar] refactoring: Moved graphql setup functionality into a separate file
    
    * [Vue3 + TS + Quasar] cleanup: Removed types directory
    Types will now be located next to the areas they affect eg. github related types will be in the github/ directory under composables
    
    * [Vue3 + TS + Quasar] feature: User repositories are now dynamic on the home page
    
    * [Vue3 + TS + Quasar] styling: Made repo card styles scoped
    TODO: Refactor plain link code into reusable code (we have the same style for gists

--- vue3-ts-quasar/src/composables/index.ts
+++ vue3-ts-quasar/src/composables/index.ts
@@ -3,4 +3,6 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 export * from './auth';
+export * from './github';
+export { useFormatter } from './useFormatter';
 export { useNav } from './useNav';
