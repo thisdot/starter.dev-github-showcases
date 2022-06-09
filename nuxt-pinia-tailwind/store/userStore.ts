102c9b1380124fb1ac2f67ac1b38d1df2fb320a2
jesus4497 <jesuspadron4497@gmail.com>
2022-06-09 16:29:24 -0400

    feat: add try/catch

--- nuxt-pinia-tailwind/store/userStore.ts
+++ nuxt-pinia-tailwind/store/userStore.ts
@@ -11,15 +11,19 @@ export const useUserStore = defineStore('userStore ', {
   }),
   actions: {
     async getUserTopRepos() {
-      const {
-        $config: { GITHUB_API_URL },
-        $axios,
-      } = this.$nuxt
-      const url = `${GITHUB_API_URL}/user/repos?sort=updated&affiliation=owner,collaborator,organization_member&per_page=20`
+      try {
+        const {
+          $config: { GITHUB_API_URL },
+          $axios,
+        } = this.$nuxt
+        const url = `${GITHUB_API_URL}/user/repos?sort=updated&affiliation=owner,collaborator,organization_member&per_page=20`
 
-      const { data } = await $axios.get<IUserRepository[]>(url)
+        const { data } = await $axios.get<IUserRepository[]>(url)
 
-      this.topRepos = data
+        this.topRepos = data
+      } catch (error) {
+        return error
+      }
     },
   },
 })
