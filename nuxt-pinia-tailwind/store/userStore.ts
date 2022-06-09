e1855bec03adcf133c0d26d987a8451cc2732fe6
jesus4497 <jesuspadron4497@gmail.com>
2022-06-09 11:45:10 -0400

    feat: fetch top repos

--- nuxt-pinia-tailwind/store/userStore.ts
+++ nuxt-pinia-tailwind/store/userStore.ts
@@ -1,5 +1,25 @@
 import { defineStore } from 'pinia'
+import { IUserRepository } from '~/types/user/interfaces'
+
+interface IUserRootState {
+  topRepos: IUserRepository[]
+}
 
 export const useUserStore = defineStore('userStore ', {
-  state: () => ({}),
+  state: (): IUserRootState => ({
+    topRepos: [],
+  }),
+  actions: {
+    async getUserTopRepos() {
+      const {
+        $config: { GITHUB_API_URL },
+        $axios,
+      } = this.$nuxt
+      const url = `${GITHUB_API_URL}/user/repos?sort=updated&affiliation=owner,collaborator,organization_member&per_page=20`
+
+      const { data } = await $axios.get<IUserRepository[]>(url)
+
+      this.topRepos = data
+    },
+  },
 })
