22d453f9592115ea1d18daa2f9fd8bb02b85765b
Tyrel Chambers <tychambers3@gmail.com>
2022-06-17 14:58:28 -0400

    remove tets code

--- cra-rxjs-styled-components/src/hooks/repositories/use-repos.ts
+++ cra-rxjs-styled-components/src/hooks/repositories/use-repos.ts
@@ -15,7 +15,7 @@ import {
   extractBranchCount,
   sanitizeBranchesUrl,
 } from '../../helpers/extract-branch-count';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { REPOS_URL } from '../../constants/url.constants';
 import { fromFetchWithAuth } from '../auth/from-fetch-with-auth';
@@ -23,33 +23,6 @@ import { fromFetchWithAuth } from '../auth/from-fetch-with-auth';
 export function useRepos(): RepositoryWithBranchCount[] {
   const [state, setState] = useState<RepositoryWithBranchCount[]>([]);
   const [repoPage, setRepoPage] = useState<number>(1);
-
-  useEffect(() => {
-    const repos = [];
-
-    for (let index = 0; index < 2; index++) {
-      fromFetchWithAuth<Repository[]>(REPOS_URL(), {
-        selector: (response: Response) => {
-          return response.json();
-        },
-      })
-        .pipe(
-          filter((repos) => !!repos.length),
-          switchMap((repositories: Repository[]) => {
-            const requests = repositories.map(createBranchCountRequest);
-            return zip(...requests).pipe(
-              map(mergeRepositoriesWithBranchCount(repositories))
-            );
-          }),
-          tap((data) => {
-            repos.push(data);
-            if (data.length < 30) return;
-            setRepoPage(repoPage + 1);
-          })
-        )
-        .subscribe();
-    }
-  }, [repoPage]);
 
   useEffect(() => {
     const subscription: Subscription = fromFetchWithAuth<Repository[]>(
