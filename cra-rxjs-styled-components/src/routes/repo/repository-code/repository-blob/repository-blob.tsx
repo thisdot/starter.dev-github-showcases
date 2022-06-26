2ed9daa4ed16348773132a66d2f74684db1e2974
Ian Mungai <iansam74@gmail.com>
2022-06-27 01:01:39 +0300

    feature: navigation fix

---
+++ cra-rxjs-styled-components/src/routes/repo/repository-code/repository-blob/repository-blob.tsx
@@ -0,0 +1,20 @@
+import {
+  RepoMain,
+  RepoContainer,
+  RepoGrid,
+} from '../../../../components/layouts/RepoLayoutPage';
+
+export default function RepoBranchBlobPath() {
+
+  return (
+    <>
+      <RepoContainer>
+        <RepoGrid>
+          <RepoMain>
+
+          </RepoMain>
+        </RepoGrid>
+      </RepoContainer>
+    </>
+  );
+}
--- cra-rxjs-styled-components/src/routes/repo/repository-code/repository-tree/repository-tree.tsx
+++ cra-rxjs-styled-components/src/routes/repo/repository-code/repository-tree/repository-tree.tsx
@@ -1,16 +1,14 @@
-import RepoAbout from '../../../../components/repo-about/RepoAbout';
 import Readme from '../../../../components/readme/Readme';
 import FileExplorer from '../../../../components/file-explorer/FileExplorer';
 import {
   RepoMain,
-  RepoAside,
   RepoContainer,
   RepoGrid,
 } from '../../../../components/layouts/RepoLayoutPage';
 import { useRepo } from '../../../../context/RepoContext';
 import { useRepoExplorer } from '../../../../hooks/repo-explorer/use-repo-explorer';
 
-export default function RepoBranchRoot() {
+export default function RepoBranchTreePath() {
   const { owner, name, branch, path, basePath, data } = useRepo();
   const { files, directories } = useRepoExplorer();
 
@@ -28,13 +26,6 @@ export default function RepoBranchRoot() {
             />
             <Readme branch={branch} username={owner} repository={name} />
           </RepoMain>
-          <RepoAside>
-            <RepoAbout
-              topics={data?.topics}
-              description={data?.description}
-              homepageUrl={data?.homepageUrl}
-            />
-          </RepoAside>
         </RepoGrid>
       </RepoContainer>
     </>
