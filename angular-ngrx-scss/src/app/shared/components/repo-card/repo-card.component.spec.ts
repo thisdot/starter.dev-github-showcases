39be8ec20385c3549a063e8f48f5289f5d4662cf
Jamie Kuppens <jamie.kuppens@thisdot.co>
2022-06-29 11:56:23 -0700

    test(ngrx): fix language color tests

--- angular-ngrx-scss/src/app/shared/components/repo-card/repo-card.component.spec.ts
+++ angular-ngrx-scss/src/app/shared/components/repo-card/repo-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RelativeTimePipe } from '../../pipes/relative-time.pipe';
 
 import { RepoCardComponent } from './repo-card.component';
 
@@ -8,13 +9,23 @@ describe('RepoCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RepoCardComponent],
+      declarations: [RepoCardComponent, RelativeTimePipe],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RepoCardComponent);
     component = fixture.componentInstance;
+    component.repo = {
+      name: '',
+      stargazers_count: 0,
+      forks_count: 0,
+      private: false,
+      updated_at: '',
+      owner: {
+        login: '',
+      },
+    };
     fixture.detectChanges();
   });
 
@@ -23,17 +34,34 @@ describe('RepoCardComponent', () => {
   });
 
   it('should map known language colors', () => {
-    expect(component.getColor('JavaScript')).toBe('#f1e05a');
-    expect(component.getColor('TypeScript')).toBe('#2b7489');
-    expect(component.getColor('CSS')).toBe('#563d7c');
-    expect(component.getColor('Ruby')).toBe('#701516');
-    expect(component.getColor('Python')).toBe('#3572A5');
-    expect(component.getColor('Rust')).toBe('#dea584');
-    expect(component.getColor('C')).toBe('#555555');
+    expect(component.repo).toBeDefined();
+
+    if (component.repo) {
+      component.repo.language = 'JavaScript';
+      expect(component.languageColor).toBe('#f1e05a');
+      component.repo.language = 'TypeScript';
+      expect(component.languageColor).toBe('#2b7489');
+      component.repo.language = 'CSS';
+      expect(component.languageColor).toBe('#563d7c');
+      component.repo.language = 'Ruby';
+      expect(component.languageColor).toBe('#701516');
+      component.repo.language = 'Python';
+      expect(component.languageColor).toBe('#3572A5');
+      component.repo.language = 'Rust';
+      expect(component.languageColor).toBe('#dea584');
+      component.repo.language = 'C';
+      expect(component.languageColor).toBe('#555555');
+    }
   });
 
   it('should map unknown language colors to #000', () => {
-    expect(component.getColor('MagicScriptByApplesoft')).toBe('#000');
-    expect(component.getColor('JabaScript')).toBe('#000');
+    expect(component.repo).toBeDefined();
+
+    if (component.repo) {
+      component.repo.language = 'MagicScriptByApplesoft';
+      expect(component.languageColor).toBe('#000');
+      component.repo.language = 'JabaScript';
+      expect(component.languageColor).toBe('#000');
+    }
   });
 });
