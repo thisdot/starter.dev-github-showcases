debe4e76429d2d4f25d5a89b7690cf159afd2fcc
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-07-01 13:58:25 +0100

    prettier format

--- cra-rxjs-styled-components/src/components/icons/LinkIcon.tsx
+++ cra-rxjs-styled-components/src/components/icons/LinkIcon.tsx
@@ -1,7 +1,7 @@
-export const LinkIcon = ({ className }: { className?: string; }) => {
+export const LinkIcon = ({ className }: { className?: string }) => {
   return (
     <svg
-    className={className}
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
