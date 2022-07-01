7b4a6bc048897637d51c611c3f3e1448c300d6b6
tyrelchambers <tychambers3@gmail.com>
2022-07-01 10:12:21 -0400

    [CRA-RXJS-SC] Get all repos and paginate them (#383)
    * modify top repos url
    
    * remove tets code
    
    * add pagination
    
    * add hasprevpage and hasnextpage
    
    * paginate user repos
    
    * style pagination buttons
    
    * fix sort bug
    
    * cleanup unused import

--- cra-rxjs-styled-components/src/constants/url.constants.ts
+++ cra-rxjs-styled-components/src/constants/url.constants.ts
@@ -17,16 +17,17 @@ export const SIGN_IN_URL = (() => {
 export const SIGN_OUT_URL = `${API_URL_BASE}/auth/signout`;
 export const GET_TOKEN_URL = `${API_URL_BASE}/auth/token`;
 
-export const TOP_REPOS_URL = `${GITHUB_URL_BASE}/user/repos?sort=updated&affiliation=owner,collaborator,organization_member&per_page=20`;
+export const REPOS_URL = (page: string = '1') =>
+  `${GITHUB_URL_BASE}/user/repos?sort=pushed&affiliation=owner,collaborator&page=${page}`;
 
 export const SINGLE_USER_REPO = (user: string, repo: string) =>
   `${GITHUB_URL_BASE}/repos/${user}/${repo}`;
 
 export const ORG_REPO_LIST = (user: string) =>
-  `${GITHUB_URL_BASE}/orgs/${user}/repos?sort=updated&per_page=10`;
+  `${GITHUB_URL_BASE}/orgs/${user}/repos?sort=pushed&per_page=10`;
 
-export const USER_REPO_LIST = (user: string) =>
-  `${GITHUB_URL_BASE}/users/${user}/repos?sort=updated&per_page=10`;
+export const USER_REPO_LIST = (user: string, page: string = '1') =>
+  `${GITHUB_URL_BASE}/users/${user}/repos?sort=pushed&page=${page}&type=all`;
 
 export const GISTS_URL = (user: string) =>
   `${GITHUB_URL_BASE}/users/${user}/gists?per_page=10`;
