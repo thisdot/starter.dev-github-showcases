514d9960b4aefadc3eedc31c90f25b5a6e4a3945
jenn <90362911+jnalei@users.noreply.github.com>
2022-07-07 11:26:12 -1000

    feat(#421): adding empty list test coverage for profile page

--- starter-dev-e2e/cypress/e2e/profile.cy.ts
+++ starter-dev-e2e/cypress/e2e/profile.cy.ts
@@ -1,7 +1,7 @@
 import { View } from "../utils/view";
 import { InterceptResponse } from "../utils/intercept-response";
 
-describe("Profile Page", () => {
+describe("When there is proper profile page responses", () => {
   beforeEach(() => {
     cy.interceptGraphQLCalls(View.Profile, InterceptResponse.Full);
     cy.interceptRestCalls(View.Profile, InterceptResponse.Full);
@@ -33,8 +33,7 @@ describe("Profile Page", () => {
   });
 
   it("should display orgs in profile", () => {
-    cy.get(`[data-testid="profile page orgs"]`)
-    .should("have.length", 1);
+    cy.get(`[data-testid="profile page orgs"]`).should("have.length", 1);
   });
 
   it("should display correct count", () => {
@@ -128,5 +127,97 @@ describe("Profile Page", () => {
       .wait("@RepoReadMeQuery")
       .get(`[data-testid="readme"]`)
       .should("be.visible");
+  });
+});
+
+describe("When there is proper empty profile page responses", () => {
+  beforeEach(() => {
+    cy.interceptGraphQLCalls(View.Profile, InterceptResponse.Empty);
+    cy.interceptRestCalls(View.Profile, InterceptResponse.Empty);
+
+    cy.visit("/thisdot");
+  });
+
+  it("should display correct name and username with empty information", () => {
+    cy.get(`[data-testid="profile page name"]`)
+      .should("be.visible")
+      .and("contain.text", "thisdotlabs")
+      .get(`[data-testid="profile page username"]`)
+      .should("be.visible")
+      .and("contain.text", "thisdot")
+      .get(`[data-testid="profile followers count"]`)
+      .contains("0")
+      .get(`[data-testid="profile following count"]`)
+      .contains("0")
+      .get(`[data-testid="profile starred count"]`)
+      .contains("0");
+  });
+
+  it("should have no organizations listed in profile", () => {
+    cy.get(`[data-testid="profile page orgs"]`).should("not.exist");
+  });
+
+  it("should not display any repos or be able to paginate previous and next ", () => {
+    cy.wait("@UserReposQuery")
+      .get(`[data-testid="profile repo list heading"]`)
+      .should("have.length", 0)
+      // next 4 lines does fails on angular-apollo due to disabled buttons
+      .get(`[data-testid="pagination button previous"]`)
+      .should("not.exist")
+      .get(`[data-testid="pagination button next"]`)
+      .should("not.exist");
+  });
+
+  it("should not display any repository when using the search bar", () => {
+    cy.get("input#search")
+      .type("sentry{enter}")
+      .get(`[data-testid="repository list item"`)
+      .should("have.length", 0);
+  });
+
+  it("should have working sort options, type, and language selection with no results", () => {
+    cy.wait("@CurrentUserQuery")
+      .wait("@UserReposQuery")
+      .wait("@UserProfileQuery")
+      .get(`[data-testid="filters dropdown Type"]`)
+      .click()
+      .get(`[data-testid="repository list item"`)
+      .should("not.exist")
+      .get(`[data-testid="filters dropdown Type items"]`)
+      .should("be.visible")
+      .get(`[data-testid="filters dropdown item All"]`)
+      .should("exist")
+      .and("be.visible")
+      .get(`[data-testid="filters dropdown item Forks"]`)
+      .should("exist")
+      .and("be.visible")
+      .get(`[data-testid="filters dropdown item Archived"]`)
+      .should("exist")
+      .and("be.visible")
+      .get(`[data-testid="filters dropdown Language"]`)
+      .click()
+      .get(`[data-testid="repository list item"`)
+      .should("not.exist")
+      .get(`[data-testid="filters dropdown Language items"]`)
+      .should("be.visible")
+      .get(`[data-testid="filters dropdown item All"]`)
+      .should("exist")
+      .and("be.visible")
+      .get(`[data-testid="filters dropdown Sort"]`)
+      .click()
+      .get(`[data-testid="repository list item"`)
+      .should("not.exist")
+      .get(`[data-testid="filters dropdown Sort items"]`)
+      .should("be.visible")
+      .get(`[data-testid="filters dropdown item Last updated"]`)
+      .should("exist")
+      .and("be.visible")
+      .get(`[data-testid="filters dropdown item Name"]`)
+      .should("exist")
+      .and("be.visible")
+      .and("contain.text", "Name")
+      .get(`[data-testid="filters dropdown item Stars"]`)
+      .should("exist")
+      .and("be.visible");
   });
 });
