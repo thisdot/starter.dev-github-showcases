d39b98c3bc348389e9cedfa3b2108fb909eb2b94
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-11 09:12:13 +0100

    Vue3 apollo quasar/feat/single repo pull request tab view (#448)
    * in  progress
    
    * in  progress
    
    * in progress
    
    * in progress
    
    * fix: quasar custom icon
    
    * in progress
    
    * fix: removed unwanted svg
    
    * chore: storybook
    
    * chore: UI design completed, upnext is writing test for every component
    
    * chore: cleanups before writing tests
    
    * test:unit test in view
    
    * test: completed test
    
    * fix: made fix on comment

---
+++ vue3-apollo-quasar/src/components/PaginationButtons/PaginationButtons.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="row justify-center items-center q-my-md">
+    <button
+      class="row justify-center items-center btn-fixed-width text-caption pagination_btn text-capitalize bg-transparent no-border"
+      :disabled="!isPrevActive"
+      @click="prev()"
+    >
+      <q-icon class="fa fa-chevron-left q-mr-xs" />
+      <span>{{ PAGINATIONS.PREV }}</span>
+    </button>
+    <button
+      class="row justify-center items-center btn-fixed-width text-caption pagination_btn text-capitalize bg-transparent no-border"
+      :disabled="!isNextActive"
+      @click="next()"
+    >
+      <span>{{ PAGINATIONS.NEXT }}</span>
+      <q-icon class="fa fa-chevron-right q-ml-xs" />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineProps, defineEmits } from 'vue';
+export default defineComponent({
+  name: 'PaginationButtons',
+});
+</script>
+
+<script lang="ts" setup>
+import { PAGINATION_EVENT, PAGINATIONS } from './data';
+
+defineProps({
+  isPrevActive: {
+    type: Boolean,
+    required: true,
+  },
+  isNextActive: {
+    type: Boolean,
+    required: true,
+  },
+});
+const emit = defineEmits([PAGINATION_EVENT]);
+
+const prev = () => emit(PAGINATION_EVENT, PAGINATIONS.PREV);
+const next = () => emit(PAGINATION_EVENT, PAGINATIONS.NEXT);
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/quasar.variables.scss';
+.pagination_btn {
+  cursor: pointer;
+  color: $primary;
+  &:disabled {
+    color: $secondary-200;
+  }
+
+  .q-icon {
+    font-size: 0.6rem;
+  }
+}
+</style>
