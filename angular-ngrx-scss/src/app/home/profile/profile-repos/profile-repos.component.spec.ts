8bfc683950b32d0928a91a9164558503d59161b7
SDaian <scuarissid@gmail.com>
2022-07-14 15:03:11 +0200

    Fix broken test

--- angular-ngrx-scss/src/app/home/profile/profile-repos/profile-repos.component.spec.ts
+++ angular-ngrx-scss/src/app/home/profile/profile-repos/profile-repos.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ProfileReposComponent } from './profile-repos.component';
 
@@ -9,6 +10,7 @@ describe('ProfileReposComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProfileReposComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
   });
 
