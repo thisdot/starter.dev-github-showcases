bf0239db5dde1b9e8a620dd5d99a1b331ca84ccd
Daian Scuarissi <scuarissid@gmail.com>
2022-07-14 20:40:59 +0200

    [angular-ngrx-scss] - Make profile components shareable (#451)
    * [angular-ngrx-scss] - Make profile components shareable

--- angular-ngrx-scss/src/app/home/home.module.ts
+++ angular-ngrx-scss/src/app/home/home.module.ts
@@ -7,13 +7,14 @@ import { OcticonsDirective } from '../shared/directives/octicons.directive';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileNavComponent } from './profile/profile-nav/profile-nav.component';
 import { ProfileAboutComponent } from './profile/profile-about/profile-about.component';
-import { ProfileReposComponent } from './profile/profile-repos/profile-repos.component';
+import { RepoListComponent } from '../shared/components/repo-list/repo-list.component';
 import { RelativeTimePipe } from '../shared/pipes/relative-time.pipe';
 import { RepoCardComponent } from '../shared/components/repo-card/repo-card.component';
 import { UserGistsComponent } from './user-gists/user-gists.component';
 import { TopRepositoriesComponent } from './top-repositories/top-repositories.component';
 import { RepoControlsComponent } from '../shared/components/repo-controls/repo-controls.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserProfileComponent } from './profile/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +24,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProfileComponent,
     ProfileNavComponent,
     ProfileAboutComponent,
-    ProfileReposComponent,
+    RepoListComponent,
     RepoCardComponent,
     RelativeTimePipe,
     UserGistsComponent,
     TopRepositoriesComponent,
     RepoControlsComponent,
+    UserProfileComponent,
   ],
   imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule],
 })
--- angular-ngrx-scss/src/app/home/profile/user-profile/user-profile.component.ts
+++ angular-ngrx-scss/src/app/home/profile/user-profile/user-profile.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
 import { selectProfileState } from 'src/app/state/profile/profile.selectors';
 
 @Component({
-  selector: 'app-profile-about',
-  templateUrl: './profile-about.component.html',
-  styleUrls: ['./profile-about.component.scss'],
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss'],
 })
-export class ProfileAboutComponent {
+export class UserProfileComponent {
   profile$ = this.store.select(selectProfileState);
+  repos$ = this.profile$.pipe(map((profile) => profile.repos ?? []));
 
   constructor(private store: Store) {}
 }
