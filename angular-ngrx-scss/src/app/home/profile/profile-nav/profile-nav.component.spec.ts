3338eb8b834d8e80e9fac49a4d51f82212a2de9d
SDaian <scuarissid@gmail.com>
2022-07-14 19:12:08 +0200

    Update class name

--- angular-ngrx-scss/src/app/home/profile/profile-nav/profile-nav.component.spec.ts
+++ angular-ngrx-scss/src/app/home/profile/profile-nav/profile-nav.component.spec.ts
@@ -2,22 +2,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 
-import { TabNavComponent } from './profile-nav.component';
+import { ProfileNavComponent } from './profile-nav.component';
 
 describe('TabNavComponent', () => {
-  let component: TabNavComponent;
-  let fixture: ComponentFixture<TabNavComponent>;
+  let component: ProfileNavComponent;
+  let fixture: ComponentFixture<ProfileNavComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [TabNavComponent],
+      declarations: [ProfileNavComponent],
       providers: [provideMockStore()],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TabNavComponent);
+    fixture = TestBed.createComponent(ProfileNavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
