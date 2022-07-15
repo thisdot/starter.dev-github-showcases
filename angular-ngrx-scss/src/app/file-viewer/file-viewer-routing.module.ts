de4939797eca2e93e9e641c6482b8b34237ee260
asincole <asincole@gmail.com>
2022-07-15 21:36:07 +0200

    feat(angular-ngrx-scss): update routing to handle repo file navigation
    Refs: #204

--- angular-ngrx-scss/src/app/file-viewer/file-viewer-routing.module.ts
+++ angular-ngrx-scss/src/app/file-viewer/file-viewer-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 import { FileExplorerRootComponent } from './file-explorer-root/file-explorer-root.component';
+import { FileExplorerBlobComponent } from './file-explorer-blob/file-explorer-blob.component';
+
+const pathMatcher = (url: UrlSegment[]) => {
+  const path = url.map((segment) => segment.path).join('/');
+  if (url.length && new RegExp(/^[.\w-]+(\/[.\w-]+)*$/gm).test(path)) {
+    return {
+      consumed: url,
+      posParams: {
+        path: new UrlSegment(path, {}),
+      },
+    };
+  }
+
+  return null;
+};
 
 const routes: Routes = [
   {
     path: '',
     component: FileExplorerRootComponent,
+  },
+  {
+    path: 'blob/:branch',
+    children: [
+      {
+        matcher: pathMatcher,
+        component: FileExplorerBlobComponent,
+      },
+    ],
   },
 ];
 
