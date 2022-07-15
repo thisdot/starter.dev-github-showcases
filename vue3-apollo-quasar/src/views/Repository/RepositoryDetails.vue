621434ad6f047cad29daa5a236c5de176d84bf38
Allan Jeremy <aj.dev254@gmail.com>
2022-07-15 22:04:54 +0300

    build: added repository details rough pass

--- vue3-apollo-quasar/src/views/Repository/RepositoryDetails.vue
+++ vue3-apollo-quasar/src/views/Repository/RepositoryDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <q-page>
     <!-- TODO: Instead of using currentRepo?.data.isPrivate to determine visibility tag, use the actual visibility tag from GitHub (there's more than just "Public" & "Private". We also have "Internal" and potentially others)  -->
     <RepoSubHeader
       :username="currentRepo.owner"
@@ -11,11 +11,21 @@
       :issuesCount="currentRepo?.data.openIssueCount"
       :pullRequestsCount="currentRepo?.data.openPullRequestCount"
     />
-  </div>
+
+    <section class="q-mx-auto q-my-xl" style="max-width: 60rem">
+      <q-card flat bordered>
+        <!-- <q-card-section> -->
+        <FileExplorer :content-list="fileTree" />
+        <!-- </q-card-section> -->
+      </q-card>
+    </section>
+
+    <pre>{{ tree }}</pre>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageRepositoryDetails',
@@ -23,18 +33,37 @@ export default defineComponent({
 </script>
 
 <script lang="ts" setup>
-import { RepoSubHeader } from '@/components';
+import { ExplorerContent } from '@/components/FileExplorer/FileExplorerNav.vue';
+import { FileExplorer, RepoSubHeader } from '@/components';
 import { useRoute } from 'vue-router';
 import { useRepoPage, useRepoTree } from '@/composables';
 const $route = useRoute();
 
 //? This structure is defined in the route for this 👇 in routes/index.ts
-const routeParams = $route.params as { owner: string; repo: string };
+const { owner, repo } = $route.params as { owner: string; repo: string };
 
 const { getRepoPage } = useRepoPage();
 
 const { context: currentRepo, loading } = getRepoPage({
-  name: routeParams.repo,
-  owner: routeParams.owner,
+  name: repo,
+  owner,
 });
+
+const { getRepoTree } = useRepoTree();
+const { data: tree } = getRepoTree({
+  owner,
+  name: repo,
+  branch: 'main',
+  path: '',
+});
+
+const fileTree = computed(() =>
+  tree.value.map((treeBranch) => ({
+    isDirectory: treeBranch.type === 'tree',
+    name: treeBranch.name,
+    latestCommitMessage: 'Test commit', //TODO: Get this
+    lastUpdated: 'Jul 15 2022', //TODO: Get this
+    to: `${repo}/${treeBranch.path}`,
+  })),
+);
 </script>
