02957b3d8b77364543b0e5fcbbb41a83411b831f
asincole <asincole@gmail.com>
2022-07-15 21:29:10 +0200

    feat(angular-ngrx-scss): create file explorer component
    Refs: #204

---
+++ angular-ngrx-scss/src/app/file-viewer/file-explorer-blob/file-explorer-blob.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FileExplorerBlobComponent } from './file-explorer-blob.component';
+
+describe('FileExplorerBlobComponent', () => {
+  let component: FileExplorerBlobComponent;
+  let fixture: ComponentFixture<FileExplorerBlobComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FileExplorerBlobComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FileExplorerBlobComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
