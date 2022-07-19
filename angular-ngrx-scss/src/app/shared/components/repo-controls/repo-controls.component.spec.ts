667c616bca1eb6da383abbb61f6f691b45b0d7e2
SDaian <scuarissid@gmail.com>
2022-07-19 14:28:29 +0200

    Fix broken tests

--- angular-ngrx-scss/src/app/shared/components/repo-controls/repo-controls.component.spec.ts
+++ angular-ngrx-scss/src/app/shared/components/repo-controls/repo-controls.component.spec.ts
@@ -7,6 +7,7 @@ import {
   selectFilterByLanguage,
   selectFilterBySearch,
   selectFilterByType,
+  selectRepos,
   selectReposCount,
 } from 'src/app/state/profile/profile.selectors';
 import { TypeFilter } from 'src/app/state/profile/profile.state';
@@ -50,6 +51,10 @@ describe('RepoControlsComponent', () => {
             {
               selector: hasActiveSortAndFilters,
               value: MOCK_VALUE_SELECT_HAS_ACTIVE_FILTERS,
+            },
+            {
+              selector: selectRepos,
+              value: [],
             },
           ],
         }),
