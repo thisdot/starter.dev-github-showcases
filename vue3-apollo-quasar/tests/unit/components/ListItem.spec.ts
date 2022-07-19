3b0ac5a8b2f7f8e3528bbf9bee72ee2e071ed5fa
Jerry Hogan <jerry.hogan@thisdot.co>
2022-07-19 10:19:12 +0100

    test: completed test corrections

--- vue3-apollo-quasar/tests/unit/components/ListItem.spec.ts
+++ vue3-apollo-quasar/tests/unit/components/ListItem.spec.ts
@@ -2,9 +2,8 @@ import { mount } from '@vue/test-utils';
 import ListItem from '@/components/RepoSubHeader/ListItem.vue';
 
 describe('ListItem', () => {
-  const wrapper = mount(ListItem);
-
   it('should mount', () => {
-    expect(wrapper.vm).toBeTruthy();
+    const wrapper = mount(ListItem);
+    expect(wrapper.exists()).toBeTruthy();
   });
 });
