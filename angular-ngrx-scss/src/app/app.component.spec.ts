1ef0a6bceddb441e09b205a2f1fc78d650938c9c
SDaian <scuarissid@gmail.com>
2022-07-27 14:47:45 +0200

    First approach to hooks

--- angular-ngrx-scss/src/app/app.component.spec.ts
+++ angular-ngrx-scss/src/app/app.component.spec.ts
@@ -15,6 +15,6 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeFalsy();
   });
 });
