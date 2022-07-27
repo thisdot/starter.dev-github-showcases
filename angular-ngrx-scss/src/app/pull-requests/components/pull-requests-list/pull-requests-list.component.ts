ce958edb24f701385c1948985d8bc1660fd85f9b
SDaian <scuarissid@gmail.com>
2022-07-27 13:55:38 +0200

    [angular-ngrx-scss] - Create single repo pull request tab-view

--- angular-ngrx-scss/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
+++ angular-ngrx-scss/src/app/pull-requests/components/pull-requests-list/pull-requests-list.component.ts
@@ -7,22 +7,4 @@ import { PullRequestAPIResponse } from '../../../state/repository';
   styleUrls: ['./pull-requests-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PullRequestsListComponent {
-  @Input() pullRequests: (PullRequestAPIResponse | undefined)[] | null = [];
-  readonly baseDate = new Date();
-
-  colorMap(color: string): string {
-    switch (color) {
-      case 'D4C5F9': // violet
-        return '#ddd6fe';
-      case '310D85': // dark purple
-        return '#c084fc';
-      case '6ED842': // bright yellow green
-        return '#bef264';
-      case 'ED1DB5': // hot pink
-        return '#f9a8d4';
-      default:
-        return `#${color}`;
-    }
-  }
-}
+export class PullRequestsListComponent {}
--- angular-ngrx-scss/src/app/pull-requests/pull-requests.module.ts
+++ angular-ngrx-scss/src/app/pull-requests/pull-requests.module.ts
@@ -5,9 +5,14 @@ import { PullRequestsRoutingModule } from './pull-requests-routing.module';
 import { PullRequestsComponent } from './pull-requests.component';
 import { PullRequestsListComponent } from './components/pull-requests-list/pull-requests-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { PullRequestsHeaderComponent } from './components/pull-requests-header/pull-requests-header.component';
 
 @NgModule({
-  declarations: [PullRequestsComponent, PullRequestsListComponent],
+  declarations: [
+    PullRequestsComponent,
+    PullRequestsListComponent,
+    PullRequestsHeaderComponent,
+  ],
   imports: [CommonModule, PullRequestsRoutingModule, SharedModule],
 })
 export class PullRequestsModule {}
