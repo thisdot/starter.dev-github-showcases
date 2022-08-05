e39ea48005435a347fe968f07de6e5ff3500107b
Daian Scuarissi <scuarissid@gmail.com>
2022-08-05 18:42:53 +0200

    Fix tests

--- angular-ngrx-scss/src/app/repository/components/repository-details/repository-details.component.spec.ts
+++ angular-ngrx-scss/src/app/repository/components/repository-details/repository-details.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RepositoryDetailsComponent } from './repository-details.component';
 
@@ -8,6 +9,7 @@ describe('RepositoryDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [RepositoryDetailsComponent],
     }).compileComponents();
   });
