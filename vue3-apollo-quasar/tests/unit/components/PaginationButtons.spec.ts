4faebdf80c57d1b7ee29067a078506eda708a3c9
Jerry Hogan <jerry.hogan@thisdot.co>
2022-08-12 08:41:43 +0100

    Vue3 apollo quasar/test/pagination buttons component (#487)
    * in progress
    
    * test: pagination test completed
    
    * test: pagination test completed and but re-structured
    
    * added non fuctional yet quasar pagination
    
    * removed unwanted props
    
    * removed package-lock file

--- vue3-apollo-quasar/tests/unit/components/PaginationButtons.spec.ts
+++ vue3-apollo-quasar/tests/unit/components/PaginationButtons.spec.ts
@@ -1,33 +1,11 @@
 import { mount } from '@vue/test-utils';
 import { PaginationButtons } from '@/components';
 
-describe('PaginationButtons', () => {
-  const wrapper = mount(PaginationButtons, {
-    props: {
-      isPrevActive: false,
-      isNextActive: true,
-    },
-  });
-
-  const pagination_btns = wrapper.findAll('.pagination_btn');
+describe('PaginationButtons mounting', () => {
   it('should mount', () => {
-    expect(wrapper.vm).toBeTruthy();
+    const wrapper = mount(PaginationButtons);
+    const pagination = wrapper.find('.pagination');
+    expect(wrapper.exists()).toBeTruthy();
+    expect(pagination.exists()).toBeTruthy();
   });
-
-  let count = 0;
-  it.each(pagination_btns)(
-    'should be either clickable or not pending the diabled state',
-    async (btn) => {
-      const prev = (wrapper.vm.prev = jest.fn());
-      const next = (wrapper.vm.next = jest.fn());
-      await btn.trigger('click');
-      if (count === 0) {
-        expect(prev).toBeCalledTimes(0);
-      } else {
-        expect(next).toBeCalledTimes(1);
-      }
-      count++;
-    },
-  );
-  count = 0;
 });
