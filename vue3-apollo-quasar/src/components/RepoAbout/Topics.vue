2e9ff5c3c8645bd688be13449805c19d4733808e
William Hutt <whutt1994@gmail.com>
2022-08-12 00:40:58 -0700

    [Vue3 Apollo Quasar] Adds single repo about sub component (#460)
    * feat: wip single repo about
    
    * refactor: style tweaks and story setup
    
    * refactor: style clean up
    
    * refactor: clean up leftover comment
    
    * comment addressed
    
    * removed package-lock file
    
    Co-authored-by: Jerry Hogan <jerry.hogan@thisdot.co>

---
+++ vue3-apollo-quasar/src/components/RepoAbout/Topics.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="topics" class="q-my-lg row items-center">
+    <span
+      v-for="topic in topics"
+      :key="topic"
+      class="topic q-mb-xs q-mr-xs q-px-md q-py-xs text-caption text-no-wrap text-weight-medium cursor-pointer"
+    >
+      {{ topic }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Topics',
+  props: {
+    topics: Array,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/quasar.variables.scss';
+.topic {
+  background-color: $primary-200;
+  color: $primary;
+  border-radius: 2rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: #fff;
+    background-color: $primary;
+  }
+}
+</style>
