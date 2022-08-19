6ea3fbb1fb4200f287a1701174f0af96c5f603ff
Mark Shenouda <mark@thisdot.co>
2022-08-19 21:30:21 +0200

    Audit app for any architectural improvements

--- cra-rxjs-styled-components/src/components/pull-request/PullRequest/PullRequest.data.tsx
+++ cra-rxjs-styled-components/src/components/pull-request/PullRequest/PullRequest.data.tsx
@@ -7,24 +7,25 @@ import {
   tap,
   zip,
 } from 'rxjs';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import type { PRTabValues } from '../types';
 import { PULLS_URL } from '../../../constants/url.constants';
-import type { PullRequest } from './PullRequest.type';
+import type { PullRequests } from './PullRequest.type';
 import PullRequestView from './PullRequest.view';
 import { fromFetchWithAuth } from '../../../hooks/auth/from-fetch-with-auth';
 import { useParams } from 'react-router-dom';
 
 export default function PullRequestCtrl() {
   const [activeTab, setActiveTab] = useState<PRTabValues>('open');
-  const [pullRequests, setPullRequests] = useState<PullRequest[]>([]);
+  const [openPullRequests, setOpenPullRequests] = useState<PullRequests>();
+  const [closedPullRequests, setClosedPullRequests] = useState<PullRequests>();
   const { username, repo } = useParams();
 
   useEffect(() => {
     if (username && repo) {
-      const subscription: Subscription = fromFetchWithAuth<PullRequest[]>(
-        PULLS_URL(username, repo),
+      const openPRsSubscription: Subscription = fromFetchWithAuth<PullRequests>(
+        PULLS_URL(username, repo, 'open'),
         {
           selector: (response: Response) => {
             return response.json();
@@ -32,6 +33,27 @@ export default function PullRequestCtrl() {
         }
       )
         .pipe(
+          tap((val) => console.log(val)),
+          filter((pulls) => !!pulls.items.length),
+          switchMap((pulls: PullRequests) => {
+            const requests = pulls.map(createCommentCountRequest);
+            return zip(...requests).pipe(
+              map(mergePullRequestsWithCommentCount(pulls))
+            );
+          }),
+          tap(setOpenPullRequests)
+        )
+        .subscribe();
+
+      const closedPRsSubscription: Subscription = fromFetchWithAuth<
+        PullRequest[]
+      >(PULLS_URL(username, repo, 'closed'), {
+        selector: (response: Response) => {
+          return response.json();
+        },
+      })
+        .pipe(
+          tap((val) => console.log(val)),
           filter((pulls) => !!pulls.length),
           switchMap((pulls: PullRequest[]) => {
             const requests = pulls.map(createCommentCountRequest);
@@ -39,28 +61,26 @@ export default function PullRequestCtrl() {
               map(mergePullRequestsWithCommentCount(pulls))
             );
           }),
-          tap(setPullRequests)
+          tap(setClosedPullRequests)
         )
         .subscribe();
 
       return () => {
-        subscription.unsubscribe();
+        openPRsSubscription.unsubscribe();
+        closedPRsSubscription.unsubscribe();
       };
     }
   }, [username, repo]);
 
-  const OPEN_PRS: PullRequest[] = pullRequests?.filter(
-    (pr) => pr.state === 'open'
-  );
-  const CLOSED_PRS: PullRequest[] = pullRequests?.filter(
-    (pr) => pr.state === 'closed'
-  );
-
   return (
     <PullRequestView
-      pullRequests={activeTab === 'open' ? OPEN_PRS : CLOSED_PRS}
-      openPRCount={OPEN_PRS?.length}
-      closedPRCount={CLOSED_PRS?.length}
+      pullRequests={
+        activeTab === 'open'
+          ? openPullRequests?.items
+          : closedPullRequests?.items
+      }
+      openPRCount={openPullRequests?.total_count}
+      closedPRCount={closedPullRequests?.total_count}
       changeActiveTab={setActiveTab}
     />
   );
