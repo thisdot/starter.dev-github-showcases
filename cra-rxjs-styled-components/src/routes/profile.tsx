732cb1fda729bbf8c45b224809fbb9e6985d9727
Mark Shenouda <mark@thisdot.co>
2022-08-26 19:57:11 +0200

    fix: profile page rendering

---
+++ cra-rxjs-styled-components/src/routes/profile.tsx
@@ -0,0 +1,16 @@
+import UserProfileView from '../components/user-profile';
+import { Layout } from '../components/layouts/ProfileLayout';
+import Header from '../components/header';
+
+function Profile() {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <UserProfileView />
+      </Layout>
+    </>
+  );
+}
+
+export default Profile;
