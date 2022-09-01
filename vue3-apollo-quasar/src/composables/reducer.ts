cfd0fe082e97385b7d15c2be120a2a0a7b1c60b7
Jerry Hogan <jerry.hogan@thisdot.co>
2022-09-01 15:15:33 +0100

    added return types where required, still missing userReducer return type

--- vue3-apollo-quasar/src/composables/reducer.ts
+++ vue3-apollo-quasar/src/composables/reducer.ts
@@ -1,11 +1,6 @@
 import { reactive } from 'vue';
 
-interface UseReducer {
-  state: boolean;
-  dispatch: (data: unknown) => unknown;
-}
-
-export const useReducer = (reducer, iState): UseReducer[] => {
+export const useReducer = (reducer, iState) => {
   const state = reactive(iState);
   const dispatch = (action) => {
     reducer(state, action);
