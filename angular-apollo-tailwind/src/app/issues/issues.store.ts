da4c2b01424ab655714d0d0e97164f6d511ef132
Brett Zeidler <brettzeidler93@gmail.com>
2022-09-02 18:28:15 -0500

    fix: remove unused location import

--- angular-apollo-tailwind/src/app/issues/issues.store.ts
+++ angular-apollo-tailwind/src/app/issues/issues.store.ts
@@ -1,4 +1,3 @@
-import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
