af852510254af197fee03201e449543c44efda74
WillHutt <whutt1994@gmail.com>
2022-09-02 12:15:39 -0700

    freat: adds details dropdown story

--- cra-rxjs-styled-components/src/components/DetailsDropdown/DetailsDropdown.stories.tsx
+++ cra-rxjs-styled-components/src/components/DetailsDropdown/DetailsDropdown.stories.tsx
@@ -1,52 +1,30 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import DetailsDropdown from './DetailsDropdown';
-// import { useCallback, useState } from 'react';
-// import { DropdownTitle } from '../pull-request/types';
-// import { Link } from 'react-router-dom';
+import { useArgs } from '@storybook/client-api';
 
 export default {
   title: 'DetailsDropdown',
   component: DetailsDropdown,
+  args: {
+    title: 'Milestones',
+    DropdownLabel: 'Filter by Milestones',
+    isOpen: false,
+    toggleDropDown: () => {},
+  },
 } as ComponentMeta<typeof DetailsDropdown>;
 
-let openDropdown = '';
-const toggleDropdown = (dropdownName: string) => {
-  return openDropdown === dropdownName ? undefined : dropdownName;
-};
-
-// function MockArgs() {
-//   const [openDropdown, setOpenDropdown] = useState<DropdownTitle>();
-//   const toggleDropdown = useCallback(
-//     (dropdownName: DropdownTitle) => {
-//       setOpenDropdown((prevValue) =>
-//         prevValue === dropdownName ? undefined : dropdownName
-//       );
-//     },
-//     [openDropdown]
-//   );
-
-//   return { openDropdown, toggleDropdown };
-// }
-
-// const { openDropdown, toggleDropdown } = MockArgs();
-
-const Template: ComponentStory<typeof DetailsDropdown> = (args) => (
-  <DetailsDropdown {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  title: 'Milestones',
-  DropdownLabel: 'Filter by Milestones',
-  isOpen: openDropdown === 'Milestones',
-  toggleDropDown: () => toggleDropdown('Milestones'),
-  children: (
-    <>
-      {/* <Link to="/"> Issues with no milestones </Link>
-      <Link to="/"> Serverless Migrations </Link> */}
-      {/* <>
-      Issues with no milestones <br /> Serverless Migrations
-    </> */}
-    </>
-  ),
+export const Default: ComponentStory<typeof DetailsDropdown> = (args) => {
+  const [{ isOpen }, updateArgs] = useArgs();
+  const handleClose = () => updateArgs({ isOpen: !isOpen });
+  return (
+    <DetailsDropdown
+      title={args.title}
+      DropdownLabel={args.DropdownLabel}
+      isOpen={args.isOpen}
+      toggleDropDown={handleClose}
+    >
+      <a href="">Issues with no milestones</a>
+      <a href="">Serverless Migrations</a>
+    </DetailsDropdown>
+  );
 };
