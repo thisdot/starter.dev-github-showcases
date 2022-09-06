77e4245108b59054a512a88bb403eec07183d42e
Mark Shenouda <mark@thisdot.co>
2022-09-06 14:32:36 +0200

    added icon names

--- cra-rxjs-styled-components/src/components/icons/icons.stories.tsx
+++ cra-rxjs-styled-components/src/components/icons/icons.stories.tsx
@@ -23,6 +23,27 @@ import {
 	TwitterIcon,
 } from '.';
 
+const ICONS = [
+	{ name: 'BookIcon', component: BookIcon },
+	{ name: 'ClosedIssueIcon', component: ClosedIssueIcon },
+	{ name: 'ClosedPRIcon', component: ClosedPRIcon },
+	{ name: 'CloseIcon', component: CloseIcon },
+	{ name: 'CodeIcon', component: CodeIcon },
+	{ name: 'CorrectIcon', component: CorrectIcon },
+	{ name: 'DirectoryIcon', component: DirectoryIcon },
+	{ name: 'DropdownIcon', component: DropdownIcon },
+	{ name: 'EyeIcon', component: EyeIcon },
+	{ name: 'FileIcon', component: FileIcon },
+	{ name: 'ForkIcon', component: ForkIcon },
+	{ name: 'IssuesIcon', component: IssuesIcon },
+	{ name: 'LinkIcon', component: LinkIcon },
+	{ name: 'PrIcon', component: PrIcon },
+	{ name: 'ReadmeBookIcon', component: ReadmeBookIcon },
+	{ name: 'ReadmeListIcon', component: ReadmeListIcon },
+	{ name: 'StarIcon', component: StarIcon },
+	{ name: 'TwitterIcon', component: TwitterIcon },
+];
+
 const IconContainerStyled = styled.div`
 	display: flex;
 	flex-wrap: wrap;
@@ -32,29 +53,24 @@ const IconContainerStyled = styled.div`
 		width: 50px;
 		height: 50px;
 	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 10px;
+	}
 `;
 
 const IconContainer = () => {
 	return (
 		<IconContainerStyled>
-			<BookIcon />
-			<ClosedIssueIcon />
-			<ClosedPRIcon />
-			<CloseIcon />
-			<CodeIcon />
-			<CorrectIcon />
-			<DirectoryIcon />
-			<DropdownIcon />
-			<EyeIcon />
-			<FileIcon />
-			<ForkIcon />
-			<IssuesIcon />
-			<LinkIcon />
-			<PrIcon />
-			<ReadmeBookIcon />
-			<ReadmeListIcon />
-			<StarIcon />
-			<TwitterIcon />
+			{ICONS.map((icon) => (
+				<div>
+					<icon.component key={icon.name} />
+					<p>{icon.name}</p>
+				</div>
+			))}
 		</IconContainerStyled>
 	);
 };
