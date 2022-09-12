8594cedf3a9ace69f87895c65c2a6aa8c1a7f430
Jerry Hogan <jerry.hogan@thisdot.co>
2022-09-12 10:42:54 +0100

    Vue3 apollo quasar/feat/single repo issues tab view (#579)
    * Vue + TS + Quasar build: added redirect to repo page in repo card component
    
    * Vue + TS + Quasar fix: added defineComponent to profile page
    Noticed this had been merged without a defineComponent export (which means it would bring issues if it were ever imported anywhere)
    
    * Vue + TS + Quasar fix: fixed issue with incorrect logo path
    Logo had a relative path, which meant it wasn't visible on some pages
    
    * Vue + TS + Quasar refactor: rename useRepoPage export from useRepo to useRepoPage
    - Also fixed error-ing out in the useRepoPage composable
    
    * Vue + TS + Quasar build: radded base repository details page
    TODO: Get repository tree and display file/directory list next
    
    * asset: added file and folder icon svgs
    
    * build: added file explorer nav component
    
    * refactor: prepend 'Page' to repository details page component name
    
    * style: updated info color to be github directory icon color
    
    * build: added file explorer and nav components
    
    * build: added repository details rough pass
    
    * in progress
    
    * chore: folder nav completed
    
    * fix: test working
    
    * in progress
    
    * feat: Refactored TabHeader to take array as config, created RepoPage with branch menu
    
    * Revert "feat: Refactored TabHeader to take array as config, created RepoPage with branch menu"
    
    This reverts commit 11f1c20c92411523cf00c9fd5ad0a23afe5bcb8d.
    
    * feat: display branch menu with branches
    
    * almost done, doing clean ups
    
    * chore: completed
    
    * in progress
    
    * in progress
    
    * added return types where required
    
    * added return types where required, still missing userReducer return type
    
    * fix all test, and types warnings
    
    * clean ups
    
    * fix comments
    
    * fix conflicts
    
    Co-authored-by: Allan Jeremy <aj.dev254@gmail.com>
    Co-authored-by: Jan Kaiser <honza.kaiser@gmail.com>

--- vue3-apollo-quasar/tests/unit/components/IssueTabView.spec.ts
+++ vue3-apollo-quasar/tests/unit/components/IssueTabView.spec.ts
@@ -1,9 +1,19 @@
 import { mount } from '@vue/test-utils';
 import { IssueTabView } from '@/components';
+import { ISSUES } from '@/components/IssueTabView/data';
 
 describe('IssueTabView', () => {
   it('should mount', () => {
-    const wrapper = mount(IssueTabView);
+    const wrapper = mount(IssueTabView, {
+      props: {
+        openIssues: {
+          edges: ISSUES.openIssue,
+        },
+        closedIssues: {
+          edges: ISSUES.closedIssue,
+        },
+      },
+    });
     expect(wrapper.vm).toBeTruthy();
   });
 });
