388f42c9f521cea87333d2c5290e76cb7e8af6f9
kodejuice <kodejuice@gmail.com>
2022-09-16 10:15:21 +0100

    add data

--- cra-rxjs-styled-components/src/components/repo-card/RepoCard.stories.tsx
+++ cra-rxjs-styled-components/src/components/repo-card/RepoCard.stories.tsx
@@ -15,33 +15,30 @@ const Template: ComponentStory<typeof RepoCard> = (args) => (
 	</MemoryRouter>
 );
 
-
-// {
-// 	id: number;
-// 	name: string;
-// 	full_name: string;
-// 	owner: { login: string; type: RepositoryOwnerType };
-// 	description: string;
-// 	private: boolean;
-// 	html_url: string;
-// 	url: string;
-// 	updated_at: Date;
-// 	stargazers_count: number;
-// 	language: string;
-// 	branches_url: string;
-// 	visibility: 'public' | 'private';
-// 	subscribers_count: number;
-// 	forks_count: number;
-// 	open_issues_count: number;
-// 	pulls: number;
-// 	default_branch: string;
-// 	homepage: string;
-// }
-
-export const StarteDevReadme = Template.bind({});
-StarteDevReadme.args = {
+export const StarteDevRepo = Template.bind({});
+StarteDevRepo.args = {
 	repo: {
+		id: 421063754,
 		owner: { login: 'thisdot', type: 'Organization' },
 		name: 'starter.dev',
+		full_name: 'thisdot/starter.dev',
+		private: false,
+		html_url: 'https://github.com/thisdot/starter.dev',
+		description: `starter.dev
+		This is a monorepo for the starter.dev project.
+		starter.dev is a collection of starter kit projects and related tools to help get you up and running on your next project without spending days adding all the pieces and configuration to supporting whatever libraries or frameworks that you choose. Choose a kit with the set of tools you are looking for and use our CLI to generate your new project. The starter kits come with all the essentials included and configured, such as TypeScript, testing, linting, storybook, and more.`,
+		url: 'https://api.github.com/repos/thisdot/starter.dev',
+		updated_at: new Date('2022-01-21T17:34:25Z'),
+		language: 'TypeScript',
+		homepage: '',
+		forks_count: 0,
+		open_issues_count: 0,
+		visibility: 'public',
+		stargazers_count: 8,
+		branches_url:
+			'https://api.github.com/repos/thisdot/starter.dev/branches{/branch}',
+		pulls: 18,
+		subscribers_count: 8,
+		default_branch: 'main',
 	},
 };
