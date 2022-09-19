14edc5e2a9b06c979a6005288d6e415da2078a32
asincole <asincole@gmail.com>
2022-09-19 16:00:31 +0200

    test(angular-apollo-tailwind): update with testcases
    Closes: #591

--- angular-apollo-tailwind/projects/shared/src/lib/pipes/generate-url-with-protocol/generate-url-with-protocol.pipe.spec.ts
+++ angular-apollo-tailwind/projects/shared/src/lib/pipes/generate-url-with-protocol/generate-url-with-protocol.pipe.spec.ts
@@ -5,4 +5,19 @@ describe('GenerateUrlWithProtocolPipe', () => {
     const pipe = new GenerateUrlWithProtocolPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should append https to url with no protocol', () => {
+    const pipe = new GenerateUrlWithProtocolPipe();
+    expect(pipe.transform('test.com')).toBe('https://test.com');
+  });
+
+  it('should return url untouched if it has protocol appended', () => {
+    const pipe = new GenerateUrlWithProtocolPipe();
+    expect(pipe.transform('https://test.com')).toBe('https://test.com');
+  });
+
+  it('should not attempt to change url with http to https', () => {
+    const pipe = new GenerateUrlWithProtocolPipe();
+    expect(pipe.transform('http://test.com')).toBe('http://test.com');
+  });
 });
