ae52b610fc7d843c0845fc919d61c7b3b11c1f89
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-09-23 17:01:26 +0100

    fix story

--- cra-rxjs-styled-components/src/components/Loading/LoadingPulseDots.stories.tsx
+++ cra-rxjs-styled-components/src/components/Loading/LoadingPulseDots.stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react';
+import React from 'react';
 import LoadingPulseDots from './LoadingPulseDots';
 
 export default {
--- cra-rxjs-styled-components/src/components/Loading/LoadingTextLine.stories.tsx
+++ cra-rxjs-styled-components/src/components/Loading/LoadingTextLine.stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react';
+import React from 'react';
 import LoadingTextLine from './LoadingTextLine';
 
 export default {
