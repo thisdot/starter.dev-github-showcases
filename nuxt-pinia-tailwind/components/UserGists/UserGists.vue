b125b6143404f4ac4d5c12168959838ab23e0771
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-09-23 21:36:08 +0200

    Nuxt-pinia-tailwind create gist panel

--- nuxt-pinia-tailwind/components/UserGists/UserGists.vue
+++ nuxt-pinia-tailwind/components/UserGists/UserGists.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="py-8 border-t border-b">
-    <h3 className="font-semibold">Gists</h3>
-    <div className="mt-3"></div>
+    <h3 class="font-semibold">Gists</h3>
+    <div class="mt-3">
+      <div v-for="gist in gists" :key="gist.id" class="mb-4">
+        <a :href="gist.html_url" class="text-blue-500 hover:underline">
+          {{ gist.description }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
-export default {};
+import defineComponent from 'vue';
+import { useUserStore } from '~/store/userStore';
+
+export default new defineComponent({
+  name: 'UserGists',
+  setup() {
+    const userStore = useUserStore();
+    useUserStore().getUserGists();
+    return {
+      gists: userStore.gists,
+    };
+  },
+});
 </script>
