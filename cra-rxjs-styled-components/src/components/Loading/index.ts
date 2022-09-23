6c5ff674973a98a1652f7cda0c60d82c4a5ad249
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-09-23 17:05:34 +0100

    refactor

--- cra-rxjs-styled-components/src/components/Loading/index.ts
+++ cra-rxjs-styled-components/src/components/Loading/index.ts
@@ -1,3 +1,2 @@
-export { default as LoadingPulseDots } from './LoadingPulseDots';
 export { default as LoadingBulletList } from './LoadingBulletList';
 export { default as LoadingTextLine } from './LoadingTextLine';
