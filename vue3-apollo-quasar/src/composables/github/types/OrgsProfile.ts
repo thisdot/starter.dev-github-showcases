28d1dafdf58617e539169ff992a857a8bb826040
Jerry Hogan <jerry.hogan@thisdot.co>
2022-09-29 09:49:27 +0100

    Org query api (#657)
    * in progress
    
    * chore: added query for organization profile

---
+++ vue3-apollo-quasar/src/composables/github/types/OrgsProfile.ts
@@ -0,0 +1,8 @@
+export interface OrgsProfile {
+  avartarUrl: string;
+  description: string;
+  login: string;
+  websiteUrl: string;
+  url: string;
+  name: string;
+}
---
+++ vue3-apollo-quasar/src/views/OrgsProfile/OrgsProfile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Orgs Profile</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OrgsProfile',
+});
+</script>
