321a486429e721d8d912eaa4d91b01905fdf17a2
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-09-30 21:01:16 +0200

    feat: create main page layout

--- nuxt-pinia-tailwind/components/UserRepos/UserRepos.vue
+++ nuxt-pinia-tailwind/components/UserRepos/UserRepos.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="w-full border rounded-lg relative bg-white">
+    <div class="flex items-center justify-between px-4 py-3">
+      <h3 class="font-semibold">Repositories</h3>
+      <NuxtLink :to="'/' + username" class="text-blue-500">View all</NuxtLink>
+    </div>
+
     <RepositoryCard
-      name="name-of-the-repo"
-      :is-private="false"
-      description="this is a description"
+      v-for="repo in repos"
+      :key="repo.id"
+      :name="repo.name"
+      :is-private="repo.private"
+      :description="repo.description"
     />
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { useAsync, useContext } from '@nuxtjs/composition-api';
+import Vue from 'vue';
+import RepositoryCard from '../RepositoryCard/RepositoryCard.vue';
+import { useUserStore } from '~/store/userStore';
+
+export default Vue.extend({
+  name: 'UserRepos',
+  components: { RepositoryCard },
+  setup() {
+    const userStore = useUserStore();
+    const { $auth } = useContext();
+    useAsync(async () => {
+      await userStore.getUserRepos();
+    });
+    return {
+      repos: userStore.repos,
+      username: $auth.$state.user.login,
+    };
+  },
+});
 </script>
