496934ba5b6ef3a0d43f2fd7a84f9aefb983719c
SDaian <scuarissid@gmail.com>
2022-10-25 18:45:51 +0200

    feat(qwik): Get Top Repositories from API

--- qwik-graphql-tailwind/src/routes/index.tsx
+++ qwik-graphql-tailwind/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import * as styles from './homepage.classNames';
+import Repositories from './repositories';
 
 export default component$(() => {
   return (
     <div className={styles.container}>
       <h1 className={styles.h1Text}>Welcome to Qwik⚡️showcase</h1>
+      <Repositories />
     </div>
   );
 });
--- qwik-graphql-tailwind/src/routes/repositories/index.tsx
+++ qwik-graphql-tailwind/src/routes/repositories/index.tsx
@@ -1,5 +1,77 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+import { GITHUB_GRAPHQL } from '~/utils/constants';
+import { useQuery } from '~/utils/useQuery';
+
+export const TOP_REPOS = `
+query UserTopRepos {
+  viewer {
+    id
+    login
+    topRepositories(
+      first: 20
+      orderBy: { field: STARGAZERS, direction: DESC }
+    ) {
+      nodes {
+        id
+        name
+        description
+        owner {
+          login
+        }
+        primaryLanguage {
+          name
+          color
+        }
+        isPrivate
+        stargazerCount
+        forkCount
+        updatedAt
+      }
+    }
+  }
+}
+`;
+
+interface RepoStore {
+  data: any[];
+}
 
 export default component$(() => {
+  const store = useStore<RepoStore>({
+    data: [],
+  });
+
+  useClientEffect$(async () => {
+    const response = await fetchTopRepos();
+
+    updateTopRepos(store, response);
+  });
+
   return <div>There will be repos!</div>;
 });
+
+export function updateTopRepos(store: RepoStore, response: any) {
+  const {
+    data: {
+      viewer: {
+        topRepositories: { nodes },
+      },
+    },
+  } = response;
+  store.data = nodes;
+}
+
+export async function fetchTopRepos(abortController?: AbortController): Promise<any> {
+  const { executeQuery$ } = useQuery(TOP_REPOS);
+
+  const resp = await executeQuery$({
+    signal: abortController?.signal,
+    url: GITHUB_GRAPHQL,
+    headersOpt: {
+      Accept: 'application/vnd.github+json',
+      authorization: `Bearer ${sessionStorage.getItem('token')}`,
+    },
+  });
+
+  return await resp.json();
+}
