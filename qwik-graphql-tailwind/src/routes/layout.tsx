5bbc40e6104c7705a1ff84bca1e907fb4a02b51e
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2022-10-26 10:41:46 +0000

    feat:(qwik) OAuth login page  (#715)
    * feat: initialized qwik kit with starter.dev CLI and created routes
    
    * setup auth layout
    
    * setup auth call
    
    * finished configuring the Github OAuth
    
    * add check for access_token
    
    * add env.example
    
    Co-authored-by: Jan Kaiser <honza.kaiser@gmail.com>

--- qwik-graphql-tailwind/src/routes/layout.tsx
+++ qwik-graphql-tailwind/src/routes/layout.tsx
@@ -1,8 +1,16 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useClientEffect$ } from '@builder.io/qwik';
+import { AUTH_TOKEN } from '~/utils/constants';
 import Header from '../components/header/header';
 import * as styles from './layout.classNames';
 
 export default component$(() => {
+  useClientEffect$(() => {
+    const access_token = sessionStorage.getItem(AUTH_TOKEN);
+    if (!access_token) {
+      window.location.href = '/auth/signin';
+    }
+  });
+
   return (
     <>
       <main className={styles.main}>
---
+++ qwik-graphql-tailwind/src/utils/constants.ts
@@ -0,0 +1,43 @@
+export const API_URL_BASE = import.meta.env.VITE_API_URL;
+export const APP_BASE_URL = import.meta.env.VITE_BASE_URL;
+export const GITHUB_URL_BASE = import.meta.env.VITE_GITHUB_URL;
+
+export const REDIRECT_URL = `${APP_BASE_URL}/auth/redirect`;
+
+export const SIGN_IN_BASE_URL = `${API_URL_BASE}/auth/signin`;
+
+export const SIGN_IN_URL = (() => {
+  const url = new URL(SIGN_IN_BASE_URL);
+  url.searchParams.set('redirect_url', REDIRECT_URL);
+  return url.toString();
+})();
+
+export const SIGN_OUT_URL = `${API_URL_BASE}/auth/signout`;
+export const GET_TOKEN_URL = `${API_URL_BASE}/auth/token`;
+
+export const AUTH_TOKEN = 'token';
+
+// export const REPOS_URL = (page: string = '1') =>
+//   `${GITHUB_URL_BASE}/user/repos?sort=pushed&affiliation=owner,collaborator&page=${page}`;
+
+// export const SINGLE_USER_REPO = (user: string, repo: string) => `${GITHUB_URL_BASE}/repos/${user}/${repo}`;
+
+// export const ORG_REPO_LIST = (user: string) => `${GITHUB_URL_BASE}/orgs/${user}/repos?sort=pushed&per_page=10`;
+
+// export const USER_REPO_LIST = (user: string, page: string = '1') =>
+//   `${GITHUB_URL_BASE}/users/${user}/repos?sort=pushed&page=${page}&type=all`;
+
+// export const GISTS_URL = (user: string) => `${GITHUB_URL_BASE}/users/${user}/gists?per_page=10`;
+
+// export const PULLS_URL = (owner: string, repoName: string) =>
+//   `${GITHUB_URL_BASE}/repos/${owner}/${repoName}/pulls?state=all`;
+
+// export const ISSUE_PR_SEARCH = (
+//   user: string,
+//   repo: string,
+//   type: IssueType,
+//   state: State,
+//   per_page: number,
+//   page: number
+// ) =>
+//   `${GITHUB_URL_BASE}/search/issues?q=repo:${user}/${repo}%20is:${type}%20state:${state}&per_page=${per_page}&page=${page}`;
