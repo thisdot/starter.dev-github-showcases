23c3ba98c3f67f689f88b032c06103f39fca3857
asincole <asincole@gmail.com>
2022-10-27 23:10:41 +0200

    feat(svelte-kit-scss): create profile information component
    Closes: #671

---
+++ svelte-kit-scss/src/lib/fixtures/user.ts
@@ -0,0 +1,53 @@
+import type {UserApiResponse} from "../interfaces";
+
+export const userInfoFixture: UserApiResponse = {
+  "login": "thisdot",
+  "id": 2435445,
+  "node_id": "MDQ6VXNlcjI0MzU0MTc1",
+  "avatar_url": "https://avatars.githubusercontent.com/u/sss?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/thisdot",
+  "html_url": "https://github.com/thisdot",
+  "followers_url": "https://api.github.com/users/thisdot/followers",
+  "following_url": "https://api.github.com/users/thisdot/following{/other_user}",
+  "gists_url": "https://api.github.com/users/thisdot/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/thisdot/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/thisdot/subscriptions",
+  "organizations_url": "https://api.github.com/users/thisdot/orgs",
+  "repos_url": "https://api.github.com/users/thisdot/repos",
+  "events_url": "https://api.github.com/users/thisdot/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/thisdot/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "This dot",
+  "company": null,
+  "blog": "https://thisdot.co",
+  "location": "USA",
+  "email": "test@thisdot.co",
+  "hireable": null,
+  "bio": null,
+  "twitter_username": "thisdotlabs",
+  "public_repos": 30,
+  "public_gists": 0,
+  "followers": 28,
+  "following": 9,
+  "created_at": "2016-12-03T11:46:38Z",
+  "updated_at": "2022-10-27T11:42:50Z"
+}
+
+export const userOrgs = [
+  {
+    "login": "thisdot",
+    "id": 2289396,
+    "node_id": "MDEyOk9yZ2FuaXphdGlvbjIyODM5Mzk2",
+    "url": "https://api.github.com/orgs/thisdot",
+    "repos_url": "https://api.github.com/orgs/thisdot/repos",
+    "events_url": "https://api.github.com/orgs/thisdot/events",
+    "hooks_url": "https://api.github.com/orgs/thisdot/hooks",
+    "issues_url": "https://api.github.com/orgs/thisdot/issues",
+    "members_url": "https://api.github.com/orgs/thisdot/members{/member}",
+    "public_members_url": "https://api.github.com/orgs/thisdot/public_members{/member}",
+    "avatar_url": "https://avatars.githubusercontent.com/u/22839396?v=4",
+    "description": ""
+  }
+]
