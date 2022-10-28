a24f1507a0ded8212a3950741a6133bb6709100e
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-10-28 14:12:00 +0300

    [sveltekit-pod] Make access token available in PageServerLoad (#742)

--- svelte-kit-scss/src/hooks.server.ts
+++ svelte-kit-scss/src/hooks.server.ts
@@ -4,8 +4,19 @@ import {
 } from '$lib/constants/auth';
 import type { Handle } from '@sveltejs/kit';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace App {
+    interface Locals {
+      accessToken?: string;
+    }
+  }
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
   const accessTokenFromCookies = event.cookies.get(AUTH_COOKIE_NAME);
+
+  event.locals.accessToken = accessTokenFromCookies;
 
   if (!accessTokenFromCookies) {
     if (!event.url.pathname.startsWith('/signin')) {
@@ -16,6 +27,7 @@ export const handle: Handle = async ({ event, resolve }) => {
   // erase token cookie
   if (event.url.pathname === '/signin') {
     event.cookies.set(AUTH_COOKIE_NAME, String(), AUTH_COOKIE_ERASE_OPTIONS);
+    event.locals.accessToken = undefined;
   }
 
   const response = await resolve(event);
