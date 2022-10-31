79833881d54d4a0eb16f4d9fb4c3e2b9db73eb31
asincole <asincole@gmail.com>
2022-10-31 16:58:45 +0100

    refactor(svelte-kit-scss): address MR comments
    Closes: #671

--- svelte-kit-scss/src/hooks.server.ts
+++ svelte-kit-scss/src/hooks.server.ts
@@ -1,9 +1,9 @@
-import { AUTH_COOKIE_ERASE_OPTIONS, AUTH_COOKIE_NAME } from '$lib/constants/auth';
-import { ENV } from '$lib/constants/env';
-import { HEADER_NAMES } from '$lib/constants/headers';
-import type { Handle, HandleFetch } from '@sveltejs/kit';
+import {AUTH_COOKIE_ERASE_OPTIONS, AUTH_COOKIE_NAME} from '$lib/constants/auth';
+import {ENV} from '$lib/constants/env';
+import {HEADER_NAMES} from '$lib/constants/headers';
+import type {Handle, HandleFetch} from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({event, resolve}) => {
   const accessTokenFromCookies = event.cookies.get(AUTH_COOKIE_NAME);
 
   event.locals.accessToken = accessTokenFromCookies;
@@ -12,27 +12,19 @@ export const handle: Handle = async ({ event, resolve }) => {
     if (!event.url.pathname.startsWith('/signin')) {
       return Response.redirect(`${event.url.origin}/signin`, 301);
     }
-  } else {
-    // get Authenticated User
-    const url = `${ENV.GITHUB_URL}/user`;
-    const res = await event.fetch(url, {
-      headers: {
-        Authorization: `Bearer ${accessTokenFromCookies}`,
-      },
-    });
-    event.locals.authenticatedUserInfo = await res.json();
   }
 
   // erase token cookie
   if (event.url.pathname === '/signin') {
     event.cookies.set(AUTH_COOKIE_NAME, String(), AUTH_COOKIE_ERASE_OPTIONS);
     event.locals.accessToken = undefined;
+    event.locals.user = undefined;
   }
 
   return resolve(event);
 };
 
-export const handleFetch: HandleFetch = async ({ event, request, fetch }): Promise<Response> => {
+export const handleFetch: HandleFetch = async ({event, request, fetch}): Promise<Response> => {
   if (request.url.startsWith(ENV.GITHUB_URL)) {
     const token = event.locals.accessToken;
     if (token) {
