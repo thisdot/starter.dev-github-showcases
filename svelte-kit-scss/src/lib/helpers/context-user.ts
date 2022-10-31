a9e41799fe9390f0a95aa83657339ac8969c191d
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-10-31 16:16:51 +0300

    Context user - all properties

--- svelte-kit-scss/src/lib/helpers/context-user.ts
+++ svelte-kit-scss/src/lib/helpers/context-user.ts
@@ -2,8 +2,5 @@ import type { ContextUser } from "$lib/interfaces/context-user";
 
 export const remapContextUserAsync = async (response: Response): Promise<ContextUser | undefined> => {
   const json = await response.json();
-
-  return json ? {
-    username: json.username
-  } : undefined;
+  return json;
 }
