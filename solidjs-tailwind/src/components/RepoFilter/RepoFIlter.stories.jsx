dcfb38c6e7affbe7cb01202172b1f105a554b0c7
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-31 08:47:25 +0100

    in progress

---
+++ solidjs-tailwind/src/components/RepoFilter/RepoFIlter.stories.jsx
@@ -0,0 +1,10 @@
+import { RepoFilter } from '.'
+
+export default {
+  title: "components/ Repo filter",
+  argTypes: {},
+}
+
+const Template = (args) => <RepoFilter {...args} />;
+
+export const Default = Template.bind({});
--- solidjs-tailwind/src/components/RepoFilter/index.jsx
+++ solidjs-tailwind/src/components/RepoFilter/index.jsx
@@ -1,7 +1,12 @@
+import SearchInput from "./SearchInput"
+
 export const RepoFilter = () => {
   return (
-    <div>
-      <p>Coming soon!!</p>
+    <div class="flex relative mb-4 space-x-4">
+      <div class="flex-grow">
+        <SearchInput />
+      </div>
+      <div class="flex items-center space-x-1.5"></div>
     </div>
-  )
+  );
 }
