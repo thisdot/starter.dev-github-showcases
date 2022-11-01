64132e5d4fe23359fe69df6dc0dc557b93852a3c
Ian Mungai <iansam74@gmail.com>
2022-11-01 15:18:29 +0300

    chore: design page layout update

---
+++ svelte-kit-scss/src/lib/components/Profile/OrgInfo/OrgInfo.svelte
@@ -0,0 +1,9 @@
+<div class="org-info-container">
+    <a href="/thisdot">
+      <img
+        src="https://avatars.githubusercontent.com/u/22839396?v=4"
+        alt="thisdot logo"
+      />
+      <span class="title">This Dot</span>
+    </a>
+  </div>
--- svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
@@ -1,45 +1,67 @@
 <script lang="ts">
   import type { PageServerData } from './$types';
-  import ProfileAboutSection from '$lib/components/ProfileAboutSection/ProfileAboutSection.svelte';
-  import RepoList from '$lib/components/RepoList/RepoList.svelte';
-  import type { UserReposState } from '$lib/interfaces';
+  import { ProfileType } from '$lib/interfaces';
+  import ProfileAboutSection from '$lib/components/Profile/ProfileAboutSection/ProfileAboutSection.svelte';
+  import ProfileNavSection from '$lib/components/Profile/ProfileNavSection/ProfileNavSection.svelte';
+  import OrgInfo from '$lib/components/Profile/OrgInfo/OrgInfo.svelte';
 
   export let data: PageServerData;
   const { userInfo, userOrgs } = data;
-  const { type } = userInfo;
+  const isOrg = userInfo.type == ProfileType.Organization;
 </script>
 
-<div class="grid grid-cols-12 profile-body container">
-  Hello
-  <div class="subpage col-span-3">
-    {#if data?.userInfo}
-      <ProfileAboutSection userInfo={data.userInfo} userOrgs={data.userOrgs} />
-    {/if}
+<div class="profile-container">
+  <div class="profile-header">
+    <div class="grid grid-cols-12 container">
+      {#if isOrg}
+          <OrgInfo class="col-span-12"></OrgInfo>
+          <nav class="col-span-12">
+            <ProfileNavSection ></ProfileNavSection>
+          </nav>
+      {:else}
+          <div class="col-span-3"></div>
+          <nav class="col-span-9">
+            <ProfileNavSection ></ProfileNavSection>
+          </nav>
+      {/if}
+    </div>
   </div>
-  <div class="col-span-9">
-    {#if data?.userRepos}
-      <RepoList repos={data?.userRepos} />
-    {/if}
-  </div>
-
+  
   <div class="grid grid-cols-12 profile-body container">
-    
-    {#if type == ProfileType.User}
+    {#if isOrg}
+      <!-- TODO controls -->
+      <!-- TODO repo list -->
+    {:else}
       <div class="subpage col-span-3">
         <ProfileAboutSection userInfo={userInfo} userOrgs={userOrgs} />
       </div>
+      <!-- TODO controls -->
+      <!-- TODO repo list -->
     {/if}
-    <!-- <app-repo-controls class="col-span-9"></app-repo-controls> -->
-    <!-- <app-repo-list class="col-span-9" [repos]="repos$ | async"></app-repo-list> -->
   </div>
-</main>
+</div>
 
 <style lang="scss">
+  @use 'src/lib/styles/variables.scss';
+  .profile-container {
+    padding-top: 2rem;
+  }
+  .profile-header {
+    position: sticky;
+    top: 0;
+    z-index: 5;
+    background-color: white;
+    border-bottom: 1px solid rgb(229, 231, 235, 1);
+  }
   .profile-body {
     grid-template-rows: max-content 1fr;
 
     .subpage {
       grid-row: 1 / 3;
     }
+  }
+  
+  .container {
+    max-width: variables.$xl;
   }
 </style>
