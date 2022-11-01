b1ba78cd09ea68ec2d8f13960ecc7c04f0168c5a
Ian Mungai <iansam74@gmail.com>
2022-11-01 17:13:15 +0300

    chore: add repos

---
+++ svelte-kit-scss/src/lib/components/RepoList/RepoList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { UserReposState } from "../../interfaces";
+  import RepositoryCard from "../RepositoryCard/RepositoryCard.svelte";
+
+  export let repos: UserReposState[];
+  export let username: string;
+</script>
+
+<ul>
+    {#each repos as repo}
+        <li>
+            <RepositoryCard {repo} {username} />
+        </li>
+    {/each}
+</ul>
+
+<style lang="scss">
+    ul {
+      list-style: none;
+
+    li {
+      padding: 32px 0;
+      border-bottom: 1px solid rgba(229, 231, 235, 1);
+    }
+}
+</style>
--- svelte-kit-scss/src/routes/(authenticated)/[username]/+page.server.ts
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/+page.server.ts
@@ -1,20 +1,27 @@
 import type { PageServerLoad } from './$types';
-import type { UserApiResponse, UserOrgsApiResponse } from '$lib/interfaces';
-import { mapUserInfoResponseToUserInfo, mapUserOrgsApiResponseToUserOrgs } from '$lib/helpers/user';
+import type { UserApiResponse, UserOrgsApiResponse, UserReposApiResponse } from '$lib/interfaces';
+import { mapUserInfoResponseToUserInfo, mapUserOrgsApiResponseToUserOrgs, mapUserReposToTopRepos } from '$lib/helpers';
 import { ENV } from '$lib/constants/env';
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
   const fetchUserInfoUrl = new URL(`/users/${params.username}`, ENV.GITHUB_URL);
-  const [userInfo, userOrgs] = await Promise.all([
+  const fetchUserReposUrl = new URL(`/users/${params.username}/repos`, ENV.GITHUB_URL);
+  const [userInfo, userOrgs, userRepos] = await Promise.all([
     fetch(fetchUserInfoUrl.toString()).then(
       (response) => response.json() as Promise<UserApiResponse>
     ),
     fetch(`https://api.github.com/user/orgs`).then(
       (response) => response.json() as Promise<UserOrgsApiResponse>
     ),
+    fetch(fetchUserReposUrl.toString()).then(
+      (response) => response.json() as Promise<UserReposApiResponse>
+    ),
   ]);
+
   return {
     userInfo: mapUserInfoResponseToUserInfo(userInfo),
     userOrgs: mapUserOrgsApiResponseToUserOrgs(userOrgs),
+    userRepos: mapUserReposToTopRepos(userRepos),
+    username: params.username
   };
 };
