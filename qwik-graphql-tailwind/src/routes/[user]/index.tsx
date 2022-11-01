c1ed6cac9ca606dc24b33d53c24dc043e2f8ed6f
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-01 15:32:58 +0100

    feat(qwik-pod): implement search funtionality

--- qwik-graphql-tailwind/src/routes/[user]/index.tsx
+++ qwik-graphql-tailwind/src/routes/[user]/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+import { component$, useClientEffect$, useContextProvider, useStore } from '@builder.io/qwik';
 import { GITHUB_GRAPHQL } from '~/utils/constants';
 import { useQuery } from '~/utils/useQuery';
 import { USER_PROFILE_QUERY } from '~/utils/queries/user-profile-query';
@@ -8,6 +8,7 @@ import * as styles from './user-page.classNames';
 import { UserProfileCard } from '../../components/user-profile-card/user-profile-card';
 import ProfileNav from '../../components/profile-nav/profile-nav';
 import { UserRepos } from '../../components/user-repos/user-repos';
+import filterStore, { FilterStoreProps } from '~/context/repo-filter';
 
 interface UserStore {
   user: User | null;
@@ -24,6 +25,15 @@ export default component$(() => {
   const store = useStore<UserStore>({
     user: null,
   });
+
+  const filterState = useStore<FilterStoreProps>({
+    search: '',
+    language: '',
+    sortType: '',
+    order: '',
+  });
+
+  useContextProvider(filterStore, filterState);
 
   const location = useLocation();
 
