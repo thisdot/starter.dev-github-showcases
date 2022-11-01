4148701aa8b2a140047169a0ff1a43299ce55bbe
Ian Mungai <iansam74@gmail.com>
2022-11-01 14:08:12 +0300

    chore: setup profile page

--- svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
   import type { PageServerData } from './$types';
-  import ProfileAboutSection from '$lib/components/ProfileAboutSection/ProfileAboutSection.svelte';
+  import { ProfileType } from '$lib/interfaces';
+  import ProfileAboutSection from '$lib/components/Profile/ProfileAboutSection/ProfileAboutSection.svelte';
+  import ProfileNavSection from '$lib/components/Profile/ProfileNavSection/ProfileNavSection.svelte';
 
   export let data: PageServerData;
+  const { userInfo, userOrgs } = data;
+  const { type } = userInfo;
 </script>
 
-<div class="grid grid-cols-12 profile-body container">
-  Hello
-  <div class="subpage col-span-3">
-    <ProfileAboutSection userInfo={data.userInfo} userOrgs={data.userOrgs} />
+<main>
+  <div class="profile-header">
+    <div class="grid grid-cols-12 container">
+      <div class="col-span-3"></div>
+      <ProfileNavSection class="col-span-9"></ProfileNavSection>
+    </div>
   </div>
-</div>
+
+  <div class="grid grid-cols-12 profile-body container">
+    
+    {#if type == ProfileType.User}
+      <div class="subpage col-span-3">
+        <ProfileAboutSection userInfo={userInfo} userOrgs={userOrgs} />
+      </div>
+    {/if}
+    <!-- <app-repo-controls class="col-span-9"></app-repo-controls> -->
+    <!-- <app-repo-list class="col-span-9" [repos]="repos$ | async"></app-repo-list> -->
+  </div>
+</main>
 
 <style lang="scss">
   .profile-body {
