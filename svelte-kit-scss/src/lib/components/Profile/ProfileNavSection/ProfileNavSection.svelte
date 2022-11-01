1a4532000d6f0fe3817ff44c9aa29af56e808d6e
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-11-01 20:00:41 +0300

    [sveltekit-pod] 18 Create profile page layout (#800)
    * chore: testing pr checks
    
    * chore: setup profile page
    
    * chore: design page layout update
    
    * chore: Add Organisation Info
    
    * chore: add repos
    
    * chore: RepoControls Component
    
    * chore: repocontrols on page
    
    * [sveltekit-pod] 07 Get all repositories from GitHub API (#805)
    
    * [sveltekit-pod] 07 Get all repositories from GitHub API#680
    
    * format
    
    * chore: testing pr checks
    
    * chore: setup profile page
    
    * chore: design page layout update
    
    * chore: Add Organisation Info
    
    * chore: add repos
    
    * chore: RepoControls Component
    
    * chore: repocontrols on page
    
    Co-authored-by: Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>

---
+++ svelte-kit-scss/src/lib/components/Profile/ProfileNavSection/ProfileNavSection.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { Repo24 } from 'svelte-octicons';
+</script>
+
+<ul>
+  <li class="active-tab">
+    <a href="/">
+      <Repo24 class="icon"/>
+      <span>Repositories</span>
+    </a>
+  </li>
+</ul>
+
+<style lang="scss">
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    a {
+      display: inline-block;
+      text-decoration: none;
+      line-height: 24px;
+      padding: 16px;
+      color: rgb(75, 85, 99);
+    }
+
+    span {
+      vertical-align: middle;
+    }
+
+    li {
+      display: inline-block;
+    }
+
+    .active-tab {
+      border-bottom: 2px solid;
+      border-color: rgba(245, 158, 11, 1);
+
+      span {
+        font-weight: 600;
+        color: black;
+
+        &.icon {
+          color: rgb(75, 85, 99);
+        }
+      }
+
+      &:hover,
+      &:focus {
+        border-color: rgb(209, 213, 219);
+      }
+    }
+  }
+
+  .icon {
+    display: inline-block;
+    margin-right: 4px;
+  }
+</style>
--- svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/+page.svelte
@@ -1,31 +1,73 @@
 <script lang="ts">
   import type { PageServerData } from './$types';
-  import ProfileAboutSection from '$lib/components/ProfileAboutSection/ProfileAboutSection.svelte';
-  import RepoList from '$lib/components/RepoList/RepoList.svelte';
-  import type { UserReposState } from '$lib/interfaces';
+  import { ProfileType } from '$lib/interfaces';
+  import ProfileAboutSection from '$lib/components/Profile/ProfileAboutSection/ProfileAboutSection.svelte';
+  import ProfileNavSection from '$lib/components/Profile/ProfileNavSection/ProfileNavSection.svelte';
+  import OrgInfo from '$lib/components/Profile/OrgInfo/OrgInfo.svelte';
+  import RepoList from '../../../lib/components/RepoList/RepoList.svelte';
+  import RepoControls from '../../../lib/components/RepoControls/RepoControls.svelte';
 
   export let data: PageServerData;
+  const { userInfo, userOrgs, userRepos, username } = data;
+  const isOrg = userInfo?.type == ProfileType.Organization;
 </script>
 
-<div class="grid grid-cols-12 profile-body container">
-  <div class="subpage col-span-3">
-    {#if data?.userInfo}
-      <ProfileAboutSection userInfo={data.userInfo} userOrgs={data.userOrgs} />
-    {/if}
+<div class="profile-container">
+  <div class="profile-header">
+    <div class="grid grid-cols-12 container">
+      {#if isOrg}
+        <OrgInfo />
+        <nav class="col-span-12">
+          <ProfileNavSection />
+        </nav>
+      {:else}
+        <div class="col-span-3"></div>
+        <nav class="col-span-9">
+          <ProfileNavSection />
+        </nav>
+      {/if}
+    </div>
   </div>
-  <div class="col-span-9">
-    {#if data?.userRepos}
-      <RepoList repos={data?.userRepos} />
+  
+  <div class="grid grid-cols-12 profile-body container">
+    {#if isOrg}
+      <div class="col-span-12">
+        <RepoControls />
+        <RepoList repos={userRepos} {username} />
+      </div>
+    {:else}
+      <div class="subpage col-span-3">
+        <ProfileAboutSection userInfo={userInfo} userOrgs={userOrgs} />
+      </div>
+      <div class="col-span-9">
+        <RepoControls />
+        <RepoList repos={userRepos} {username} />
+      </div>
     {/if}
   </div>
 </div>
 
 <style lang="scss">
+  @use 'src/lib/styles/variables.scss';
+  .profile-container {
+    padding-top: 2rem;
+  }
+  .profile-header {
+    position: sticky;
+    top: 0;
+    z-index: 5;
+    background-color: white;
+    border-bottom: 1px solid rgb(229, 231, 235, 1);
+  }
   .profile-body {
     grid-template-rows: max-content 1fr;
 
     .subpage {
       grid-row: 1 / 3;
     }
+  }
+  
+  .container {
+    max-width: variables.$xl;
   }
 </style>
