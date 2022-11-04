ab4697a70ddbcebf8274a1f6fc99634132fce761
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-11-04 18:00:30 +0100

    [solidjs-tailwinf] Add user profile card

--- solidjs-tailwind/src/components/index.js
+++ solidjs-tailwind/src/components/index.js
@@ -1,4 +1,6 @@
 export * from './CounterExample';
 export * from './FetchExample';
+export * from './UserProfile';
+export * from './Icons';
 export { default as PageHeader } from './PageHeader';
 export { default as PageFooter } from './PageFooter';
