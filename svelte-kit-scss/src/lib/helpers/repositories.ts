17db6a21a40513f27e75883e31fb54677de95b2c
asincole <asincole@gmail.com>
2022-11-04 18:51:44 +0100

    feat(svelte-kit-scss): add language filter functionality
    Refs: #772

--- svelte-kit-scss/src/lib/helpers/repositories.ts
+++ svelte-kit-scss/src/lib/helpers/repositories.ts
@@ -1,4 +1,6 @@
 import type { UserReposApiResponse, UserReposState } from '../interfaces';
+import type { FilterDropdownOption } from '../components/shared/FilterDropdown/filter-option';
+import { LanguageFilters, TypeFilters } from '../enums';
 
 export const mapUserReposToTopRepos = (repos: UserReposApiResponse): UserReposState[] => {
   if (repos) {
@@ -28,3 +30,45 @@ export const mapUserReposToTopRepos = (repos: UserReposApiResponse): UserReposSt
   }
   return [];
 };
+
+export function filterRepoUtil(
+  searchInput: string,
+  type: FilterDropdownOption<string>,
+  language: FilterDropdownOption<string>
+) {
+  return (repo: UserReposState) => {
+    const searchTermCondition = searchInput
+      ? repo.name.toLowerCase().includes(searchInput.toLowerCase())
+      : true;
+    let typeCondition = false;
+    let languageCondition: boolean;
+
+    if (type?.value === TypeFilters.ALL) {
+      typeCondition = true;
+    } else if (type?.value === TypeFilters.ARCHIVED) {
+      typeCondition = Boolean(repo.archived);
+    } else if (type?.value === TypeFilters.FORKED) {
+      typeCondition = Boolean(repo.fork);
+    }
+
+    if (language?.value === LanguageFilters.ALL) {
+      languageCondition = true;
+    } else {
+      languageCondition = repo.language?.toLowerCase() === language?.value.toLowerCase();
+    }
+
+    return searchTermCondition && typeCondition && languageCondition;
+  };
+}
+
+export function createLanguageMap(repos: UserReposState[]): FilterDropdownOption[] {
+  const uniqueLanguages = new Set();
+  const langFilterList: FilterDropdownOption[] = [];
+  repos.forEach((repo) => {
+    if (repo.language && !uniqueLanguages.has(repo.language)) {
+      uniqueLanguages.add(repo.language);
+      langFilterList.push({ label: repo.language, value: repo.language });
+    }
+  });
+  return langFilterList;
+}
