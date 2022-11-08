ed28acb476c9065360ab7a4c4ae5246e7f765517
Daian Scuarissi <scuarissid@gmail.com>
2022-11-08 13:09:24 +0100

    feat(solidjs) - Create navigation header (#840)

--- solidjs-tailwind/src/components/index.js
+++ solidjs-tailwind/src/components/index.js
@@ -1,4 +1,6 @@
 export * from './CounterExample';
 export * from './FetchExample';
+export * from './Header';
+export * from './UserDropdown';
 export { default as PageHeader } from './PageHeader';
 export { default as PageFooter } from './PageFooter';
