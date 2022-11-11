65974b377c836600bc06c92f99afab6edd0bafe2
Ian Mungai <iansam74@gmail.com>
2022-11-11 13:03:01 +0300

    chore: add pr list to show info

--- svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/pull-requests/+page.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/pull-requests/+page.svelte
@@ -1,1 +1,28 @@
-<p>Pull Requests Goes Here</p>
+<script lang="ts">
+  import type { PageServerData } from './$types';
+  import PRsList from '$lib/components/PRsList/PRsList.svelte';
+
+  export let data: PageServerData;
+
+  const { openPRs, closedPRs } = data;
+
+  let viewState = 'open';
+</script>
+
+<div class="container">
+  <span> header should go here </span>
+  <div class="issues-container">
+    <PRsList prs={viewState === 'open' ? openPRs : closedPRs} />
+  </div>
+  <span> Pagination should go here </span>
+</div>
+
+<style lang="scss">
+  @use 'src/lib/styles/variables.scss';
+
+  .issues-container {
+    border-radius: 0.5rem;
+    border: 1px solid variables.$gray200;
+    margin: 2rem auto;
+  }
+</style>
