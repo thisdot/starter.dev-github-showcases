f4a1e75538f8104e017d423f69ed76d34e7e2f51
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-11-11 18:48:52 +0300

    Cleanup

--- svelte-kit-scss/src/lib/components/FileViewer/types/model.ts
+++ svelte-kit-scss/src/lib/components/FileViewer/types/model.ts
@@ -1,5 +1,4 @@
 import type { Language } from './language';
-import type { Grammar, Token as PrismToken } from 'prismjs';
 
 export type Token = {
   types: string[];
@@ -7,46 +6,7 @@ export type Token = {
   empty?: boolean;
 };
 
-export type TokenEnv = {
-  code: string;
-  grammar: Grammar;
-  language: Language;
-  tokens: Array<string | PrismToken>;
-};
-
 export type StyleObj = { [styleKey: string]: string | number | symbol | null | undefined };
-
-export type LineInputProps = {
-  style?: StyleObj;
-  className?: string;
-  line: Token[];
-};
-
-// todo: remove
-// export type LineOutputProps = {
-//   style: StyleObj;
-//   className: string;
-// };
-
-export type TokenInputProps = {
-  style?: StyleObj;
-  className?: string;
-  token: Token;
-};
-
-export type TokenOutputProps = {
-  style: StyleObj;
-  className: string;
-  content: string;
-};
-
-// todo: remove
-// export type RenderProps = {
-//   tokens: Token[][];
-//   klass: string;
-//   getLineProps: (input: LineInputProps) => LineOutputProps;
-//   getTokenProps: (input: TokenInputProps) => TokenOutputProps;
-// };
 
 export type PrismThemeStyleEntry = StyleObj & {
   color?: string;
