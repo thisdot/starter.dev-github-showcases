cbdce298aa916bfac22afeb8ca4127e8e8268461
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-11 10:34:13 +0100

    chore: repo card with story and test (#737)
    * chore: repo card with story and test
    
    * fix comments
    
    * fix coment
    
    Co-authored-by: Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>

--- solidjs-tailwind/src/components/index.js
+++ solidjs-tailwind/src/components/index.js
@@ -1,2 +1,9 @@
+export * from './CounterExample';
+export * from './FetchExample';
+export * from './RepoMeta';
+export * from './RepoCard';
+export * from './PrivacyBadge';
+export * from './Header';
+export * from './UserDropdown';
 export { default as PageHeader } from './PageHeader';
 export { default as PageFooter } from './PageFooter';
