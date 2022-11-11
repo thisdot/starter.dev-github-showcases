3678a3368870261bd48f1480fa4125beaa622b0b
Oluwakorede Cole <asincole@gmail.com>
2022-11-11 01:15:04 +0100

    fix(angular-apollo-tailwind): append protocol if missing from user url (#637)
    * fix(angular-apollo-tailwind): append protocol if missing from user url
    
    Refs: #591
    
    * test(angular-apollo-tailwind): update with testcases
    
    Closes: #591

--- angular-apollo-tailwind/projects/shared/src/lib/pipes/pipes.module.ts
+++ angular-apollo-tailwind/projects/shared/src/lib/pipes/pipes.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { RoundUpPipe } from './number/round-up.pipe';
 import { FormatDistancePipe } from './dfns/format-distance.pipe';
 import { MarkdownPipe } from './markdown/markdown.pipe';
+import { GenerateUrlWithProtocolPipe } from './generate-url-with-protocol/generate-url-with-protocol.pipe';
 
 @NgModule({
-  declarations: [FormatDistancePipe, RoundUpPipe, MarkdownPipe],
-  exports: [FormatDistancePipe, RoundUpPipe, MarkdownPipe],
+  declarations: [
+    FormatDistancePipe,
+    RoundUpPipe,
+    MarkdownPipe,
+    GenerateUrlWithProtocolPipe,
+  ],
+  exports: [
+    FormatDistancePipe,
+    RoundUpPipe,
+    MarkdownPipe,
+    GenerateUrlWithProtocolPipe,
+  ],
 })
 export class PipesModule {}
