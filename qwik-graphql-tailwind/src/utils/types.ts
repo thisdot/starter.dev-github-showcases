d22ecf8ae85566c116ed777853584902ab276151
Jan Kaiser <jan.kaiser@thisdot.co>
2022-11-11 13:06:35 +0100

    [qwik-pod] feat: user logout + export types (#747)
    * feat: user logout + export types
    
    * Remove repositories folder
    
    * fix: add missing import
    
    Co-authored-by: Daian Scuarissi <scuarissid@gmail.com>

--- qwik-graphql-tailwind/src/utils/types.ts
+++ qwik-graphql-tailwind/src/utils/types.ts
@@ -1,3 +1,5 @@
 export * from './../routes/[user]/types';
 export * from './../components/repo-card/types';
 export * from './../components/user-repos/types';
+export * from './../components/icons/types';
+export * from './../components/tab-navigation/types';
