d53ed016397f27248b0971f12f59465cc0d8f079
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 16:02:29 +0100

    added auth token

--- solidjs-tailwind/src/services/get-top-repos.js
+++ solidjs-tailwind/src/services/get-top-repos.js
@@ -1,13 +1,16 @@
+import { useAuth } from "../auth";
 import { TOP_REPOS_QUERY } from "./queries/top-repos";
 import FetchApi from "./api";
 
 const getTopRepos = async ({url}) => {
+  const { authStore } = useAuth();
+
   const data = {
     url,
     query: TOP_REPOS_QUERY,
     variable: null,
     headersOptions: {
-      authorization: `Bearer tokenvalue`,
+      authorization: `Bearer ${authStore.token}`,
     }
   }
   const resp = await FetchApi(data);
