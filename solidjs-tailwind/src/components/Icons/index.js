01a0077c95c3d0985a7f244ed026397ce0d4e3dd
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 16:54:26 +0100

    resolve

--- solidjs-tailwind/src/components/Icons/index.js
+++
@@ -1,5 +0,0 @@
-export { default as TwitterIcon } from './TwitterIcon';
-export { default as CaretIcon } from './caret';
-export { default as CloseIcon } from './close';
-export { default as CorrectIcon } from './correct';
-export { default as RepoBookIcon } from './repo-book';
