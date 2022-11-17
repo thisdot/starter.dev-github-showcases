e5fd596f86fd10ef8bc78380385f5b5fa76215d9
SDaian <scuarissid@gmail.com>
2022-11-17 14:15:38 +0100

    Add file icons

---
+++ solidjs-tailwind/src/components/Icons/index.js
@@ -0,0 +1,7 @@
+export * from './repo.icon';
+export * from './TwitterIcon';
+export * from './LinkIcon';
+export * from './LocationMarkerIcon';
+export * from './OfficeBuildingIcon';
+export * from './StarIcon';
+export * from './UsersIcon';
