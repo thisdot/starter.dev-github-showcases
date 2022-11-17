9710312ad1708621d1322a407a98f11e794db2ba
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 15:14:39 +0100

    auth token added to header

--- solidjs-tailwind/src/components/icons/index.js
+++ solidjs-tailwind/src/components/icons/index.js
@@ -1,4 +1,1 @@
-export { default as CaretIcon } from './caret';
-export { default as CloseIcon } from './close';
-export { default as CorrectIcon } from './correct';
-export { default as RepoBookIcon } from './repo-book';
+export { default as TwitterIcon } from './TwitterIcon';
