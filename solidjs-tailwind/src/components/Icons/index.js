36fe7a90c1a074a83a597ebe27dadb83c24ad55b
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 14:49:22 +0100

    ..updates

--- solidjs-tailwind/src/components/Icons/index.js
+++ solidjs-tailwind/src/components/Icons/index.js
@@ -3,4 +3,7 @@ export { default as CaretIcon } from './caret';
 export { default as CloseIcon } from './close';
 export { default as CorrectIcon } from './correct';
 export { default as RepoBookIcon } from './repo-book';
+<<<<<<< HEAD
 
+=======
+>>>>>>> ead018f3 (..updates)
