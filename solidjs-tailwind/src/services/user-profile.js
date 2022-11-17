491ead20656223782dde1ddfa78ea8b75c11b659
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 16:04:07 +0100

    added auth token

--- solidjs-tailwind/src/services/user-profile.js
+++ solidjs-tailwind/src/services/user-profile.js
@@ -1,15 +1,18 @@
 import FetchApi from "./api";
+import { useAuth } from "../auth";
 import { USER_PROFILE_QUERY } from "./queries/user-profile";
 
 const userProfile = async ({
   url
 }) => {
+  const { authStore } = useAuth();
+
   const data = {
     url,
     query: USER_PROFILE_QUERY,
     variable: null,
     headersOptions: {
-      authorization: `Bearer tokenvalue`,
+      authorization: `Bearer ${authStore.token}`,
     }
   }
   const resp = await FetchApi(data);
