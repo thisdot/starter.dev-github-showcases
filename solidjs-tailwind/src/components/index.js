457d31176875a8453d0f8335616147e6ac1c0411
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-17 14:11:42 +0100

    ..updates

--- solidjs-tailwind/src/components/index.js
+++ solidjs-tailwind/src/components/index.js
@@ -9,3 +9,9 @@ export * from './Header';
 export * from './UserDropdown';
 export { default as PageHeader } from './PageHeader';
 export { default as PageFooter } from './PageFooter';
+// Icons
+export { default as CaretIcon } from './icons/caret';
+export { default as CloseIcon } from './icons/close';
+export { default as CorrectIcon } from './icons/correct';
+export { default as RepoBookIcon } from './icons/repo-book';
+export { default as TwitterIcon } from './icons/TwitterIcon';
