7ec79d5ab2f2dfaf9f849ea9986cdca39e80d182
WillHutt <whutt1994@gmail.com>
2022-11-22 14:49:04 -0800

    feat: adds codegen, typescript, WIP ignoring of existing js

---
+++ starter-dev-backend/codegen.ts
@@ -0,0 +1,16 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: './schema/index.ts',
+  generates: {
+    './generated/graphql.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
+
+export default config;
