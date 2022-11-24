b042066caddf4d6a5e443f789958afbe6a6b3ad4
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-11-24 15:38:50 +0100

    added userRepos stories

---
+++ solidjs-tailwind/src/components/UserRepos/UserRepos.stories.jsx
@@ -0,0 +1,23 @@
+import { Router } from '@solidjs/router';
+import UserRepos from './UserRepos';
+import { repoCardProps } from '../RepoCard/data';
+
+export default {
+  title: 'Components/User Repos',
+  component: UserRepos,
+  argTypes: {
+   repos: {},
+  },
+};
+
+const Template = (args) => (
+  <Router>
+    <UserRepos {...args} />
+  </Router>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  repos: [repoCardProps],
+};
