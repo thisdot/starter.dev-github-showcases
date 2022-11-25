db542b7d6a1b3838ca11f52bb85c671feb048517
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-11-25 19:57:09 +0300

    Cleanup

--- svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/[issueSearchType=issue_search_type]/+page.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/[issueSearchType=issue_search_type]/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import type { PageServerData } from './$types';
-  import IssuesList from '$lib/components/IssuesList/IssuesList.svelte';
-  import IssueHeader from '$lib/components/IssueHeader/IssueHeader.svelte';
   import Pagination from '$lib/components/shared/Pagination/Pagination.svelte';
-  // import type { PR_STATE } from '$lib/interfaces';
-  import type { FilterDropdownOption } from '$lib/components/shared/FilterDropdown/filter-option';
   import IssueSearchList from '$lib/components/IssueSearch/IssueSearchList.svelte';
   import IssueSearchControls from '$lib/components/IssueSearch/IssueSearchControls/IssueSearchControls.svelte';
   import BoxLayout from '$lib/components/shared/layouts/BoxLayout.svelte';
