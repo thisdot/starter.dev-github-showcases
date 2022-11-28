5c16a1c9a8c0644e86b3aadacc73f0f8b19f7fa3
Dane Grant <danecando@gmail.com>
2022-11-28 10:49:06 -0500

    fix(solidjs-tailwind): add home page changes back

--- solidjs-tailwind/src/pages/Home.classNames.js
+++
@@ -1,7 +0,0 @@
-export const container = 'w-full min-h-[calc(100vh-70px)] flex flex-col-reverse lg:flex-row bg-gray-100';
-export const content = 'max-w-screen-lg w-full';
-export const title = 'text-lg font-medium mb-4';
-export const ul = 'list-inside list-disc my-8 text-xl';
-export const li = 'py-2';
-export const link = 'text-blue-500 underline italic hover:text-blue-800';
-export const aside = 'w-full lg:w-96 bg-white p-8';
