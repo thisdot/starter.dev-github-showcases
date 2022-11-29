0efe2384c8dc2f279934759f6b5af26211a1103b
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 14:32:15 -0700

    gql endpoint

---
+++ starter-dev-backend/src/graphql.ts
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    hello(greeting: String!): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: async ({ greeting }) => {
+      return `Hello, ${greeting}`;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+exports.handler = server.createHandler();
--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -23,7 +23,6 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/graphql', graphql);
 app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
