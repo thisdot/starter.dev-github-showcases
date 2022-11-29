0d62a4328c990d4b76bcfd13e3e6aa86ba84a26e
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 14:33:45 -0700

    d'oh

--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -2,7 +2,6 @@ import serverless from 'serverless-http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import graphql from './routes';
 // import {
 //   accessToken,
 //   clearCookies,
