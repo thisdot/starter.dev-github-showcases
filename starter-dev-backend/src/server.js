e2a2f348eaf8a63f233cf917a4b6562b754cc4c6
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 16:03:35 -0700

    callback, token, signout

--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -12,17 +12,26 @@ import {
 
 export const app = express();
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.get('/api/auth/signin', fetchSigninUrl);
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
+
+router.get('/', (req, res) => {
+  res.redirect(303, `https://starter.dev`);
+});
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// Step 1 - push user to Github OAuth
+app.get('/api/auth/signin', fetchSigninUrl);
+
+// Step 2 - verify code and state then fetch token and save it as a cookie
+// TODO: replace Github OAuth app with new endpoint -> `https://<host>/api/auth/signin/callback`
+app.get('/api/auth/signin/callback', accessToken);
+
+// Step 3 - client fetches token from cookie
+app.get('/api/auth/token', getAccessToken);
+
+app.post('/api/auth/signout', clearCookies);
 
 // Step 1 - push user to Github OAuth
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
