1131dce0fa2e3c752a6fe4a6a11d1161f90e3d97
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 14:18:38 -0700

    gql route

--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -2,7 +2,7 @@ import serverless from 'serverless-http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import routes from './routes';
+import graphql from './routes';
 // import {
 //   accessToken,
 //   clearCookies,
@@ -23,6 +23,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+app.use('/graphql', graphql);
 app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
