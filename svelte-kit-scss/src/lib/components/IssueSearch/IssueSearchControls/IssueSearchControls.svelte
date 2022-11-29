9dbed2c27bbe7b578dd444232b8cfb7dcd5a6baa
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-11-29 12:40:42 +0300

    Code review + Components cleanup

--- svelte-kit-scss/src/lib/components/IssueSearch/IssueSearchControls/IssueSearchControls.svelte
+++ svelte-kit-scss/src/lib/components/IssueSearch/IssueSearchControls/IssueSearchControls.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import { goto, prefetch } from '$app/navigation';
-  import SelectButton from '$lib/components/shared/buttons/SelectButton.svelte';
   import DropdownMenuSelect from '$lib/components/shared/Dropdown/DropdownMenuSelect/DropdownMenuSelect.svelte';
-  import { IssueState } from '$lib/interfaces';
-  import IssueStateIcon from '../IssueSearchListItem/IssueStateIcon.svelte';
   import DropdownFilterTextButton from './DropdownFilterTextButton.svelte';
   import type { NavigationFilterOption } from './models';
+  import IssueStateFilter from './IssueStateFilter.svelte';
 
   export let sortFilters: NavigationFilterOption[];
   export let stateFilters: NavigationFilterOption[];
@@ -17,34 +15,11 @@
     await prefetch(option.href);
     await goto(option.href);
   };
-
-  const resolveStateFilterIssueState = (option: NavigationFilterOption): IssueState | undefined => {
-    let result: IssueState | undefined;
-    if (option.label.endsWith('Closed')) {
-      result = IssueState.Closed;
-    } else if (option.label.endsWith('Open')) {
-      result = IssueState.Open;
-    }
-    return result;
-  };
 </script>
 
 <div class="issue-search-controls">
   <div class="primary">
-    <SelectButton
-      options={stateFilters}
-      labelAccessor={(x) => x.label}
-      checkedPredicate={(x) => x.active}
-      on:select={async ({ detail }) => await handleFilterSelect(detail)}
-    >
-      <svelte:fragment slot="option-icon-left" let:option>
-        {#each [resolveStateFilterIssueState(option)] as state}
-          {#if state}
-            <IssueStateIcon {state} />
-          {/if}
-        {/each}
-      </svelte:fragment>
-    </SelectButton>
+    <IssueStateFilter options={stateFilters} />
   </div>
   <div>
     <DropdownMenuSelect
---
+++ svelte-kit-scss/src/lib/components/IssueSearch/IssueSearchControls/IssueStateFilter.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import IssueStateIcon from '../IssueSearchListItem/IssueStateIcon.svelte';
+  import type { NavigationFilterOption } from './models';
+  import { IssueState } from '$lib/interfaces';
+
+  export let options: NavigationFilterOption[];
+  $: [openFilter, closedFilter] = options;
+</script>
+
+<div class="issue-state-filters" data-sveltekit-preload-data="off">
+  <a class="link" href={openFilter.href} class:active={openFilter.active}
+    ><IssueStateIcon state={IssueState.Open} /><span>{openFilter.label}</span></a
+  >
+  <a class="link" href={closedFilter.href} class:active={closedFilter.active}
+    ><IssueStateIcon state={IssueState.Closed} /><span>{closedFilter.label}</span></a
+  >
+</div>
+
+<style lang="scss">
+  .issue-state-filters {
+    display: flex;
+    gap: 1em;
+    .link {
+      text-decoration: none;
+      opacity: 0.7;
+      color: inherit;
+      display: flex;
+      align-items: center;
+      gap: 0.25em;
+      &.active {
+        font-weight: 600;
+      }
+      &:hover,
+      &.active {
+        opacity: 1;
+      }
+    }
+  }
+</style>
