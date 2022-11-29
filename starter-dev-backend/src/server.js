4ec0a79ae06871293b5448c8fc9ceccebcf9735e
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 14:01:49 -0700

    missing deps

--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -1,5 +1,6 @@
 import serverless from 'serverless-http';
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
 // import {
