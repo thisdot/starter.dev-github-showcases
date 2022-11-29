536c7dcb8c5fd4751d1f7b9f1e8ae36d06862586
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 16:37:28 -0700

    remove serverless copies

--- starter-dev-backend/lib/expiration.js
+++
@@ -1,6 +0,0 @@
-/**
- * Add more time to expiration when generating a new token.
- * @param {number} expiresIn - time to expiration
- * @returns {number} new time to expiration
- */
-export default (expiresIn) => Date.now() + expiresIn * 1000;
--- starter-dev-backend/lib/get-access-token.js
+++
@@ -1,7 +0,0 @@
-import { ACCESS_TOKEN_COOKIE } from './constants';
-
-export default async (req, res, next) => {
-  res.status(200).json({
-    access_token: req.cookies[ACCESS_TOKEN_COOKIE],
-  });
-};
