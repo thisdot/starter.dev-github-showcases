81b48f4fa3d99576e7f5b997ac8fbc40b94e2b04
Dane Grant <danecando@gmail.com>
2022-11-29 10:53:08 -0500

    fix(solidjs-tailwind): define default arg object update file to jsx

--- solidjs-tailwind/src/components/RepoHeading/RepoHeading.stories.jsx
+++ solidjs-tailwind/src/components/RepoHeading/RepoHeading.stories.jsx
@@ -1,14 +1,6 @@
 import RepoHeading from './RepoHeading';
 
-export default {
-  component: RepoHeading,
-  title: 'RepoPage/RepoHeading',
-};
-
-const Template = (args) => <RepoHeading {...args} />;
-
-export const Public = Template.bind({});
-Public.args = {
+const args = {
   name: 'starter.dev',
   owner: 'thisdot',
   data: {
@@ -23,26 +15,27 @@ Public.args = {
   },
 };
 
+export default {
+  component: RepoHeading,
+  title: 'RepoPage/RepoHeading',
+};
+
+const Template = (args) => <RepoHeading {...args} />;
+
+export const Public = Template.bind({});
+Public.args = args;
+
 export const Private = Template.bind({});
 Private.args = {
-  name: 'starter.dev',
-  owner: 'thisdot',
+  ...args,
   data: {
+    ...args.data,
     isPrivate: true,
-    stargazerCount: 30,
-    forkCount: 10,
-    watcherCount: 5,
-    openIssueCount: 2,
-    openPullRequestCount: 1,
-    topics: [],
-    isOrg: true,
   },
 };
 
 export const Loading = Template.bind({});
 Loading.args = {
-  name: 'starter.dev',
-  owner: 'thisdot',
-  isRepoLoading: true,
+  ...args,
   data: undefined,
 };
