7d4b3efd2cb75040db2066856517c8e6fe871de1
Jesse Tomchak <jtomchak@gmail.com>
2022-11-29 15:19:32 -0700

    check for event.requestContext

--- starter-dev-backend/src/graphql.js
+++ starter-dev-backend/src/graphql.js
@@ -17,18 +17,25 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+const getHandler = (event, context) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
 
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // install the Playground plugin and set the `introspection` option explicitly to `true`.
-  introspection: true,
-  persistedQueries: false,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
+    // By default, the GraphQL Playground interface and GraphQL introspection
+    // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+    //
+    // If you'd like to have GraphQL Playground and introspection enabled in production,
+    // install the Playground plugin and set the `introspection` option explicitly to `true`.
+    introspection: true,
+    persistedQueries: false,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+  const graphqlHandler = server.createHandler();
+  if (!event.requestContext) {
+    event.requestContext = context;
+  }
+  return graphqlHandler(event, context);
+};
 
-exports.handler = server.createHandler();
+exports.handler = getHandler;
