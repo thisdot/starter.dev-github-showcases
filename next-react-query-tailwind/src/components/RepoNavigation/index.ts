thisdot/starter.dev-github-showcases

commit c73e24edb49b8b891417a96b28fd3809322665ee

[remix-gql-tailwind] Feature/restructure components (#525)
* Restructure components version 2

* fix missing imports

* # 505 Rename FileExplorerNav file that contains RepoNavigation

* #505 Feature/RepoNavigation naming and React Best Practices

* bug-fix/added cuid to key insted of id

* bug-fix/added cuid to key insted of id

* #507 bug-fix/Fix external URL anchor tags

* update gitignore

Co-authored-by: Adrivinci <sheerikie@gmail.com>

next-react-query-tailwind/src/components/RepoNavigation/index.ts
@@ -1,1 +1,1 @@
-export { default as default } from './RepoNavigation';
+export { default as default } from './FileExplorerNav';

commit d10c9d218f80cde15152c799a63ae35d1394ff33

fix: fix build
Broken in c73e24edb49b8b891417a96b28fd3809322665ee

next-react-query-tailwind/src/components/RepoNavigation/index.ts
@@ -1,1 +1,1 @@
-export { default as default } from './FileExplorerNav';
+export { default as default } from './RepoNavigation';