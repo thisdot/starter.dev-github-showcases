e0121f6d807a83da0194c73478590a6371d3d2d2
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-11-30 09:49:13 +0300

    chore: remove !important issue-search-assignees (#985)

--- svelte-kit-scss/src/lib/components/IssueSearch/IssueSearchListItem/IssueSearchAssignees.svelte
+++ svelte-kit-scss/src/lib/components/IssueSearch/IssueSearchListItem/IssueSearchAssignees.svelte
@@ -21,7 +21,7 @@
     {#each assigneesReversed as assignee, i}
       <div
         class="assignee"
-        style="margin-right: {computeMarginRightPx(i)}px;"
+        style={`--squish-margin: ${computeMarginRightPx(i)}px`}
         class:last={i === assigneesCount - 1}
       >
         <img class="image" src={assignee.avatarUrl} alt={assignee.login} />
@@ -42,9 +42,9 @@
       height: 100%;
       &:hover {
         .assignee {
-          margin-right: 4px !important;
+          margin-right: 4px;
           &.last {
-            margin-right: 0px !important;
+            margin-right: 0px;
           }
         }
       }
@@ -53,6 +53,7 @@
         width: $size;
         display: inline-block;
         transition: margin-right 0.3s ease-in-out;
+        margin-right: var(--squish-margin, 0px);
         .image {
           border-radius: 100%;
           height: 100%;
