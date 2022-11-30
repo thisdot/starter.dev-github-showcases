2b643c6c67849218589a58f17ed12e7563a2d0b9
Chinedu Ekene Okpala <allstackdev@gmail.com>
2022-11-30 19:52:58 +0000

    update OrgProfile to show ProfileNav and added test for ProfileNav

--- solidjs-tailwind/src/pages/OrgProfile.jsx
+++ solidjs-tailwind/src/pages/OrgProfile.jsx
@@ -1,8 +1,9 @@
 import { createEffect, createResource, createSignal } from 'solid-js';
 import { useParams } from '@solidjs/router';
-import { UserRepos } from '../components/UserRepos';
 import { OrgAbout } from '../components/OrgAbout';
 import getOrgRepos from '../services/get-org-repos';
+import { UserRepos } from '../components/UserRepos';
+import ProfileNav from '../components/ProfileNav/ProfileNav';
 
 const parseRepoData = (repos) => {
   return repos?.edges.map((res) => res.node);
@@ -29,26 +30,31 @@ const OrgProfile = () => {
   });
 
   return (
-    <div class="relative pt-4">
-      <div class="border-b border-gray-200 sticky top-0 bg-white hidden md:block z-50">
-        <div class="grid grid-cols-12 max-w-screen-lg mx-auto">
-          <div class="col-span-12 md:col-span-8 xl:col-span-12 px-4">
-            <OrgAbout {...orgInfo()} />
+    <>
+      {resp.loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div class="relative pt-4">
+          <div class="border-b border-gray-200 sticky top-0 bg-white hidden md:block z-50">
+            <div class="grid grid-cols-12 max-w-screen-lg mx-auto">
+              <div class="col-span-12 md:col-span-8 xl:col-span-12 px-4">
+                <OrgAbout {...orgInfo()} />
+              </div>
+              <div class="col-span-12 md:col-span-8 xl:col-span-12">
+                <ProfileNav class="border-none" />
+              </div>
+            </div>
           </div>
-          <div class="col-span-12 md:col-span-8 xl:col-span-12">
-            {/* TODO:  <ProfileNav /> goes here with class="border-none */}
+          <div class="max-w-screen-lg mx-auto py-8 px-4">
+            <div class="grid grid-cols-12 gap-8">
+              <div class="col-span-12 md:col-span-8 xl:col-span-12">
+                <UserRepos loading={resp.loading} repos={repos()} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="max-w-screen-lg mx-auto py-8 px-4">
-        <div class="grid grid-cols-12 gap-8">
-          <div class="col-span-12 md:col-span-8 xl:col-span-12">
-            {/* TODO:  <ProfileNav /> goes here with class="border-none md:hidden" */}
-            <UserRepos loading={resp.loading} repos={repos()} />
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
