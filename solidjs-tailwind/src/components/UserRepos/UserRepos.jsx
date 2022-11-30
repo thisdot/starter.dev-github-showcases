edb2c337e65c1f1f89fe0b03ed154656e92e04b3
Jerry Hogan <jerry.hogan@thisdot.co>
2022-11-30 14:50:06 +0100

    Update UserRepos.jsx
    fix lint error

--- solidjs-tailwind/src/components/UserRepos/UserRepos.jsx
+++ solidjs-tailwind/src/components/UserRepos/UserRepos.jsx
@@ -1,7 +1,6 @@
 import { For, splitProps } from 'solid-js';
 import { RepoCard } from '../RepoCard';
 import { RepoFilter } from '../RepoFilter';
-import RepoCard from '../RepoCard/RepoCard';
 
 const UserRepos = (props) => {
   const [local] = splitProps(props, ['loading', 'repos', 'languages']);
