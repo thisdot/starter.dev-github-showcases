51a591af5db3b60713aec095fa7c7d43a6b05507
William Takashi Mimura <william.mimura@thisdot.co>
2022-11-30 12:42:08 -0800

    feat (#961): Add Netlify badge to CRA Styled Components
    - add package `eslint-config-react-app` because of eslint errors when
      running app
    - use yarn because of README.md file
    - add netlify badge to footer pages

--- cra-rxjs-styled-components/src/components/layouts/ProfileLayout.tsx
+++ cra-rxjs-styled-components/src/components/layouts/ProfileLayout.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
-  height: 100%;
+  height: calc(100% - 70px);
 	padding: 0 1rem;
   background-color: rgb(255, 255, 255);
   display: grid;
@@ -11,11 +11,20 @@ export const Layout = styled.div`
     grid-template-areas:
       'header'
       'aside'
-      'content';
+      'content'
+	  'footer';
   }
 
   @media (min-width: 850px) {
     grid-template-columns: 384px auto;
-    grid-template-areas: 'aside content';
+    grid-template-areas: 'aside content' 'footer footer';
     margin: 20px;
+	height: calc(100% - 110px);
+`;
+
+export const NetlifyBadgeContainer = styled.div`
+	grid-area: footer;	
+	margin-top: auto;
+	text-align: center;
+	background-color: white;
 `;
