thisdot/starter.dev-github-showcases

commit c73e24edb49b8b891417a96b28fd3809322665ee

[remix-gql-tailwind] Feature/restructure components (#525)
* Restructure components version 2

* fix missing imports

* # 505 Rename FileExplorerNav file that contains RepoNavigation

* #505 Feature/RepoNavigation naming and React Best Practices

* bug-fix/added cuid to key insted of id

* bug-fix/added cuid to key insted of id

* #507 bug-fix/Fix external URL anchor tags

* update gitignore

Co-authored-by: Adrivinci <sheerikie@gmail.com>

next-react-query-tailwind/src/components/RepoNavigation/index.ts
@@ -1,1 +1,1 @@
-export { default as default } from './RepoNavigation';
+export { default as default } from './FileExplorerNav';

remix-gql-tailwind/app/routes/$user/$repository/index.tsx
@@ -1,17 +1,18 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData, useOutletContext } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { auth } from '~/services/auth.server';
 import gqlClient from '~/lib/graphql-client';
 import { RepoContext, RepoProvider } from '~/context/RepoContext';
-import RepoHeader from '../../../components/RepoHeader/RepoHeader';
-import RepoNavigation from '../../../components/RepoNavigation/RepoNavigation';
-import FileExplorer from '../../../components/FileExplorer/FileExplorer.view';
-import RepoReadMe from '../../../components/RepoReadMe/RepoReadMe.view';
-import RepoAboutWidget from '~/components/RepoAboutWidget/RepoAboutWidget';
-import { parseTopics } from '~/components/RepoPage/parseTopics';
-import { parseQueryData } from '~/components/FileExplorer/parseQueryData';
-import { parseQuery } from '~/components/RepoReadMe/parseQuery';
+import RepoHeader from '../../../components/Repositories/RepoHeader/RepoHeader';
+import RepoNavigation from '../../../components/Repositories/RepoNavigation/RepoNavigation';
+import FileExplorer from '../../../components/Repositories/FileExplorer/FileExplorer.view';
+import RepoReadMe from '../../../components/Repositories/RepoReadMe/RepoReadMe.view';
+import RepoAboutWidget from '~/components/Repositories/RepoAboutWidget/RepoAboutWidget';
+import { parseTopics } from '~/components/Repositories/RepoPage/parseTopics';
+import { parseQueryData } from '~/components/Repositories/FileExplorer/parseQueryData';
+import { parseQuery } from '~/components/Repositories/RepoReadMe/parseQuery';
+
 import { REPO_PAGE_QUERY } from '~/lib/queries/RepoPage';
 import { REPO_TREE_QUERY } from '~/lib/queries/FileExplorer';
 import { REPO_README_QUERY } from '~/lib/queries/RepoReadMe';

remix-gql-tailwind/app/routes/$user/index.tsx
@@ -4,12 +4,12 @@ import { useLoaderData, useLocation } from '@remix-run/react';
 import gqlClient from '~/lib/graphql-client';
 import { FULL_USER_PROFILE_QUERY } from '~/lib/queries/UserProfile';
 import { auth } from '~/services/auth.server';
-import { parseQuery } from '~/components/UserRepos/parseQuery';
-import ProfilePage from '~/components/ProfilePage/ProfilePage.view';
+import { parseQuery } from '~/components/User/UserRepos/parseQuery';
+import ProfilePage from '~/components/Profiles/ProfilePage/ProfilePage.view';
 import {
   OrderDirection,
   RepositoryOrderField,
-} from '~/components/RepoFilters/useRepoFilters';
+} from '~/components/Repositories/RepoFilters/useRepoFilters';
 
 type LoaderData = {
   userProfileData: any;

remix-gql-tailwind/app/routes/index.tsx
@@ -3,14 +3,14 @@ import { useLoaderData } from '@remix-run/react';
 import type { GitHubProfile } from 'remix-auth-github';
 import { auth } from '~/services/auth.server';
 import gqlClient from '~/lib/graphql-client';
-import { parseQuery } from '~/components/UserTopRepos/parseQuery';
-import { parseQueryUserGists } from '~/components/UserGists/parseQuery';
+import { parseQuery } from '~/components/User/UserTopRepos/parseQuery';
+import { parseQueryUserGists } from '~/components/User/UserGists/parseQuery';
 import { USER_TOP_REPOS_QUERY } from '~/lib/queries/UserTopRepos';
 import { USER_GISTS_QUERY } from '~/lib/queries/UserGists';
-import UserTopReposView from '~/components/UserTopRepos/UserTopRepos.view';
-import UserGistsView from '~/components/UserGists/UserGists.view';
-import { TopRepo } from '~/components/UserTopRepos/types';
-import { GistItem } from '~/components/UserGists/types';
+import UserTopReposView from '~/components/User/UserTopRepos/UserTopRepos.view';
+import UserGistsView from '~/components/User/UserGists/UserGists.view';
+import { TopRepo } from '~/components/User/UserTopRepos/types';
+import { GistItem } from '~/components/User/UserGists/types';
 
 type LoaderData = {
   profile: GitHubProfile;

commit 6039f3c2ba2291a2e8e936ce3f3556abffa431e7

feat (#961): Add next react query tailwind netlify badge (#1004)
* fix: fix build

Broken in c73e24edb49b8b891417a96b28fd3809322665ee

* feat (#961): Add Netlify badge to Next React Query Tailwind showcase

* add noreferrer noopener to new anchors

next-react-query-tailwind/src/components/ProfilePage/ProfilePage.tsx
@@ -23,7 +23,19 @@ function ProfilePage({ owner, children }: ProfilePageProps) {
     return <>{error.message}</>;
   }
 
-  return <ErrorBoundary>{children({ username: owner })}</ErrorBoundary>;
+  return (
+    <ErrorBoundary>
+      {children({ username: owner })}
+      <div className="flex justify-center mt-auto pb-5 pt-6">
+        <a target="_blank" rel="noreferrer noopener" href="https://www.netlify.com">
+          <img
+            src="https://www.netlify.com/v3/img/components/netlify-light.svg"
+            alt="Deploys by Netlify"
+          />
+        </a>
+      </div>
+    </ErrorBoundary>
+  );
 }
 
 export default ProfilePage;

next-react-query-tailwind/src/components/RepoNavigation/index.ts
@@ -1,1 +1,1 @@
-export { default as default } from './FileExplorerNav';
+export { default as default } from './RepoNavigation';

next-react-query-tailwind/src/components/RepoPage/RepoPage.data.tsx
@@ -83,7 +83,17 @@ function RepoPage({ name, owner, branch, path = '', children }: RepoPageProps) {
 
   return (
     <ErrorBoundary>
-      <RepoProvider value={context}>{children}</RepoProvider>
+      <div className="h-[calc(100%-70px)] flex flex-col">
+        <RepoProvider value={context}>{children}</RepoProvider>
+        <div className="flex justify-center mt-auto pb-5 pt-6">
+          <a target="_blank" rel="noreferrer noopener" href="https://www.netlify.com">
+            <img
+              src="https://www.netlify.com/v3/img/components/netlify-light.svg"
+              alt="Deploys by Netlify"
+            />
+          </a>
+        </div>
+      </div>
     </ErrorBoundary>
   );
 }

next-react-query-tailwind/src/pages/index.tsx
@@ -12,16 +12,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-full  min-h-[calc(100vh-70px)] flex flex-col-reverse lg:flex-row bg-gray-100">
+      <div className="w-full  min-h-[calc(100vh-165px)] flex flex-col-reverse lg:flex-row bg-gray-100">
         <aside className="w-full lg:w-96 bg-white p-8">
           <UserGists />
         </aside>
         <main className="max-w-screen-lg w-full">
           <div className="p-12">
-            <h2 data-testid="show repo list" className="text-lg font-medium mb-4">Top Repositories</h2>
+            <h2
+              data-testid="show repo list"
+              className="text-lg font-medium mb-4"
+            >
+              Top Repositories
+            </h2>
             <UserTopRepos />
           </div>
         </main>
+      </div>
+      <div className="flex justify-center mt-auto pb-5 pt-6">
+        <a target="_blank" rel="noreferrer noopener" href="https://www.netlify.com">
+          <img
+            src="https://www.netlify.com/v3/img/components/netlify-light.svg"
+            alt="Deploys by Netlify"
+          />
+        </a>
       </div>
     </div>
   );

next-react-query-tailwind/src/styles/globals.css
@@ -2,6 +2,10 @@
 @tailwind components;
 @tailwind utilities;
 
+html, body, div#__next {
+  height: 100%;
+}
+
 .prose :where(code):not(:where([class~="not-prose"] *))::before,
 .prose :where(code):not(:where([class~="not-prose"] *))::after {
   content: ""