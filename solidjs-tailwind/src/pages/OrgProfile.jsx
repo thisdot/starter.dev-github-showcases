66f6112123ad2dd9d3b5c47d2a68aa82f1d88239
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-12-02 14:53:18 +0100

    reduce the z index as it is blocking user dropdown

--- solidjs-tailwind/src/pages/OrgProfile.jsx
+++ solidjs-tailwind/src/pages/OrgProfile.jsx
@@ -35,7 +35,7 @@ const OrgProfile = () => {
 
   return (
     <div class="relative pt-4">
-      <div class="border-b border-gray-200 sticky top-0 bg-white hidden md:block z-50">
+      <div class="border-b border-gray-200 sticky top-0 bg-white hidden md:block z-30">
         <div class="grid grid-cols-12 max-w-screen-lg mx-auto">
           <div class="col-span-12 md:col-span-8 xl:col-span-12 px-2">
             <OrgAbout {...orgInfo()} />
