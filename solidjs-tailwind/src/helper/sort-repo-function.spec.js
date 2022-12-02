3e2bc2a1c12f2e607bfe912b491ec27903699f41
SDaian <scuarissid@gmail.com>
2022-12-02 18:25:58 +0300

    fix conflicts

--- solidjs-tailwind/src/helper/sort-repo-function.spec.js
+++ solidjs-tailwind/src/helper/sort-repo-function.spec.js
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { SORT_OPTIONS } from '../components/RepoFilter/data';
 import { sortedRepoData } from './sort-repo-function';
 
