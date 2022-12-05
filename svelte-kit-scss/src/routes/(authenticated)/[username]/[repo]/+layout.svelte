20836507d84d8698b39f32f303135a6bef39d7c7
Ian Mungai <iansam74@gmail.com>
2022-12-05 13:50:14 +0300

    chore: repo header test

--- svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/+layout.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/[username]/[repo]/+layout.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
-  import RepoHeader from '$lib/components/Repo/RepoHeader/RepoHeader.svelte';
+  import RepoHeader from '$lib/components/RepoHeader/RepoHeader.svelte';
   import type { LayoutServerData } from './$types';
 
   export let data: LayoutServerData;
   const { repoInfo } = data;
 </script>
 
-<RepoHeader repo={repoInfo} />
 <div class="container">
-  <slot />
+  <RepoHeader repo={repoInfo} />
+  <div class="main">
+    <slot />
+  </div>
 </div>
 
 <style lang="scss">
+  .container {
+    .main {
+      padding: 1.5rem;
+    }
+  }
 </style>
