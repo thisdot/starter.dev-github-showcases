1db338f1f3a0ede0b8c41dcb3c89ef7131553a0d
Ian Mungai <iansam74@gmail.com>
2022-12-05 17:00:18 +0300

    chore: test navbar

--- svelte-kit-scss/src/routes/(authenticated)/+layout.svelte
+++ svelte-kit-scss/src/routes/(authenticated)/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import NavBar from '$lib/components/NavBar/NavBar.svelte';
   import type { LayoutServerData } from './$types';
+  import { browser } from '$app/environment';
 
   export let data: LayoutServerData;
 </script>
 
 <div class="page">
   <header class="nav">
-    <NavBar username={data?.userInfo.username} userAvatar={data?.userInfo.avatar} />
+    <NavBar username={data?.userInfo.username} userAvatar={data?.userInfo.avatar} {browser} />
   </header>
   <main class="main">
     <slot />
