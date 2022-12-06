16442d972a9ea522492b5f0c054d5fbe8949e88b
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-12-06 23:57:46 +0300

    module fix

--- svelte-kit-scss/src/lib/interfaces/index.ts
+++ svelte-kit-scss/src/lib/interfaces/index.ts
@@ -2,7 +2,6 @@ export * from './repositories';
 export * from './gists';
 export * from './user';
 export * from './repository';
-export * from './repository-contents';
 export * from './branches';
 export * from './data-contract/github';
 export * from './issues';
