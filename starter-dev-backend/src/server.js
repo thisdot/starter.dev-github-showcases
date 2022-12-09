563b555aa85f016e4982aba9999de50c764e2ede
Dustin Goodman <dustin@thisdot.co>
2022-12-09 12:04:46 -0600

    fix backend routing issues (#1048)
    * fix express server routing
    
    * update redirect rules
    
    * add back deleted routes

--- starter-dev-backend/src/server.js
+++ starter-dev-backend/src/server.js
@@ -3,7 +3,6 @@ import serverless from 'serverless-http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import path from 'path';
 import {
   accessToken,
   clearCookies,
@@ -15,6 +14,32 @@ import cookieParser from 'cookie-parser';
 export const app = express();
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  return res.status(200).json({
+    message: 'Welcome to the starter.dev backend!',
+  });
+});
+
+router.get('/hello', (req, res) => {
+  if (!req.query.greeting) {
+    res.send('Hello, there');
+  }
+  res.send(`Hello, ${req.query.greeting}`);
+});
+
+// Step 1 - push user to Github OAuth
+router.get('/api/auth/signin', fetchSigninUrl);
+
+// Step 2 - verify code and state then fetch token and save it as a cookie
+// TODO: replace Github OAuth app with new endpoint -> `https://<host>/api/auth/signin/callback`
+router.get('/api/auth/signin/callback', accessToken);
+
+// Step 3 - client fetches token from cookie
+router.get('/api/auth/token', getAccessToken);
+
+router.post('/api/auth/signout', clearCookies);
+
+// middleware configs
 app.use(
   cors({
     credentials: true,
@@ -35,24 +60,5 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
 
-router.get('/', (req, res) => {
-  res.redirect(303, `https://starter.dev`);
-});
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-// Step 1 - push user to Github OAuth
-app.get('/api/auth/signin', fetchSigninUrl);
-
-// Step 2 - verify code and state then fetch token and save it as a cookie
-// TODO: replace Github OAuth app with new endpoint -> `https://<host>/api/auth/signin/callback`
-app.get('/api/auth/signin/callback', accessToken);
-
-// Step 3 - client fetches token from cookie
-app.get('/api/auth/token', getAccessToken);
-
-app.post('/api/auth/signout', clearCookies);
-
-// Step 1 - push user to Github OAuth
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
+export default app;
 export const handler = serverless(app);
