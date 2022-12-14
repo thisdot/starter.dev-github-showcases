8d3520b916788bd285855dc9b496a4818c23edcb
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-12-14 22:35:51 +0200

    fix: Sveltekit netlify adaptor (#1065)
    * fix: Sveltekit netlify adaptor
    
    * fix: resolve npm conflict
    
    * fix: adjust the adaptor version
    
    * fix: adjust the adaptor version
    
    * fix: adjust the adaptor version

--- svelte-kit-scss/svelte.config.js
+++ svelte-kit-scss/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
   preprocess: preprocess({ typescript: true, scss: true }),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      edge: true,
+    }),
   },
 };
 
