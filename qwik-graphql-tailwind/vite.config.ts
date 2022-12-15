280db0263449ec7e795a97fde4a6a8e8facc7d6b
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2022-12-15 21:10:12 +0200

    fix: add qwik react integrations (#1075)
    * fix: add qwik react integrations
    
    * fix: a typo
    
    * add static adaptor

--- qwik-graphql-tailwind/vite.config.ts
+++ qwik-graphql-tailwind/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { qwikReact } from '@builder.io/qwik-react';
+import { qwikReact } from '@builder.io/qwik-react/vite';
 
 export default defineConfig(() => {
   return {
