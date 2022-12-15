5f2f5c8e8d08c870780842a5437beebf36715460
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-12-15 20:49:21 +0200

    fix: add qwik react integrations

--- qwik-graphql-tailwind/vite.config.ts
+++ qwik-graphql-tailwind/vite.config.ts
@@ -3,10 +3,11 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { qwikReact } from '@builder.io/qwik-react';
+import { qwikReact } from '@builder.io/qwik-react/vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), qwikReact(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), qwikReact(), tsconfigPaths(), qwikReact()],
     test: {
       // ...
     },
