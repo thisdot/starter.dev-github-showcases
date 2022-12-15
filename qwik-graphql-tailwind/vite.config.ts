46a76c03d4e16ccc9be5a75d124c12e7faf468e8
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-12-15 20:55:23 +0200

    fix: a typo

--- qwik-graphql-tailwind/vite.config.ts
+++ qwik-graphql-tailwind/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { qwikReact } from '@builder.io/qwik-react';
 import { qwikReact } from '@builder.io/qwik-react/vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), qwikReact(), tsconfigPaths(), qwikReact()],
+    plugins: [qwikCity(), qwikVite(), qwikReact(), tsconfigPaths()],
     test: {
       // ...
     },
