9309daba98fca0f1b4bd1c6c53a6d446d6c2c56e
Daian Scuarissi <scuarissid@gmail.com>
2022-12-16 17:05:19 +0100

    Update getLanguages.js

--- solidjs-tailwind/src/helper/getLanguages.js
+++ solidjs-tailwind/src/helper/getLanguages.js
@@ -1,6 +1,5 @@
 export function getLanguages(repos) {
   let languages = new Set(['All']);
-  console.log(repos);
 
   repos.forEach((repo) => {
     if (repo.primaryLanguage && repo.primaryLanguage.name) {
