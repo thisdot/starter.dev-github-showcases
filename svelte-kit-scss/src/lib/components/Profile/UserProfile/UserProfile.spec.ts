1908383aae843051133378f99603a3d12e3717a4
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-12-20 20:36:45 +0300

    Profile page: User Profile

---
+++ svelte-kit-scss/src/lib/components/Profile/UserProfile/UserProfile.spec.ts
@@ -0,0 +1,34 @@
+import { beforeEach, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import UserProfile from './UserProfile.svelte';
+import { remapPublicProfileInformation, remapOrganizationSimple } from '$lib/helpers';
+import { MOCK_GITHUB_PUBLIC_PROFILE_INFORMATION } from '$lib/helpers/mocks/users';
+import { MOCK_ORGANIZATION_SIMPLE_ARRAY } from '$lib/helpers/mocks/organizations';
+
+const mockUserProfile = remapPublicProfileInformation(MOCK_GITHUB_PUBLIC_PROFILE_INFORMATION);
+const mockOrganizations = MOCK_ORGANIZATION_SIMPLE_ARRAY.map(remapOrganizationSimple);
+
+describe('UserProfile', () => {
+  beforeEach(() => {
+    render(UserProfile, {
+      profile: mockUserProfile,
+      organizations: mockOrganizations,
+    });
+  });
+
+  it.each([
+    ['name', mockUserProfile.name],
+    ['login', mockUserProfile.login],
+    ['bio', mockUserProfile.login],
+    // ['followers', mockUserProfile.followers],
+    // ['following', mockUserProfile.following],
+    // ['email', mockUserProfile.email],
+    // ['blog', mockUserProfile.blog],
+    // ['twitterUsername', mockUserProfile.twitterUsername],
+    // ['location', mockUserProfile.location],
+  ])('should render: %s', (testId, expectedValue) => {
+    const element = screen.getByTestId(testId);
+    const expectedText = String(expectedValue);
+    expect(element.innerHTML).toEqual(expectedText);
+  });
+});
---
+++ svelte-kit-scss/src/lib/components/Profile/common/ProfileFollowers.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { People16 } from 'svelte-octicons';
+  export let followers: number;
+  export let following: number | undefined = undefined;
+
+  $: followingDefined = typeof following === typeof Number();
+</script>
+
+<div class="profile-followers">
+  <People16 />
+  <span class="label">
+    <span class="count" data-testid="followers">{followers}</span> followers</span
+  >
+  {#if followingDefined}
+    <span class="separator-dot">&#x2022;</span>
+    <span class="label">
+      <span class="count" data-testid="following">{following}</span> following</span
+    >
+  {/if}
+</div>
+
+<style lang="scss">
+  .profile-followers {
+    display: flex;
+    align-items: center;
+    gap: 0.25em;
+    flex-wrap: wrap;
+    .label {
+      display: flex;
+      align-items: center;
+      gap: 0.25em;
+      .count {
+        font-weight: 600;
+      }
+    }
+    .separator-dot {
+      justify-content: center;
+      text-align: center;
+    }
+  }
+</style>
