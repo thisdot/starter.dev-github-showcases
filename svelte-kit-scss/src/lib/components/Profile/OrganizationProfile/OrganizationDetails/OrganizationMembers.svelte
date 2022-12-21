9acd76e2da869412c92a97bb3f8a7086cc65c3a7
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-12-21 17:11:05 +0300

    Profile page: Organization Profile

---
+++ svelte-kit-scss/src/lib/components/Profile/OrganizationProfile/OrganizationDetails/OrganizationMembers.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { SimpleUser } from '$lib/interfaces';
+
+  export let members: SimpleUser[];
+</script>
+
+<div class="organization-members">
+  {#each members as member}
+    <a class="member-link" href="/{member.login}">
+      <img class="image" src={member.avatarUrl} alt={member.login} />
+    </a>
+  {/each}
+</div>
+
+<style lang="scss">
+  @use 'src/lib/styles/variables.scss';
+  .organization-members {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.25em;
+    .member-link {
+      text-decoration: none;
+      width: 2rem;
+      height: 2rem;
+      border: 1px solid variables.$gray300;
+      border-radius: 100%;
+      overflow: hidden;
+      .image {
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+  }
+</style>
--- svelte-kit-scss/src/lib/helpers/router.ts
+++ svelte-kit-scss/src/lib/helpers/router.ts
@@ -1,4 +1,6 @@
-import type { Repository } from '$lib/interfaces';
+import type { GithubSimpleUser, Repository } from '$lib/interfaces';
 
 export const resolveRepositoryHref = (repository: Repository): string =>
   `/${repository.owner.login}/${repository.name}`;
+
+export const resolveUserHref = (user: GithubSimpleUser): string => `/${user.login}`;
