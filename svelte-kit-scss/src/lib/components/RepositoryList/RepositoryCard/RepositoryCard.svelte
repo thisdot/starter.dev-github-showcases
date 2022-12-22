58bae423397c2d04b497a6ecd52d9f6c7c16ac48
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-12-22 14:53:10 +0300

    [svelte-kit-scss] On repos fix tag styling on side of screen#1119

--- svelte-kit-scss/src/lib/components/RepositoryList/RepositoryCard/RepositoryCard.svelte
+++ svelte-kit-scss/src/lib/components/RepositoryList/RepositoryCard/RepositoryCard.svelte
@@ -3,6 +3,7 @@
   import { Law16, Star16, RepoForked16 } from 'svelte-octicons';
   import { LANGUAGE_COLORS } from '$lib/constants/language-colors';
   import type { RepositoryCardViewModel } from '../view-models';
+  import ProgrammingLanguage from '$lib/components/shared/ProgrammingLanguage/ProgrammingLanguage.svelte';
 
   export let model: RepositoryCardViewModel;
 
@@ -16,111 +17,163 @@
     updatedAt,
     license,
     routeHref: href,
+    topics,
   } = model);
-
-  function getLanguageColor(language: string | null | undefined): string {
-    return LANGUAGE_COLORS[String(language)];
-  }
 </script>
 
-<h3 class="name-container" data-testid="repo-card">
-  <a class="name" {href} data-testid="name">{name}</a>
-  <span class="visibility" data-testid="visibility">{visibility}</span>
-</h3>
-{#if description}
-  <p class="description" data-testid="description">{description}</p>
-{/if}
-<div class="subline">
-  {#if language}
-    <div class="language">
-      <div class="language-color" style="background-color: {getLanguageColor(language)}" />
-      <div class="language-label" data-testid="language">{language}</div>
+<div class="repository-card">
+  <div class="body">
+    <div class="title">
+      <h3>
+        <a class="link" {href} data-testid="name">{name}</a>
+        <span class="visibility" data-testid="visibility">{visibility}</span>
+      </h3>
     </div>
-  {/if}
-  <div class="stars">
-    <Star16 />
-    <span class="star-count" data-testid="stargazersCount">{stargazersCount}</span>
+    {#if description}
+      <div class="description">
+        <span data-testid="description">{description}</span>
+      </div>
+    {/if}
+    <!-- {#if topics}
+      <div class="topics">
+        <span class="description" data-testid="description">{description}</span>
+      </div>
+    {/if} -->
   </div>
-  <div class="stars">
-    <RepoForked16 />
-    <span class="star-count" data-testid="forksCount">{forksCount}</span>
-  </div>
-  {#if license}
-    <div class="license">
-      <Law16 />
-      <span class="license-name" data-testid="license_name">{license.name}</span>
+  <div class="footer">
+    {#if language}
+      <div class="entry language">
+        <ProgrammingLanguage name={language} />
+      </div>
+    {/if}
+    <div class="entry stars">
+      <Star16 />
+      <span class="count" data-testid="stargazersCount">{stargazersCount}</span>
     </div>
-  {/if}
-  {#if updatedAt}
-    <div class="updated" data-testid="updated">{relativeTimeFmt(updatedAt)}</div>
-  {/if}
+    <div class="entry forks">
+      <RepoForked16 />
+      <span class="count" data-testid="forksCount">{forksCount}</span>
+    </div>
+    {#if license}
+      <div class="entry license">
+        <Law16 />
+        <span class="license-name" data-testid="license_name">{license.name}</span>
+      </div>
+    {/if}
+    {#if updatedAt}
+      <div class="entry updated" data-testid="updated">
+        <span>Updated <span>{relativeTimeFmt(updatedAt)}</span></span>
+      </div>
+    {/if}
+  </div>
 </div>
 
 <style lang="scss">
   @use 'src/lib/styles/variables.scss';
-
-  .name-container {
+  .repository-card {
     display: flex;
-    margin: 0 0 0.5rem 0;
-
-    .name {
-      font-size: 1.25rem;
-      line-height: 1.75em;
-      font-weight: 600;
-      color: rgba(37, 99, 235, 1);
+    flex-direction: column;
+    gap: 1em;
+    .body {
+      overflow-wrap: break-word;
+      .title {
+        h3 {
+          font-size: 1em;
+          margin: 0;
+          .link {
+            font-weight: 600;
+            color: variables.$blue600;
+          }
+          .visibility {
+            font-size: 0.75em;
+            font-weight: 500;
+            text-transform: capitalize;
+            border: 1px solid variables.$gray300;
+            line-height: 1.5em;
+            padding: 0 0.5em;
+            border-radius: 0.75em;
+          }
+        }
+      }
+      .description {
+        font-size: 0.875em;
+      }
     }
-
-    .visibility {
-      display: inline-block;
-      padding: 0 7px;
-      margin: 2px 2px 2px 8px;
-      font-size: 0.9rem;
-      font-weight: 500;
-      line-height: 1.8em;
-      color: rgba(75, 85, 99, 1);
-      border: 1px solid rgba(209, 213, 219, 1);
-      border-radius: 2em;
-    }
-  }
-
-  .description {
-    margin: 8px auto;
-  }
-
-  .subline {
-    display: flex;
-    gap: 16px;
-    font-size: 0.85rem;
-    line-height: 1.4em;
-    color: rgba(75, 85, 99, 1);
-
-    .language {
+    .footer {
+      font-size: 0.75em;
       display: flex;
-      align-items: center;
-
-      .language-color {
-        display: inline-block;
-        width: 12px;
-        height: 12px;
-        border-radius: 50%;
-        margin-right: 8px;
-      }
-    }
-
-    .stars {
-      display: flex;
-      align-items: center;
-      line-height: 1.4em;
-
-      .star-count {
-        margin-left: 4px;
-      }
-    }
-
-    .license {
-      .license-name {
-        margin-left: 6px;
+      flex-wrap: wrap;
+      gap: 1em;
+      .entry {
+        display: flex;
+        align-items: center;
+        gap: 0.25em;
       }
     }
   }
+
+  // .name-container {
+  //   display: flex;
+  //   margin: 0 0 0.5rem 0;
+
+  //   .name {
+  //     font-size: 1.25rem;
+  //     line-height: 1.75em;
+  //     font-weight: 600;
+  //     color: rgba(37, 99, 235, 1);
+  //   }
+
+  //   .visibility {
+  //     display: inline-block;
+  //     padding: 0 7px;
+  //     margin: 2px 2px 2px 8px;
+  //     font-size: 0.9rem;
+  //     font-weight: 500;
+  //     line-height: 1.8em;
+  //     color: rgba(75, 85, 99, 1);
+  //     border: 1px solid rgba(209, 213, 219, 1);
+  //     border-radius: 2em;
+  //   }
+  // }
+
+  // .description {
+  //   margin: 8px auto;
+  // }
+
+  // .subline {
+  //   display: flex;
+  //   gap: 16px;
+  //   font-size: 0.85rem;
+  //   line-height: 1.4em;
+  //   color: rgba(75, 85, 99, 1);
+
+  //   .language {
+  //     display: flex;
+  //     align-items: center;
+
+  //     .language-color {
+  //       display: inline-block;
+  //       width: 12px;
+  //       height: 12px;
+  //       border-radius: 50%;
+  //       margin-right: 8px;
+  //     }
+  //   }
+
+  //   .stars {
+  //     display: flex;
+  //     align-items: center;
+  //     line-height: 1.4em;
+
+  //     .star-count {
+  //       margin-left: 4px;
+  //     }
+  //   }
+
+  //   .license {
+  //     .license-name {
+  //       margin-left: 6px;
+  //     }
+  //   }
+  // }
 </style>
---
+++ svelte-kit-scss/src/lib/components/shared/ProgrammingLanguage/ProgrammingLanguage.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { LANGUAGE_COLORS } from '$lib/constants/language-colors';
+  export let name: string;
+
+  function getLanguageColor(name: string | null | undefined): string {
+    return LANGUAGE_COLORS[String(name)];
+  }
+</script>
+
+<div class="programming-language">
+  <span class="marker" style="background-color: {getLanguageColor(name)}" />
+  <span class="label">{name}</span>
+</div>
+
+<style lang="scss">
+  .programming-language {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    gap: 0.25em;
+    .marker {
+      height: 1em;
+      width: 1em;
+      border-radius: 100%;
+    }
+  }
+</style>
