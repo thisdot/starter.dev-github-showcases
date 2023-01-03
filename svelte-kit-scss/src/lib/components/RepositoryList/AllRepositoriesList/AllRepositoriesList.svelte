ea47b87fc47ad6cf7601e8c7215e45b38843dca2
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-01-03 16:56:30 +0300

    [svelte-kit-scss] Add empty state on search on profile (#1157)
    * [svelte-kit-scss] Add empty state on search on profile#1124
    
    * Linting fix

--- svelte-kit-scss/src/lib/components/RepositoryList/AllRepositoriesList/AllRepositoriesList.svelte
+++ svelte-kit-scss/src/lib/components/RepositoryList/AllRepositoriesList/AllRepositoriesList.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import BoxLayout from '$lib/components/shared/layouts/BoxLayout.svelte';
+  import BlankSlate from '../RepositoryList/BlankSlate.svelte';
   import RepositoryList from '../RepositoryList/RepositoryList.svelte';
   import RepositoryListControls from '../RepositoryListFilters/RepositoryListControls.svelte';
   import type { AllRepositoriesListViewModel } from '../view-models';
 
   export let model: AllRepositoriesListViewModel;
   $: ({ repositories: items, controls } = model);
+  $: hasSelectedFilters = controls.resetFiltersHref;
+  $: hasItemsToShow = Boolean(items.length);
 </script>
 
 <BoxLayout>
   <RepositoryListControls slot="top" {controls} />
-  <RepositoryList {items} />
-  <!-- todo: add pagination here (slot="bottom")-->
+  {#if hasItemsToShow}
+    <RepositoryList {items} />
+    <!-- todo: add pagination here (slot="bottom")-->
+  {/if}
 </BoxLayout>
+
+{#if !hasItemsToShow}
+  {#if hasSelectedFilters}
+    <BlankSlate heading="The profile doesn't have any repositories that match." />
+  {:else}
+    <BlankSlate heading="The profile doesn't have any repositories yet." />
+  {/if}
+{/if}
