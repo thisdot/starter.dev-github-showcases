2f9a16fda4e2d7ae372763e759366efb68d8d467
SDaian <scuarissid@gmail.com>
2023-01-05 12:47:46 +0100

    feat(solidjs-pod): Add missing repo header

--- solidjs-tailwind/src/components/RepoHeading/RepoHeading.jsx
+++ solidjs-tailwind/src/components/RepoHeading/RepoHeading.jsx
@@ -2,29 +2,36 @@ import { Link } from '@solidjs/router';
 import PrivacyIcon from './PrivacyIcon';
 import { PrivacyBadge } from '../PrivacyBadge';
 import styles from './RepoHeading.module.css';
+import { useRepo } from '../../contexts/RepoContext';
+import { useParams } from '@solidjs/router';
 
-function RepoHeading(props) {
+function RepoHeading() {
+  const { info } = useRepo();
+  const params = useParams();
+
   return (
     <h1 class={styles.heading}>
-      <PrivacyIcon isPrivate={props.data?.isPrivate} />
+      <PrivacyIcon isPrivate={info().info?.isPrivate} />
       <span class={styles.navContainer}>
         <Link
-          href={props.data?.isOrg ? `/orgs/${props.owner}` : `/${props.owner}`}
+          href={
+            info().info?.isOrg ? `/orgs/${params.owner}` : `/${params.owner}`
+          }
         >
           <a data-testid="header owner name" class={styles.ownerLink}>
-            {props.owner}
+            {params.owner}
           </a>
         </Link>
         <span class={styles.separator}>/</span>
-        <Link href={`/${props.owner}/${name}`}>
+        <Link href={`/${params.owner}/${params.name}`}>
           <a data-testid="header repo name" class={styles.nameLink}>
-            {props.name}
+            {params.name}
           </a>
         </Link>
       </span>
-      {props.data ? (
+      {info().info ? (
         <PrivacyBadge
-          visibility={props.data.isPrivate ? 'Private' : 'Public'}
+          visibility={info().info?.isPrivate ? 'Private' : 'Public'}
         />
       ) : (
         <div class={styles.badgePlaceholder} />
