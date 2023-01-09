0e86e73e6ddab56460aeb1f1debfdc99a6978dac
SDaian <scuarissid@gmail.com>
2023-01-09 14:09:25 +0100

    Add highlight code

--- solidjs-tailwind/src/components/FileViewer/FileCode.jsx
+++ solidjs-tailwind/src/components/FileViewer/FileCode.jsx
@@ -1,42 +1,13 @@
-import { For } from 'solid-js';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import cn from 'classnames';
-import styles from './FileViewer.module.css';
+import Highlight from "solid-highlight";
+import "highlight.js/styles/stackoverflow-light.css";
 
-const FileCode = (props) => {
+
+function FileCode (props){
   return (
-    <Highlight {...defaultProps} code={props.text} language={props.language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          data-testid="code-block"
-          class={cn(styles.codeBlock, className)}
-          style={style}
-        >
-          <For each={tokens}>
-            {(line, i) => {
-              const { className: defaultClassName, ...lineProps } =
-                getLineProps({
-                  line,
-                  key: i,
-                });
-              return (
-                <div class={cn('table-row', defaultClassName)} {...lineProps}>
-                  <span class={styles.lineNumber}>{i() + 1}</span>
-                  <span class="table-cell">
-                    <For each={line}>
-                      {(token, key) => (
-                        <span {...getTokenProps({ token, key })} />
-                      )}
-                    </For>
-                  </span>
-                </div>
-              );
-            }}
-          </For>
-        </pre>
-      )}
+    <Highlight language={props.language}>
+      {props.text}
     </Highlight>
   );
-};
+}
 
 export default FileCode;
