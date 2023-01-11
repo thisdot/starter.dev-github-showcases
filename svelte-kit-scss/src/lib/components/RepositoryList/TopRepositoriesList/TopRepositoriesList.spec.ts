059ea6284ecc2a8e664b93f135e93e4f84219c84
Ian Sam Mungai <ian.mungai@thisdot.co>
2023-01-11 09:29:58 +0300

    chore: all repository files (#1208)

--- svelte-kit-scss/src/lib/components/RepositoryList/TopRepositoriesList/TopRepositoriesList.spec.ts
+++ svelte-kit-scss/src/lib/components/RepositoryList/TopRepositoriesList/TopRepositoriesList.spec.ts
@@ -1,19 +1,39 @@
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import TopRepositoriesList from './TopRepositoriesList.svelte';
 import type { TopRepositoriesListViewModel } from '../view-models';
 import { MOCK_REPOSITORY_CARD_VIEW_MODELS } from '../mocks';
 
-const MOCK_VIEW_MODEL: TopRepositoriesListViewModel = {
-  repositories: MOCK_REPOSITORY_CARD_VIEW_MODELS,
-  viewAllRouteHref: '/mock_href_view_all',
-};
-
 describe('TopRepositories', () => {
-  // todo: add tests
-  it('should create a component', () => {
-    const { component } = render(TopRepositoriesList, {
-      model: MOCK_VIEW_MODEL,
+  describe('handle view all repositories', () => {
+    it('should show a link to the view all repositories', () => {
+      const viewAllRouteHref = '/mock_href_view_all';
+      const MOCK_VIEW_MODEL: TopRepositoriesListViewModel = {
+        repositories: MOCK_REPOSITORY_CARD_VIEW_MODELS,
+        viewAllRouteHref,
+      };
+
+      const { container } = render(TopRepositoriesList, {
+        model: MOCK_VIEW_MODEL,
+      });
+
+      const viewAllLink = container.getElementsByClassName('link-text')[0] as HTMLAnchorElement;
+      expect(viewAllLink.href).toContain(viewAllRouteHref);
     });
-    expect(component).toBeTruthy();
+  });
+
+  describe('handle no repositories', () => {
+    it('should show no repositories', () => {
+      const MOCK_VIEW_MODEL: TopRepositoriesListViewModel = {
+        repositories: [],
+      };
+
+      render(TopRepositoriesList, {
+        model: MOCK_VIEW_MODEL,
+      });
+
+      const noRepositoriesElement = screen.getByText('No repositories');
+
+      expect(noRepositoriesElement).toBeTruthy();
+    });
   });
 });
