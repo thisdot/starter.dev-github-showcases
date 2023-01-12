cc94359c8508ebdbf45dad0b1ac917fe005093fc
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-01-12 12:28:22 +0300

    [svelte-kit-scss] Clicking on links has no progress indicator, you don't know if it's loading or not#1197

--- svelte-kit-scss/src/routes/+layout.svelte
+++ svelte-kit-scss/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import NavigationProgress from '$lib/components/NavigationProgress/NavigationProgress.svelte';
   import { configureServices } from '$lib/services/configure-services';
   import './styles.scss';
 
   configureServices();
 </script>
 
+<NavigationProgress />
 <slot />
