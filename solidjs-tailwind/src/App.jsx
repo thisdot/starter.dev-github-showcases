797f08d5fba0945f367d2fa5cc5999182a06f386
Jerry Hogan <jerry.hogan@thisdot.co>
2023-01-12 08:56:20 +0100

    fix lint issue

--- solidjs-tailwind/src/App.jsx
+++ solidjs-tailwind/src/App.jsx
@@ -24,10 +24,10 @@ function App() {
           <Route component={Profile} path={ROUTES.PROFILE} />
           <Route component={OrgProfile} path={ROUTES.ORGPROFILE} />
           <Route component={Repo} path={ROUTES.REPO_DETAILS}>
-            <Route component={RepoDetails} path={'/'}  />
-            <Route component={RepoTree} path={ROUTES.REPO_TREE}  />
+            <Route component={RepoDetails} path={'/'} />
+            <Route component={RepoTree} path={ROUTES.REPO_TREE} />
             {/* <Route component={RepoBlob} path={ROUTES.REPO_BLOB} /> */}
-            <Route component={PullReqAndIssues} path={ROUTES.REPO_PULLS} />
+            <Route component={PullRequests} path={ROUTES.REPO_PULLS} />
             <Route component={Issues} path={ROUTES.REPO_ISSUES} />
           </Route>
         </Route>
