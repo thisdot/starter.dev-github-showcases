f612468a0b8be44141dde3ea86ebea16a5ea23fa
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2023-01-13 03:18:07 +0100

    created pagination component and added pagination in profile page (#1228)

--- solidjs-tailwind/src/components/ProfilePage/ProfilePage.jsx
+++ solidjs-tailwind/src/components/ProfilePage/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import { useLocation, useParams } from '@solidjs/router';
 import { createEffect, createSignal, splitProps } from 'solid-js';
 import useRepoSortFilter from '../../utils/useRepoSortFilter';
 import { ProfileNav } from '../ProfileNav';
@@ -6,13 +7,18 @@ import { UserRepos } from '../UserRepos';
 import styles from './Profile.module.css';
 
 const ProfilePage = (props) => {
+  const location = useLocation();
+  const params = useParams();
+
   const [local] = splitProps(props, ['reposInfo', 'user']);
   const [repoState, setRepoState] = createSignal([]);
+  const [pageInfo, setPageInfo] = createSignal({});
   const [reposlanguages, setReposLanguages] = createSignal([]);
 
   createEffect(() => {
     const [reposResults, languages] = useRepoSortFilter(local.reposInfo.repos);
     setRepoState(reposResults);
+    setPageInfo(local.reposInfo.pageInfo);
     setReposLanguages(languages);
   });
 
@@ -40,7 +46,7 @@ const ProfilePage = (props) => {
               pathname={location.pathname}
             />
             {local.reposInfo ? (
-              <UserRepos languages={reposlanguages()} repos={repoState()} />
+              <UserRepos languages={reposlanguages()} repos={repoState()} pageInfo={pageInfo()} owner={params.login} />
             ) : null}
           </div>
         </div>
