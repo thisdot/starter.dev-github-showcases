4456af6df2cc9416d9dea8f8eb88a27b31d8db0c
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-01-17 01:53:03 +0300

    [SvelteKit-pod] Unit test Helpers folder: formatting#1001

--- svelte-kit-scss/src/lib/helpers/formatting.ts
+++ svelte-kit-scss/src/lib/helpers/formatting.ts
@@ -12,36 +12,33 @@ const MINUTE = 60,
  * Based on: https://gist.github.com/pomber/6195066a9258d1fb93bb59c206345b38?permalink_comment_id=3642456#gistcomment-3642456
  * @param dt ISO 8601 datetime string
  */
-export function relativeTimeFmt(dt: string | Date): string {
-  if (dt) {
-    const localizedDt = new Date(dt);
-    const secondsAgo = Math.round((new Date().getTime() - localizedDt.getTime()) / 1000);
+export function relativeTimeFmt(dateTimeIsoString: string): string {
+  const localizedDt = new Date(dateTimeIsoString);
+  const secondsAgo = Math.round((new Date().getTime() - localizedDt.getTime()) / 1000);
 
-    let divisor = 1;
-    let unit = '?';
-
-    if (secondsAgo < MINUTE) {
-      return secondsAgo + ' seconds ago';
-    } else if (secondsAgo < HOUR) {
-      [divisor, unit] = [MINUTE, 'minute'];
-    } else if (secondsAgo < DAY) {
-      [divisor, unit] = [HOUR, 'hour'];
-    } else if (secondsAgo < WEEK) {
-      [divisor, unit] = [DAY, 'day'];
-    } else if (secondsAgo < MONTH) {
-      [divisor, unit] = [WEEK, 'week'];
-    } else if (secondsAgo < YEAR) {
-      [divisor, unit] = [MONTH, 'month'];
-    } else if (secondsAgo >= YEAR) {
-      [divisor, unit] = [YEAR, 'year'];
-    }
-
-    const count = Math.floor(secondsAgo / divisor);
-    return `${count} ${unit}${count > 1 ? 's' : ''} ago`;
+  if (!dateTimeIsoString || isNaN(Number(localizedDt))) {
+    return '';
   }
-  return '';
-}
 
-export function titleCase(str: string): string {
-  return str.toLowerCase().replace(/\b\w/g, (s) => s.toUpperCase());
+  let divisor = 1;
+  let unit = '?';
+
+  if (secondsAgo < MINUTE) {
+    [divisor, unit] = [1, 'second'];
+  } else if (secondsAgo < HOUR) {
+    [divisor, unit] = [MINUTE, 'minute'];
+  } else if (secondsAgo < DAY) {
+    [divisor, unit] = [HOUR, 'hour'];
+  } else if (secondsAgo < WEEK) {
+    [divisor, unit] = [DAY, 'day'];
+  } else if (secondsAgo < MONTH) {
+    [divisor, unit] = [WEEK, 'week'];
+  } else if (secondsAgo < YEAR) {
+    [divisor, unit] = [MONTH, 'month'];
+  } else if (secondsAgo >= YEAR) {
+    [divisor, unit] = [YEAR, 'year'];
+  }
+
+  const count = Math.floor(secondsAgo / divisor);
+  return `${count} ${unit}${count > 1 ? 's' : ''} ago`;
 }
