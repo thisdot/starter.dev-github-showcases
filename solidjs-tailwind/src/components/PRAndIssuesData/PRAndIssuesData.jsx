b099a3b034338bc798c21f971062d08c6d2963a4
Mattia <32470461+Megio@users.noreply.github.com>
2023-01-23 18:37:16 +0100

    solid-fix: header tab in repo page and no result behaviour in issues and pr pages (#1316)

--- solidjs-tailwind/src/components/PRAndIssuesData/PRAndIssuesData.jsx
+++ solidjs-tailwind/src/components/PRAndIssuesData/PRAndIssuesData.jsx
@@ -5,21 +5,19 @@ import { PRAndIssuesHeader } from "../PRAndIssuesHeader";
 export const PRAndIssuesData = (props) => {
   return (
     <div class="border border-gray-300 rounded-lg">
-      {props.data.length > 0 ? (
-        <div>
-          <PRAndIssuesHeader {...props}/>
+      <PRAndIssuesHeader {...props}/>
+        {props.data.length > 0 ? (
           <For each={props.data}>
             {(data) => <PRAndIssuesListItem {...data()} />}
           </For>
-        </div>
-      ) : (
-        <div class="text-center p-3 flex flex-col items-center justify-center">
-          <span class="font-bold mb-4">No results matched your search.</span>
-          <a class=" text-blue-600 underline" href={location.pathname}>
-            Refresh
-          </a>
-        </div>
-      )}
+        ) : (
+          <div class="text-center p-3 flex flex-col items-center justify-center">
+            <span class="font-bold mb-4">No results matched your search.</span>
+            <a class=" text-blue-600 underline" href={location.pathname}>
+              Refresh
+            </a>
+          </div>
+        )}
     </div>
   );
 }
