3fd0c2bc8bab1c9c5009a67a34ba1f23bf5823d3
Mattia Magi <mattia.magi91@gmail.com>
2023-02-03 10:34:51 +0100

    feat: hide header in signin page

--- solidstart-tanstackquery-tailwind-modules/src/components/layout.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/layout.tsx
@@ -1,14 +1,20 @@
-import { JSXElement } from 'solid-js';
+import { JSXElement, Show } from 'solid-js';
 import Header from '../components/Header/header';
+import { useAuth } from '~/auth';
+import { StoreProps } from '~/auth/AuthStore';
 
 interface LayoutProps {
   children: JSXElement
 }
 
 export const Layout = (props: LayoutProps) => {
+  const { authStore }: { authStore: StoreProps } = useAuth();
+
   return (
     <>
-      <Header />
+      <Show when={authStore.isAuthenticated}>
+        <Header />
+      </Show>
       <main class="min-h-screen bg-gray-100">
         {props.children}
       </main>
