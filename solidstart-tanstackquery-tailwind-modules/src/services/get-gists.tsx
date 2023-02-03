bd47ec7ff4a1c93c5b723e1f4afc582bab1fdfc7
Mattia Magi <mattia.magi91@gmail.com>
2023-02-03 15:46:08 +0100

    refactor fetch func, using common fetchapi

---
+++ solidstart-tanstackquery-tailwind-modules/src/services/get-gists.tsx
@@ -0,0 +1,23 @@
+import { useAuth } from '~/auth';
+import { StoreProps } from '~/auth/AuthStore';
+import { GITHUB_GRAPHQL } from '~/utils/constants';
+import { USER_GISTS_QUERY } from './queries/gists';
+import FetchApi, { ApiProps } from './api';
+
+
+const getGists = async () => {
+  const { authStore }: { authStore: StoreProps } = useAuth();
+  const data: ApiProps<undefined> = {
+    url: `${GITHUB_GRAPHQL}`,
+    query: USER_GISTS_QUERY,
+    headersOptions: {
+      authorization: `Bearer ${authStore.token}`,
+    },
+  };
+
+  const resp = (await FetchApi(data)) as Response;
+
+  return resp
+};
+
+export default getGists;
--- solidstart-tanstackquery-tailwind-modules/src/services/get-user-repos.ts
+++ solidstart-tanstackquery-tailwind-modules/src/services/get-user-repos.ts
@@ -36,24 +36,24 @@ const getUserRepos = async (variables: UserReposVariables) => {
   const repos = nodes?.reduce((acc: UserRepo[], repo: UserRepo) => {
     return repo
       ? [
-          ...acc,
-          {
-            id: repo.id,
-            name: repo.name,
-            description: repo.description,
-            stargazerCount: repo.stargazerCount,
-            forkCount: repo.forkCount,
-            isFork: repo.isFork,
-            isArchived: repo.isArchived,
-            primaryLanguage: {
-              name: repo.primaryLanguage?.name,
-              color: repo.primaryLanguage?.color,
-            },
-            visibility: repo.visibility,
-            updatedAt: repo.updatedAt,
-            owner: repo.owner,
+        ...acc,
+        {
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          stargazerCount: repo.stargazerCount,
+          forkCount: repo.forkCount,
+          isFork: repo.isFork,
+          isArchived: repo.isArchived,
+          primaryLanguage: {
+            name: repo.primaryLanguage?.name,
+            color: repo.primaryLanguage?.color,
           },
-        ]
+          visibility: repo.visibility,
+          updatedAt: repo.updatedAt,
+          owner: repo.owner,
+        },
+      ]
       : acc;
   }, []);
 
