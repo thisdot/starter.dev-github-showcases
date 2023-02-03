95e50b6fc2676067e29cfa14d479373e914cf711
Mattia Magi <mattia.magi91@gmail.com>
2023-02-03 13:18:07 +0100

    format code

--- solidstart-tanstackquery-tailwind-modules/src/services/fetchGists.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/services/fetchGists.tsx
@@ -1,20 +1,20 @@
-import { useAuth } from "~/auth";
-import { StoreProps } from "~/auth/AuthStore";
-import { GITHUB_GRAPHQL } from "~/utils/constants";
-import { USER_GISTS_QUERY } from "./queries/gists";
+import { useAuth } from '~/auth';
+import { StoreProps } from '~/auth/AuthStore';
+import { GITHUB_GRAPHQL } from '~/utils/constants';
+import { USER_GISTS_QUERY } from './queries/gists';
 
 const { authStore }: { authStore: StoreProps } = useAuth();
 
 const fetchGists = async () => {
   const response = await fetch(`${GITHUB_GRAPHQL}`, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({ query: USER_GISTS_QUERY }),
     headers: {
       Authorization: `Bearer ${authStore.token}`,
-    }
+    },
   });
   const gists = await response.json();
   return gists;
 };
 
-export default fetchGists
+export default fetchGists;
