421eebcfc7cd23a7ce939d7e34580fd8c23d6277
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-03 16:13:05 +0100

    types

--- solidstart-tanstackquery-tailwind-modules/src/types/org-repos.ts
+++ solidstart-tanstackquery-tailwind-modules/src/types/org-repos.ts
@@ -1,25 +1,25 @@
 export interface Repository {
+  name: string;
+  description: string;
+  url: string;
+  forkCount: number;
+  stargazerCount: number;
+  primaryLanguage: {
+    color: string;
     name: string;
-    description: string;
-    url: string;
-    forkCount: number;
-    stargazerCount: number;
-    primaryLanguage: {
-        color: string;
-        name: string;
-        id: string;
-    };
-    updatedAt: string;
-    owner: {
-        login: string;
-    };
-    visibility: string;
+    id: string;
+  };
+  updatedAt: string;
+  owner: {
+    login: string;
+  };
+  visibility: string;
 }
 
 export interface OrgRepoInfo {
-    organization: {
-        avatarUrl: string;
-        name: string;
-        repositories: Repository[];
-    }
+  organization: {
+    avatarUrl: string;
+    name: string;
+    repositories: Repository[];
+  };
 }
