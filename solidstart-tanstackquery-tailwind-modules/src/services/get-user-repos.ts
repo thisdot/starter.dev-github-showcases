b155c40873906945ab60a63e44cf31694f41c4dd
Jerry Hogan <jerry.hogan@thisdot.co>
2023-02-03 15:07:46 +0100

    Solidstart/get all repos api (#1372)
    * fetch all repos api
    
    * fetch all repos api
    
    * testing if dep for msw-storybook-addon will cause CI to fail
    
    * testing if dep for msw-storybook-addon will cause CI to fail
    
    * testing if dep for msw-storybook-addon will cause CI to fail
    
    * testing if dep for msw-storybook-addon will cause CI to fail
    
    * testing if dep for msw-storybook-addon will cause CI to fail

---
+++ solidstart-tanstackquery-tailwind-modules/src/services/get-user-repos.ts
@@ -0,0 +1,59 @@
+import { useAuth } from '../auth';
+import FetchApi, { ApiProps } from './api';
+import { GITHUB_GRAPHQL } from '../utils/constants';
+import { USER_REPOS_QUERY } from './queries/user-repos';
+import { UserRepo, UserRepoInfo } from '~/types/user-repo-type';
+
+type Variables = Record<string, string | number | null> | null;
+type Response = {
+  data: UserRepoInfo;
+};
+
+const getUserRepos = async (variables: Variables) => {
+  const { authStore } = useAuth();
+  const data: ApiProps = {
+    url: `${GITHUB_GRAPHQL}`,
+    query: USER_REPOS_QUERY,
+    variables,
+    headersOptions: {
+      authorization: `Bearer ${authStore.token}`,
+    },
+  };
+  const resp = (await FetchApi(data)) as Response;
+  const nodes = resp.data?.owner?.repositories?.nodes;
+  const pageInfo = resp.data?.owner?.repositories?.pageInfo;
+
+  if (!nodes) {
+    return undefined;
+  }
+  const repos = nodes?.reduce((acc: UserRepo[], repo: UserRepo) => {
+    return repo
+      ? [
+          ...acc,
+          {
+            id: repo.id,
+            name: repo.name,
+            description: repo.description,
+            stargazerCount: repo.stargazerCount,
+            forkCount: repo.forkCount,
+            isFork: repo.isFork,
+            isArchived: repo.isArchived,
+            primaryLanguage: {
+              name: repo.primaryLanguage?.name,
+              color: repo.primaryLanguage?.color,
+            },
+            visibility: repo.visibility,
+            updatedAt: repo.updatedAt,
+            owner: repo.owner,
+          },
+        ]
+      : acc;
+  }, []);
+
+  return {
+    pageInfo,
+    repos,
+  };
+};
+
+export default getUserRepos;
