4d319455a0c5e208d534c4a16fdd9c251637c416
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-03 17:17:09 +0100

    add space

--- solidstart-tanstackquery-tailwind-modules/src/types/user-profile-type.ts
+++ solidstart-tanstackquery-tailwind-modules/src/types/user-profile-type.ts
@@ -1,25 +1,25 @@
 export interface UserProfile {
-    avatarUrl: string;
-    bio: string;
-    company: string;
-    followers: {
-        totalCount: number;
-    };
-    following: {
-        totalCount: number;
-    };
-    location: string;
-    login: string;
-    name: string;
-    organizations: {
-        nodes: {
-            avatarUrl: string;
-            login: string;
-        }[];
-    };
-    starredRepositories: {
-        totalCount: number;
-    };
-    twitterUsername: string;
-    websiteUrl: string;
+  avatarUrl: string;
+  bio: string;
+  company: string;
+  followers: {
+    totalCount: number;
+  };
+  following: {
+    totalCount: number;
+  };
+  location: string;
+  login: string;
+  name: string;
+  organizations: {
+    nodes: {
+      avatarUrl: string;
+      login: string;
+    }[];
+  };
+  starredRepositories: {
+    totalCount: number;
+  };
+  twitterUsername: string;
+  websiteUrl: string;
 }
