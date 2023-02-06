b76c417c6834726d77bab2febe0a833d84baf836
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-06 19:25:48 +0100

    add test

---
+++ solidstart-tanstackquery-tailwind-modules/storybook-static/assets/PrivacyBadge.stories.3f7fb64f.js
@@ -0,0 +1,14 @@
+var o=Object.defineProperty;var t=(e,a)=>o(e,"name",{value:a,configurable:!0});import{P as r}from"./PrivacyBadge.2f070d91.js";import{c as i}from"./web.9316b53f.js";const d={parameters:{storySource:{source:`import { createComponent as _$createComponent } from "solid-js/web";
+import PrivacyBadge from './PrivacyBadge';
+export default {
+  title: 'Components/PrivacyBadge',
+  argTypes: {
+    visibility: {}
+  }
+};
+const Template = args => _$createComponent(PrivacyBadge, args);
+export const Default = Template.bind({});
+Default.args = {
+  visibility: 'Private'
+};`,locationsMap:{default:{startLoc:{col:17,line:9},endLoc:{col:62,line:9},startBody:{col:17,line:9},endBody:{col:62,line:9}}}}},title:"Components/PrivacyBadge",argTypes:{visibility:{}}},n=t(e=>i(r,e),"Template"),s=n.bind({});s.args={visibility:"Private"};const m=["Default"];export{s as Default,m as __namedExportsOrder,d as default};
+//# sourceMappingURL=PrivacyBadge.stories.3f7fb64f.js.map
