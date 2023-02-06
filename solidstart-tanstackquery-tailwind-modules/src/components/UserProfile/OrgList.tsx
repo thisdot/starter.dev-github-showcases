8b2366428de9f3c074b0802d7d3ca4a978754b68
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-06 18:12:30 +0100

    change <a> to <A>

--- solidstart-tanstackquery-tailwind-modules/src/components/UserProfile/OrgList.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/UserProfile/OrgList.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from '@solidjs/router';
+import { A } from '@solidjs/router';
 import { For } from 'solid-js';
 
 interface OrgListProps {
@@ -9,21 +9,17 @@ interface OrgListProps {
 }
 
 function OrgList(props: OrgListProps) {
-  const navigate = useNavigate();
   return (
     <div class="mt-5 border-t border-gray-200">
       <h2 class="my-2 pt-2 text-gray-800 font-bold">Organizations</h2>
       <div data-testid="profile page orgs" class="flex flex-wrap space-x-2">
         <For each={props.organizations}>
           {(props) => (
-            <a
-              onClick={() => navigate(`/orgs/${props.login}`)}
-              class="cursor-pointer"
-            >
+            <A href={`/orgs/${props.login}`} class="cursor-pointer">
               <div class="relative w-9 h-9 rounded border border-gray-300 overflow-hidden">
                 <img src={props.avatarUrl} alt="Organization" />
               </div>
-            </a>
+            </A>
           )}
         </For>
       </div>
