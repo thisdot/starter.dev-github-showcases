200aa1dbcaf0c7d5a9c992ae1355cab40c893169
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2023-02-06 17:15:36 +0100

    Issues information API (#1377)
    * issues info api
    
    * liniting

---
+++ solidstart-tanstackquery-tailwind-modules/src/types/issues-type.ts
@@ -0,0 +1,89 @@
+export interface MilestoneProps {
+  id: string;
+  closed: boolean;
+  description: string;
+  number: number;
+  title: string;
+}
+
+export interface LabelProps {
+  color: string;
+  name: string;
+}
+
+export interface PageInfo {
+  startCursor: string;
+  endCursor: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+
+export interface IssueProps {
+  totalCount: number;
+  pageInfo: PageInfo;
+  nodes: IssueNodeProps[];
+}
+
+export interface IssueNodeProps {
+  state: string;
+  createdAt: string;
+  closedAt: string;
+  title: string;
+  author: {
+    login: string;
+  };
+  url: string;
+  labels: {
+    totalCount: number;
+    nodes: LabelProps[];
+  };
+  comments: {
+    totalCount: number;
+  };
+  number: number;
+}
+
+export interface IssuesInfo {
+  repository: {
+    milestones: {
+      nodes: MilestoneProps[];
+      pageInfo: PageInfo;
+      totalCount: number;
+    };
+    labels: {
+      totalCount: number;
+      nodes: LabelProps[];
+    };
+    openIssues: IssueProps;
+    closedIssues: IssueProps;
+  };
+}
+
+export interface Issue {
+  login: string;
+  commentCount: number;
+  labelCount: number;
+  labels: LabelProps[];
+  title: string;
+  number: number;
+  createdAt: string;
+  closedAt: string;
+  state: string;
+  url: string;
+}
+
+export type Variables = {
+  owner: string;
+  name: string;
+  orderBy: string;
+  direction: string;
+  filterBy: string;
+  before: string;
+  after: string;
+  first: string;
+  last: string;
+};
+
+export type Response = {
+  data: IssuesInfo;
+};
