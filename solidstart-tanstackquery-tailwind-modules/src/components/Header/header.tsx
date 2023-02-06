20b425e37f15e33007d9bf137afac062ee1a7ea2
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-06 16:57:58 +0100

    header update with user avater

--- solidstart-tanstackquery-tailwind-modules/src/components/Header/header.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/Header/header.tsx
@@ -1,12 +1,31 @@
 import { A } from '@solidjs/router';
+import { useAuth } from '../../auth';
 import { GitHubLogo } from './github-logo';
+import styles from './Header.module.css';
+import { SIGN_IN_BASE_URL } from '../../utils/constants';
+import { UserDropdown } from '../UserDropdown';
 
 const Header = () => {
+  const { authStore } = useAuth();
+
   return (
-    <header class="bg-gray-900 flex justify-between items-center py-4 px-8">
+    <header class={styles.header}>
       <A href="/">
         <GitHubLogo />
       </A>
+
+      <div>
+        {authStore.user ? (
+          <UserDropdown
+            image={authStore.user.avatarUrl}
+            username={authStore.user.login}
+          />
+        ) : (
+          <A href={`${SIGN_IN_BASE_URL}?redirect_url=${window.location.href}`}>
+            <span class={styles.navLink}>Sign In</span>
+          </A>
+        )}
+      </div>
     </header>
   );
 };
