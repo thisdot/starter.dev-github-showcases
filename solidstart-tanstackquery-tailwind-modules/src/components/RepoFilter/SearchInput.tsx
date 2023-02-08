4fef19999ffe7170f56c378a8d2ac477809e91a5
Mattia Magi <mattia.magi91@gmail.com>
2023-02-08 16:26:52 +0100

    solidstart-feat: added search functionality

--- solidstart-tanstackquery-tailwind-modules/src/components/RepoFilter/SearchInput.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/RepoFilter/SearchInput.tsx
@@ -1,17 +1,19 @@
 import styles from './RepoFilter.module.css';
 import { JSX } from 'solid-js';
+import { setSearch } from './RepoFilter.store';
 
 const SearchInput = () => {
-  const handleChange: JSX.EventHandler<HTMLInputElement, InputEvent> = () => {
-    // Serch functionality
+  const handleChange: JSX.EventHandler<HTMLInputElement, KeyboardEvent> = (e: KeyboardEvent) => {
+    setSearch((e.target as HTMLInputElement).value || '');
   };
+
   return (
     <input
       data-testid="search-input"
       placeholder="find a repository.."
       role="search"
       type="search"
-      onInput={handleChange}
+      onKeyUp={handleChange}
       class={styles.searchInput}
     />
   );
