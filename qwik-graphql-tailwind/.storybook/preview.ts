9424cbbe64bbcc97355a26786b2acce1bb01546a
Mattia Magi <mattia.magi91@gmail.com>
2023-02-08 16:06:39 +0100

    solidstart-feat: modified eslint rules adding typescript plugin

--- qwik-graphql-tailwind/.storybook/preview.ts
+++ qwik-graphql-tailwind/.storybook/preview.ts
@@ -1,5 +1,5 @@
 // Based on https://github.com/BuilderIO/qwik/discussions/787#discussioncomment-3715103
-import { JSXNode, useContextProvider } from '@builder.io/qwik';
+import { JSXNode } from '@builder.io/qwik';
 import { QWIK_LOADER } from '@builder.io/qwik/loader/index';
 import { render } from '@builder.io/qwik';
 import '../src/global.css';
