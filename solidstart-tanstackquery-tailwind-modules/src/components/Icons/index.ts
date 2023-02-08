b962fdd89836f566dbf89afa0d5a0dc513bad7e6
Jerry Hogan <jerry.hogan@thisdot.co>
2023-02-08 10:11:12 +0100

    in progress

--- solidstart-tanstackquery-tailwind-modules/src/components/Icons/index.ts
+++ solidstart-tanstackquery-tailwind-modules/src/components/Icons/index.ts
@@ -1,2 +1,6 @@
 export { default as GitBranchIcon } from './GitBranchIcon';
 export { default as StarIcon } from './StarIcon';
+export { default as PullRequestIcon } from './PullRequestIcon';
+export { default as InformationCircleIcon } from './InformationCircleIcon';
+export { default as CodeIcon } from './CodeIcon';
+export { default as EyeIcon } from './EyeIcon';
