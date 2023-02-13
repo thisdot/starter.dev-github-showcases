7ef469015a469fef011433a8875e4fcf1cd447f9
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-13 17:49:57 +0100

    update readme component to be more solid

--- solidstart-tanstackquery-tailwind-modules/src/components/RepoReadMe/RepoReadMe.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/RepoReadMe/RepoReadMe.tsx
@@ -1,14 +1,20 @@
 import { useLocation, useParams } from '@solidjs/router';
-import { createEffect, createSignal } from 'solid-js';
-import SolidMarkdown from 'solid-markdown';
+import { createEffect, createSignal, Match, Show, Switch } from 'solid-js';
+import { unstable_clientOnly } from 'solid-start';
+import { createQuery } from '@tanstack/solid-query';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
-import { Empty } from './Empty';
+
+import getReadme from '../../services/get-repo-readme';
 import { TocIcon } from '../Icons';
 
 import styles from './RepoReadMe.module.css';
-import { createQuery } from '@tanstack/solid-query';
-import getReadme from '../../services/get-repo-readme';
+import { Empty } from './Empty';
+
+// SolidMarkdown doesn't work with SSR so we need to use unstable_clientOnly
+// This causes storybook to not work with this component
+// But normal import works in storybook.
+const SolidMarkdown = unstable_clientOnly(() => import('solid-markdown'));
 
 const RepoReadMe = () => {
   const { pathname } = useLocation();
@@ -16,7 +22,7 @@ const RepoReadMe = () => {
   const [readme, setReadme] = createSignal<string>();
 
   const resReadMe = createQuery(
-    () => ['read-me'],
+    () => [`read-me_${params.owner}_${params.name}`],
     () =>
       getReadme({
         owner: params.owner,
@@ -34,26 +40,29 @@ const RepoReadMe = () => {
   });
 
   return (
-    <>
-      {readme() ? (
-        <div class={styles.container} data-testid="readme">
-          <header class={styles.header}>
-            <span class={styles.tocIconContainer}>
-              <TocIcon class={styles.tocIcon} />
-            </span>
-            <span class={styles.filename}>README.md</span>
-          </header>
-          <article class={styles.article}>
-            <SolidMarkdown
-              rehypePlugins={[rehypeRaw, remarkGfm]}
-              children={readme()}
-            />
-          </article>
-        </div>
-      ) : (
-        <>{!pathname.includes('tree') ? <Empty /> : null}</>
-      )}
-    </>
+    <Show when={!resReadMe.isLoading && resReadMe.data}>
+      <Switch>
+        <Match when={readme()}>
+          <div class={styles.container} data-testid="readme">
+            <header class={styles.header}>
+              <span class={styles.tocIconContainer}>
+                <TocIcon class={styles.tocIcon} />
+              </span>
+              <span class={styles.filename}>README.md</span>
+            </header>
+            <article class={styles.article}>
+              <SolidMarkdown
+                rehypePlugins={[rehypeRaw, remarkGfm]}
+                children={readme()}
+              />
+            </article>
+          </div>
+        </Match>
+        <Match when={!pathname.includes('tree')}>
+          <Empty />
+        </Match>
+      </Switch>
+    </Show>
   );
 };
 
