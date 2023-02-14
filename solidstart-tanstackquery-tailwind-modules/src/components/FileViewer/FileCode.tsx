8393a3f0ebbd237a932ce0d58d740bb92b86fe2c
Mattia Magi <mattia.magi91@gmail.com>
2023-02-14 15:37:34 +0100

    feat: added file viewer

--- solidstart-tanstackquery-tailwind-modules/src/components/FileViewer/FileCode.tsx
+++ solidstart-tanstackquery-tailwind-modules/src/components/FileViewer/FileCode.tsx
@@ -1,5 +1,9 @@
-import Highlight from 'solid-highlight';
 import 'highlight.js/styles/stackoverflow-light.css';
+import { unstable_clientOnly } from 'solid-start';
+
+// Highlight doesn't work with SSR so we need to use unstable_clientOnly
+// This causes storybook to not work with this component
+const Highlight = unstable_clientOnly(() => import('solid-highlight'));
 
 function FileCode(props: { text: string; language: string | undefined }) {
   return <Highlight language={props.language}>{props.text}</Highlight>;
