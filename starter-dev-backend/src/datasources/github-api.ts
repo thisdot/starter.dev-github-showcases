7935b4b0aa1edb228fb8481a73d59a8128e13634
William Hutt <whutt1994@gmail.com>
2023-02-13 16:30:41 -0800

    feat: adds orgs and starred repos to schema (#1031)
    Co-authored-by: Jamie Kuppens <jamie.kuppens@thisdot.co>

--- starter-dev-backend/src/datasources/github-api.ts
+++ starter-dev-backend/src/datasources/github-api.ts
@@ -3,6 +3,10 @@ import {
   WillSendRequestOptions,
 } from '@apollo/datasource-rest';
 
+// TODO: follow up PR will cover formatters and testing
+// https://github.com/thisdot/starter.dev-github-showcases/issues/1040
+// import { repoFormatter, orgFormatter, ownerFormatter } from 'src/formatters';
+
 export class GitHubAPI extends RESTDataSource {
   context: any;
 
@@ -19,20 +23,49 @@ export class GitHubAPI extends RESTDataSource {
     request.headers['authorization'] = this.context.token;
   }
 
+  async getOrgsMemberCount(login: string) {
+    return await this.get(`/orgs/${login}/members?per_page=100}`);
+  }
+
+  async getOrgRepos(login: string) {
+    return await this.get(`/orgs/${login}`);
+  }
+
+  // https://docs.github.com/en/rest/orgs/orgs?apiVersion=2022-11-28#list-organizations-for-a-user
+  async getOrgs(login: string) {
+    const data = await this.get(`/users/${login}/orgs`);
+    // https://github.com/thisdot/starter.dev-github-showcases/issues/1040
+    // return orgFormatter(data);
+    return data;
+  }
+
+  async getOwnerStarCount(login: string) {
+    return await this.get(`/users/${login}/starred`);
+  }
+
   // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#about-the-users-api
   async getOwner() {
-    return await this.get('/user');
+    const data = await this.get('/user');
+    // https://github.com/thisdot/starter.dev-github-showcases/issues/1040
+    // return ownerFormatter(data);
+    return data;
   }
 
   // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
   async getRepo(owner: string, repoName: string) {
-    return await this.get(`/repos/${owner}/${repoName}`);
+    const data = await this.get(`/repos/${owner}/${repoName}`);
+    // https://github.com/thisdot/starter.dev-github-showcases/issues/1040
+    // return repoFormatter(data);
+    return data;
   }
 
   // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
   async getRepos(username: string, perPage?: string) {
-    return await this
+    const data = await this
       .get(`/users/${username}/repos?sort=updated&per_page=${perPage}
     `);
+    // https://github.com/thisdot/starter.dev-github-showcases/issues/1040
+    // return repoFormatter(data);
+    return data;
   }
 }
--- starter-dev-backend/src/schema/github/github.resolvers.ts
+++ starter-dev-backend/src/schema/github/github.resolvers.ts
@@ -1,4 +1,28 @@
 export const GithubResolvers = {
+  Owner: {
+    orgs: async ({ login }: any, test: any, { dataSources }: any) => {
+      const data = await dataSources.githubAPI.getOrgs(login);
+      return data;
+    },
+    starred_url: async ({ login }: any, __: any, { dataSources }: any) => {
+      const data = await dataSources.githubAPI.getOwnerStarCount(login);
+      return data.length;
+    },
+  },
+  Orgs: {
+    repos_url: async ({ login }: any, __: any, { dataSources }: any) => {
+      const data = await dataSources.githubAPI.getOrgRepos(login);
+      return data.public_repos;
+    },
+    members_url: async ({ login }: any, __: any, { dataSources }: any) => {
+      const data = await dataSources.githubAPI.getOrgsMemberCount(login);
+      return data.length;
+    },
+    name: async ({ login }: any, __: any, { dataSources }: any) => {
+      const data = await dataSources.githubAPI.getOrgRepos(login);
+      return data.name;
+    },
+  },
   Query: {
     repos: async (_: any, { username, perPage }: any, { dataSources }: any) => {
       const data = dataSources.githubAPI.getRepos(username, perPage);
