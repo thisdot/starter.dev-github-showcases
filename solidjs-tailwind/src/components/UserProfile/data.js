297e7f910e6838b9cc5748d98a864658d1c032c7
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-02-16 18:57:20 +0100

    update user profile card to show missing data points

--- solidjs-tailwind/src/components/UserProfile/data.js
+++ solidjs-tailwind/src/components/UserProfile/data.js
@@ -2,11 +2,18 @@ export const userProfileProps = {
   avatarUrl: 'https://avatars.githubusercontent.com/u/2487968?v=4',
   bio: `Senior Software Engineer @thisdot`,
   company: '@thisdot',
-  followers: 24,
-  following: 20,
+  followers: {
+    totalCount: 20,
+  },
+  following: {
+    totalCount: 24,
+  },
   location: 'Washington, DC',
   login: 'tvanantwerp',
   name: 'Tom VanAntwerp',
+  starredRepositories: {
+    totalCount: 2,
+  },
   twitterUsername: 'tvanantwerp',
   websiteUrl: 'https://tomvanantwerp.com',
   organizations: {
