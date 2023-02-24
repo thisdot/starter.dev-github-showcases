06a2df177cbd9b8ba0856f1030c894d58098a5b3
Mattia Magi <mattia.magi91@gmail.com>
2023-02-24 15:09:18 +0100

    removed unused file and revert mockReadMe deletion

--- solidjs-tailwind/msw/mockRepoInfo.js
+++ solidjs-tailwind/msw/mockRepoInfo.js
@@ -1,6 +1,7 @@
 import { rest } from "msw";
+import { GITHUB_GRAPHQL } from "../src/utils/constants";
 import { repoInforResponse } from "./data";
 
-export const mockRepoInfo = rest.post("https://api.github.com/graphql", (req, res, ctx) => {
+export const mockRepoInfo = rest.post(GITHUB_GRAPHQL, (req, res, ctx) => {
   return res(ctx.status(200), ctx.json(repoInforResponse));
 });
--- solidjs-tailwind/msw/mockRepoTree.js
+++ solidjs-tailwind/msw/mockRepoTree.js
@@ -1,6 +1,7 @@
 import { rest } from "msw";
+import { GITHUB_GRAPHQL } from "../src/utils/constants";
 import { repoTreeResponse } from "./data";
 
-export const mockRepoTree = rest.post("https://api.github.com/graphql", (req, res, ctx) => {
+export const mockRepoTree = rest.post(GITHUB_GRAPHQL, (req, res, ctx) => {
   return res(ctx.status(200), ctx.json(repoTreeResponse))
 })
