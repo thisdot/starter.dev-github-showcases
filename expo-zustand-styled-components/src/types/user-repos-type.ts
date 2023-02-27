652a6c13e296797a255e0effce0a475005b27f92
Mattia Magi <mattia.magi91@gmail.com>
2023-02-27 10:35:46 +0100

    expo-feat: user repos api call

---
+++ expo-zustand-styled-components/src/types/user-repos-type.ts
@@ -0,0 +1,33 @@
+export interface Repo {
+  id: string;
+  name: string;
+  description: string;
+  stargazerCount: number;
+  forkCount: number;
+  isFork: boolean;
+  isArchived: boolean;
+  visibility: string;
+  updatedAt: string;
+  owner: {
+    login: string;
+  };
+  primaryLanguage: {
+    name: string;
+    color: string;
+  };
+}
+
+export interface PageInfo {
+  endCursor?: string;
+  startCursor?: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+export interface UserRepoInfo {
+  owner: {
+    repositories: {
+      nodes: Repo[];
+      pageInfo: PageInfo;
+    };
+  };
+}
