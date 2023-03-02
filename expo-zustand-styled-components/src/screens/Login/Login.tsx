4f019aec61f4ae088582c4d396b542570943cd3f
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-03-03 00:31:40 +0200

    fix: env vars

--- expo-zustand-styled-components/src/screens/Login/Login.tsx
+++ expo-zustand-styled-components/src/screens/Login/Login.tsx
@@ -3,12 +3,13 @@ import { AuthStackScreenProps } from '../../../types';
 import { SafeAreaViewStyled, LoginButtonStyled, LoginButtonTextStyled } from './Login.styles';
 import * as WebBrowser from 'expo-web-browser';
 import { useTokenStore } from '../../stores/token';
+import { SERVER_BASE_URL, REDIRECT_URI } from '@env';
 
 WebBrowser.maybeCompleteAuthSession();
 const Login = ({ navigation }: AuthStackScreenProps<'Login'>) => {
   const _handlePressButtonAsync = async () => {
     let result = await WebBrowser.openAuthSessionAsync(
-      'http://localhost:9999/.netlify/functions/signin?redirect_url=http://localhost:19006'
+      `${SERVER_BASE_URL}/.netlify/functions/signin?redirect_url=${REDIRECT_URI}`
     );
 
     if (result.type === 'success') {
