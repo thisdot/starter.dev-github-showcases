2efe288d341cd30c789a4672ba9da035dc01c681
Mattia Magi <mattia.magi91@gmail.com>
2023-03-03 18:32:11 +0100

    added default export

--- expo-zustand-styled-components/src/hooks/stores/index.ts
+++ expo-zustand-styled-components/src/hooks/stores/index.ts
@@ -2,3 +2,4 @@ export { default as useAuthStore } from './useAuthStore';
 export { default as useGistsStore } from './useGistsStore';
 export { default as useUserRepos } from './useUserRepos';
 export { default as useTopRepoStore } from './useTopRepoStore';
+export { default as useOrgReposStore } from './useOrgReposStore';
