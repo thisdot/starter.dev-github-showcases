6442198ee978c823db95d1543f421b4caade4d7d
Jerry Hogan <jerry.hogan@thisdot.co>
2023-03-07 13:35:48 +0100

    user profile card completed (#1551)

---
+++ expo-zustand-styled-components/src/components/UserProfileCard/OrgList.styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const OrganizationsWrapper = styled.View`
+  margin-top: 20px;
+  border-top-width: 1px;
+  border-style: solid;
+  border-top-color: rgb(31, 41, 55);
+`;
+export const Heading = styled.Text`
+  margin-top: 8px;
+  margin-bottom: 8px;
+  padding-top: 8px;
+  color: rgb(31, 41, 55);
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export const Organizations = styled.View`
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const Organisation = styled.View`
+  width: 36px;
+  height: 36px;
+  border-radius: 4px;
+  overflow: hidden;
+  border: 1px solid rgb(209, 213, 219);
+`;
+
+export const OrgImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  resizemode: cover;
+`;
---
+++ expo-zustand-styled-components/src/components/UserProfileCard/UserProfileCard.styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components/native';
+type ScreenWidth = {
+  screenWidth: number | undefined;
+};
+
+export const UserProfileCardWrapper = styled.View<ScreenWidth>`
+  max-width: ${({ screenWidth }) => (screenWidth >= 768 ? '33.3%' : '90%')};
+  display: flex;
+  flex-grow: 0;
+  padding-horizontal: 24px;
+`;
+
+export const UserDetails = styled.View<ScreenWidth>`
+  flex-direction: ${({ screenWidth }) => (screenWidth >= 768 ? 'column' : 'row')};
+  gap: 8px;
+  align-items: ${({ screenWidth }) => (screenWidth >= 768 ? 'flex-start' : 'center')};
+`;
+
+export const Avatar = styled.Image<ScreenWidth>`
+  border-radius: 260px;
+  width: ${({ screenWidth }) => (screenWidth >= 768 ? '260px' : '80px')};
+  height: ${({ screenWidth }) => (screenWidth >= 768 ? '260px' : '80px')};
+`;
+
+export const NameContainer = styled.View`
+  margin-top: 8px;
+`;
+
+export const Name = styled.Text<ScreenWidth>`
+  font-size: ${({ screenWidth }) => (screenWidth >= 768 ? '24px' : '16px')};
+  color: rgb(31, 41, 55);
+  font-weight: 700;
+`;
+
+export const Username = styled.Text<ScreenWidth>`
+  font-size: ${({ screenWidth }) => (screenWidth >= 768 ? '20px' : '14px')};
+  color: rgb(107, 114, 128);
+  font-weight: 300;
+`;
+
+export const Bio = styled.Text`
+  color: rgb(31, 41, 55);
+  margin-top: 16px;
+`;
+
+export const FieldWrapper = styled.View`
+  margin-bottom: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Field = styled.Text`
+  font-size: 14px;
+  line-height: 20px;
+  color: rgb(31, 41, 55);
+  margin-left: 8px;
+`;
+
+export const SpanCount = styled.Text`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Count = styled.Text`
+  font-weight: 500;
+  color: rgb(17, 24, 39);
+`;
+
+export const SpaceSpan = styled.Text`
+  margin-left: 4px;
+  margin-right: 4px;
+`;
+
+export const Socials = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
