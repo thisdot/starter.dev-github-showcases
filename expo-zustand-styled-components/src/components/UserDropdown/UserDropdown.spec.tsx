85178ab0828af300514b2cc3e7c3d26b51a46059
Mattia Magi <mattia.magi91@gmail.com>
2023-03-08 09:25:18 +0100

    fix test

--- expo-zustand-styled-components/src/components/UserDropdown/UserDropdown.spec.tsx
+++ expo-zustand-styled-components/src/components/UserDropdown/UserDropdown.spec.tsx
@@ -2,18 +2,13 @@ import UserDropdown from './UserDropdown';
 import { fireEvent, render } from '@testing-library/react-native';
 
 describe('User dropdown', () => {
-  const data = {
-    username: 'hdjerry',
-    image: 'https://avatars.githubusercontent.com/u/22839396?v=4',
-  };
-
   it('should mount', () => {
-    const wrapper = render(<UserDropdown {...data} />);
+    const wrapper = render(<UserDropdown />);
     expect(wrapper).toBeTruthy();
   });
 
   it('should show dropdown options', async () => {
-    const wrapper = render(<UserDropdown {...data} />);
+    const wrapper = render(<UserDropdown />);
     fireEvent.press(wrapper.getByTestId('profile-image'));
     const option = wrapper.getByText('Profile');
     expect(option).toBeDefined();
