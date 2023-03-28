ae0a46243117b1ab933fdcbd4e7d49d318093c15
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-03-28 16:30:32 +0100

    fix bug in test

--- expo-zustand-styled-components/src/components/UserDropdown/UserDropdown.spec.tsx
+++ expo-zustand-styled-components/src/components/UserDropdown/UserDropdown.spec.tsx
@@ -3,12 +3,12 @@ import { fireEvent, render } from '@testing-library/react-native';
 
 describe('User dropdown', () => {
   it('should mount', () => {
-    const wrapper = render(<UserDropdown />);
+    const wrapper = render(<UserDropdown width={200} />);
     expect(wrapper).toBeTruthy();
   });
 
   it('should show dropdown options', async () => {
-    const wrapper = render(<UserDropdown />);
+    const wrapper = render(<UserDropdown width={200}/>);
     fireEvent.press(wrapper.getByTestId('profile-image'));
     const option = wrapper.getByText('Profile');
     expect(option).toBeDefined();
