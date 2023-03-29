f948e01c020abf9a61995b96e30bc0608d6c97e0
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2023-03-29 11:56:37 +0100

    Update Markdown Package with workable one (#1700)
    * Update Markdown Package with workable one
    
    * change package and fix webpack
    
    * fix
    
    * disable eslint

--- expo-zustand-styled-components/src/hooks/useCachedResources.ts
+++ expo-zustand-styled-components/src/hooks/useCachedResources.ts
@@ -15,7 +15,7 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
-          // 'roboto-300': require('../fonts/Roboto-Regular.ttf'),
+          'Courier': require('../../assets/fonts/courier.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
