db748b6fc664049ccf5cb9c524c4368badc27c1f
kodejuice <kodejuice@gmail.com>
2023-03-31 15:07:13 +0100

    add test

---
+++ expo-zustand-styled-components/src/components/BranchNavigation/BranchNavigation.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import BranchNavigation from './BranchNavigation';
+import { useRepoInfoStore } from '../../hooks/stores';
+
+jest.mock('../../hooks/stores', () => ({
+  useRepoInfoStore: jest.fn(),
+}));
+
+describe('BranchNavigation', () => {
+  beforeEach(() => {
+    (useRepoInfoStore as jest.Mock).mockReturnValue({
+      name: 'test-repo',
+      owner: 'test-owner',
+      branch: 'main',
+    });
+  });
+
+  test('renders branch navigation without path', () => {
+    render(<BranchNavigation />);
+    expect(screen.getByText('main')).toBeTruthy();
+  });
+
+  test('renders branch navigation with path', () => {
+    render(<BranchNavigation path="src/components" />);
+    expect(screen.getByText('main')).toBeTruthy();
+    expect(screen.getByText('src')).toBeTruthy();
+    expect(screen.getByText('components')).toBeTruthy();
+  });
+
+  test('renders branch navigation with custom branch', () => {
+    render(<BranchNavigation path="src/components" branch="custom-branch" />);
+    expect(screen.getByText('custom-branch')).toBeTruthy();
+    expect(screen.getByText('src')).toBeTruthy();
+    expect(screen.getByText('components')).toBeTruthy();
+  });
+});
