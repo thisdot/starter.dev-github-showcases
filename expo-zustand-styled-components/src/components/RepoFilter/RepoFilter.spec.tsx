027230153841515a5d21da140a26c3240c8dfb1e
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-04-04 05:45:47 +0100

    repo filter test

--- expo-zustand-styled-components/src/components/RepoFilter/RepoFilter.spec.tsx
+++ expo-zustand-styled-components/src/components/RepoFilter/RepoFilter.spec.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 import RepoFilter from './RepoFilter';
 
 import { useRepoFilterStore } from '../../hooks/stores';
-// import { useWindowDimensions } from 'react-native';
 
 jest.mock('../../hooks/stores', () => ({
   useRepoFilterStore: jest.fn(),
 }));
-
-// jest.mock('react', () => ({
-//     ...jest.requireActual('react'),
-//     useState: jest.fn(),
-// }))
-
-// jest.mock('react-native/Libraries/Utilities/useWindowDimensions', () => (
-//    {
-//     default: jest.fn(() => ({ width: 300 }))
-//   }
-// ))
 
 describe('RepoFilter', () => {
   const mockedStore = useRepoFilterStore as jest.MockedFunction<typeof useRepoFilterStore>;
@@ -67,34 +55,6 @@ describe('RepoFilter', () => {
     expect(screen.getByText('Langauge')).toBeTruthy();
   });
 
-  // it('should show options when a filter is clicked', () => {
-  //  const x = render(
-  //     <RepoFilter
-  //       languages={languages}
-  //       filteredRepoCount={filteredRepoCount}
-  //       repoBtnText={repoBtnText}
-  //     />
-  //   );
-
-  //   x.debug();
-  //   expect(screen.getByText('JavaScript')).toBeTruthy();
-  //   fireEvent.press(screen.getByText('Langauge'));
-  //   expect(screen.getByText('JavaScript')).toBeTruthy();
-  // });
-
-  //   it('should call the selectLanguage function when a language is selected', () => {
-  //     const { getByText } = render(
-  //       <RepoFilter
-  //         languages={languages}
-  //         filteredRepoCount={filteredRepoCount}
-  //         repoBtnText={repoBtnText}
-  //       />
-  //     );
-  //     fireEvent.press(getByText('Langauge'));
-  //     fireEvent.press(getByText('Python'));
-  //     expect(mockedStore().setLanguage).toHaveBeenCalledWith('Python');
-  //   });
-
   it('should render the repo button with the given text', () => {
     render(
       <RepoFilter
@@ -105,20 +65,4 @@ describe('RepoFilter', () => {
     );
     expect(screen.getByText('New')).toBeTruthy();
   });
-
-  //   it('should render the filtered repo count if some filter is selected', () => {
-  //     const { rerender } = render(
-  //       <RepoFilter
-  //         languages={languages}
-  //         filteredRepoCount={filteredRepoCount}
-  //         repoBtnText={repoBtnText}
-  //       />
-  //     );
-  //     expect(screen.queryByText('10 repositories found')).toBeNull();
-  //     mockedStore.mockReturnValue({ search: 'query' });
-  //     rerender(
-  //       <RepoFilter languages={languages} filteredRepoCount={filteredRepoCount} />
-  //     );
-  //     expect(screen.getByText('10 repositories found')).toBeTruthy();
-  //   });
 });
