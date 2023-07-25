78e79aa39b7aa4b30339855cd69cfa3bfc3f853d
Jerry Hogan <jerry.hogan@thisdot.co>
2023-07-25 14:01:13 +0100

    Feat profile api (#1857)
    * profile api
    
    * profile api
    
    * fix comments
    
    * ...
    
    * ... types
    
    * format
    
    * format

---
+++ nuxt3-pinia-vuetify/hooks/useFetchApI.ts
@@ -0,0 +1,16 @@
+import { UseFetchOptions } from 'nuxt/app';
+
+export default function useFetchAPI<T>(
+	url: string,
+	opts?: UseFetchOptions<Record<string, any>>
+): Record<string, any> | { data: T } {
+	const config = useRuntimeConfig();
+	const { GITHUB_API_URL } = config;
+	const token = useCookie('token');
+	const headers = {
+		...(opts?.headers || {}),
+		...(token && { Authorization: `Bearer ${token.value}` }),
+	};
+
+	return useFetch(url, { ...opts, headers, baseURL: GITHUB_API_URL });
+}
---
+++ nuxt3-pinia-vuetify/store/profileStore.ts
@@ -0,0 +1,66 @@
+import useFetchAPI from '~~/hooks/useFetchApI';
+import { IUser, IUserOrg } from '~~/types/users/interface';
+
+export interface IProfileRootState {
+	user: IUser | Record<string, any> | null;
+	login: string | null;
+	avatar_url: string | null;
+}
+
+export const useProfileStore = defineStore('profileStore', {
+	state: (): IProfileRootState => ({
+		user: null,
+		login: null,
+		avatar_url: null,
+	}),
+	actions: {
+		async getProfile() {
+			try {
+				const url = `/users/${this.login}`;
+				const { data } = await useFetchAPI<IUser>(url, {
+					headers: {
+						Accept: 'application/vnd.github+json',
+					},
+				});
+
+				const resp = data.value;
+
+				this.user = resp;
+				const companyURL = resp.organizations_url;
+
+				const { data: companyData } =
+					await useFetchAPI<IUserOrg>(companyURL, {
+						headers: {
+							Accept: 'application/vnd.github+json',
+						},
+					});
+
+				const orgs = companyData.value;
+
+				this.user = {
+					...this.user,
+					orgs,
+				};
+			} catch (error) {
+				throw new Error('Error fetching user profile');
+			}
+		},
+		async getAuthUser() {
+			try {
+				const url = `/user`;
+				const { data } = await useFetchAPI<IUser>(url, {
+					headers: {
+						Accept: 'application/vnd.github+json',
+					},
+				});
+
+				const user = data.value;
+				this.login = user.login;
+				this.avatar_url = user.avatar_url;
+				this.getProfile();
+			} catch (error) {
+				throw new Error('Error fetching authenticated user');
+			}
+		},
+	},
+});
