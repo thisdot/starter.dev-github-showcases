6343f460364a8863033a89211bfebfbb1af38598
Jerry Hogan <jerry.hogan@thisdot.co>
2023-07-26 16:15:27 +0100

    fixing duplicate lines

--- nuxt3-pinia-vuetify/types/repository/interface.ts
+++ nuxt3-pinia-vuetify/types/repository/interface.ts
@@ -1,29 +1,6 @@
 import { IUser } from '../users/interface';
 import { StateEnum, SortEnum, DirectionEnum } from './enums';
 
-export interface IRepository {
-	id: number;
-	name: string;
-	full_name: string;
-	owner: { login: string };
-	description: string;
-	private: boolean;
-	html_url: string;
-	url: string;
-	updated_at: Date;
-	stargazers_count: number;
-	language: string;
-	branches_url: string;
-	visibility: 'public' | 'private';
-	subscribers_count: number;
-	forks_count: number;
-	open_issues_count: number;
-	pulls: number;
-	default_branch: string;
-	homepage: string;
-	watchers_count: number;
-}
-
 export interface IPullRequest {
 	title: string;
 	number: string;
