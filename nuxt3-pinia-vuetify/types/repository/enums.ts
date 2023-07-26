5b935c93cb7529423084e79608598705802b5dd8
Jerry Hogan <jerry.hogan@thisdot.co>
2023-07-26 15:42:10 +0100

    single repo info

---
+++ nuxt3-pinia-vuetify/types/repository/enums.ts
@@ -0,0 +1,26 @@
+export enum StateEnum {
+	OPEN = 'open',
+	CLOSED = 'closed',
+	MERGED = 'merged',
+}
+
+export enum IssueStateEnum {
+	OPEN = 'open',
+	CLOSED = 'closed',
+}
+
+export enum SortEnum {
+	CREATED = 'created',
+	UPDATED = 'updated',
+	COMMENTS = 'comments',
+}
+
+export enum DirectionEnum {
+	ASC = 'asc',
+	DESC = 'desc',
+}
+
+export enum IssueTypeEnum {
+	ISSUE = 'issue',
+	PULL_REQUEST = 'pr',
+}
