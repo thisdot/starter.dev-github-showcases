3fd4e1185e39c2abac2428e7db2fae5095cc4b9d
Jerry Hogan <jerry.hogan@thisdot.co>
2023-08-03 17:21:21 +0100

    chore: fix broken story, removed unnecessary story

--- cra-rxjs-styled-components/src/components/header/Header.stories.tsx
+++ cra-rxjs-styled-components/src/components/header/Header.stories.tsx
@@ -1,51 +1,50 @@
-import { UserContext, IUserContext } from '../../context/UserProvider';
+import { UserContext, UserProvider } from '../../context/UserProvider';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Routes, Route, MemoryRouter } from 'react-router-dom';
 import Header from './Header';
-import { useParameter } from '@storybook/addons';
+import { Observable } from 'rxjs';
 
-// const HeaderDecorator = (storyFn: () => React.ReactNode) => {
-// 	const user = useParameter<IUserContext>('user');
-// 	return <UserContext.Provider value={user}>{storyFn()}</UserContext.Provider>;
-// };
+const user: any = {
+	login: 'johndoe',
+	avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+	organizations: [
+		{
+			login: 'org1',
+			avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+		},
+		{
+			login: 'org2',
+			avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+		},
+	],
+	starredRepos: 5,
+};
+
+type ILoadUser = () => Observable<[any, any, any]>;
+
+const loadUser: ILoadUser = () => user;
+const loading: boolean = false;
 
 export default {
 	title: 'Navbar/Header',
 	component: Header,
+	decorators: [
+		(Story) => (
+			<UserProvider>
+				<UserContext.Provider value={{ user, loading, loadUser }}>
+					<Story />
+				</UserContext.Provider>
+			</UserProvider>
+		),
+	],
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = (args) => (
+const Template: ComponentStory<typeof Header> = () => (
 	<MemoryRouter>
 		<Routes>
-			<Route
-				path="/"
-				element={
-					<UserContext.Provider value={args.value}>
-						<Header />
-					</UserContext.Provider>
-				}
-			></Route>
+			<Route path="/" element={<Header />}></Route>
 		</Routes>
 	</MemoryRouter>
 );
 
 export const Default = Template.bind({});
-
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-	user: {
-		login: 'johndoe',
-		avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
-		organizations: [
-			{
-				login: 'org1',
-				avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
-			},
-			{
-				login: 'org2',
-				avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
-			},
-		],
-		starredRepos: 5,
-	},
-};
