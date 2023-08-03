ed8bd24b9f8097ea7516a1dff809ff1e24c12960
Sochima Biereagu <kodejuice@gmail.com>
2023-08-03 16:17:38 +0100

    [CRA-RXJS-SC]: Add story for Issue (#617)
    * Add Issues story
    
    * open / closed issues
    
    * chore: format and fix broken app
    
    * chore: fix format
    
    ---------
    
    Co-authored-by: Jerry Hogan <jerry.hogan@thisdot.co>

---
+++ cra-rxjs-styled-components/src/components/issue/Issue.stories.tsx
@@ -0,0 +1,94 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Routes, Route, MemoryRouter } from 'react-router-dom';
+import Issue from './Issue/Issue.view';
+
+export default {
+	title: 'Repo/Issue',
+	component: Issue,
+} as ComponentMeta<typeof Issue>;
+
+const Template: ComponentStory<typeof Issue> = (args) => (
+	<MemoryRouter>
+		<Routes>
+			<Route path="/" element={<Issue {...args} />}></Route>
+		</Routes>
+	</MemoryRouter>
+);
+
+const openDate = new Date().toISOString();
+
+export const OpenIssues = Template.bind({});
+OpenIssues.args = {
+	issues: [
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'open',
+			messageCount: 8,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'open',
+			messageCount: 8,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'open',
+			messageCount: 0,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'open',
+			messageCount: 2,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'open',
+			messageCount: 3,
+		},
+	],
+};
+
+export const ClosedIssues = Template.bind({});
+ClosedIssues.args = {
+	issues: [
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'closed',
+			messageCount: 8,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'closed',
+			messageCount: 8,
+		},
+		{
+			title: 'Remix/Feature/125 individual repo page',
+			openedNum: '#134 opened',
+			openedDay: openDate,
+			openedBy: 'by vyktoremario',
+			status: 'closed',
+			messageCount: 8,
+		},
+	],
+};
