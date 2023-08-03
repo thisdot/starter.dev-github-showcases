53ea74c67964c4b41ddb08866090a94faa703d20
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2023-08-03 19:36:45 +0300

    feat: Add story for Header (#581)
    * feat: Add story for Header
    
    * chore: fix broken story, removed unnecessary story
    
    * chore: revert some changes
    
    * chore: remove unused files
    
    ---------
    
    Co-authored-by: Jerry Hogan <jerry.hogan@thisdot.co>

--- cra-rxjs-styled-components/src/components/header/Header.stories.tsx
+++ cra-rxjs-styled-components/src/components/header/Header.stories.tsx
@@ -1,13 +1,45 @@
+import { UserContext, UserProvider } from '../../context/UserProvider';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Routes, Route, MemoryRouter } from 'react-router-dom';
 import Header from './Header';
+import { Observable } from 'rxjs';
+
+const user: any = {
+	login: 'johndoe',
+	avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+	organizations: [
+		{
+			login: 'org1',
+			avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+		},
+		{
+			login: 'org2',
+			avatar_url: 'https://avatars.githubusercontent.com/u/1234567?v=4',
+		},
+	],
+	starredRepos: 5,
+};
+
+type ILoadUser = () => Observable<[any, any, any]>;
+
+const loadUser: ILoadUser = () => user;
+const loading: boolean = false;
 
 export default {
 	title: 'Navbar/Header',
 	component: Header,
+	decorators: [
+		(Story) => (
+			<UserProvider>
+				<UserContext.Provider value={{ user, loading, loadUser }}>
+					<Story />
+				</UserContext.Provider>
+			</UserProvider>
+		),
+	],
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = (args) => (
+const Template: ComponentStory<typeof Header> = () => (
 	<MemoryRouter>
 		<Routes>
 			<Route path="/" element={<Header />}></Route>
@@ -15,5 +47,4 @@ const Template: ComponentStory<typeof Header> = (args) => (
 	</MemoryRouter>
 );
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {};
+export const Default = Template.bind({});
