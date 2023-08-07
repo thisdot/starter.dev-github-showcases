6215e9f8571d847a08679238b54da0f583839913
Mattia Magi <mattia.magi91@gmail.com>
2023-08-07 16:00:29 +0200

    feat: added netlify badge

--- qwik-graphql-tailwind/src/routes/auth/signin/index.tsx
+++ qwik-graphql-tailwind/src/routes/auth/signin/index.tsx
@@ -4,7 +4,7 @@ import { SIGN_IN_URL } from '~/utils/constants';
 
 export default component$(() => {
   return (
-    <section class="flex h-screen w-full items-center justify-center bg-black">
+    <section class="flex h-screen w-full flex-col items-center justify-center bg-black">
       <button
         class="rounded-md border-2 border-zinc-700 px-20 py-2 text-zinc-100 transition-all hover:border-zinc-100 hover:bg-zinc-100 hover:text-zinc-900"
         onClick$={() => {
@@ -13,6 +13,11 @@ export default component$(() => {
       >
         Sign In with GitHub
       </button>
+      <div class="absolute bottom-0 flex justify-center pb-5 pt-6">
+        <a target="_blank" rel="noreferrer noopener" href="https://www.netlify.com">
+          <img src="https://www.netlify.com/v3/img/components/netlify-light.svg" alt="Deploys by Netlify" />
+        </a>
+      </div>
     </section>
   );
 });
