07665a26cef2739e83225de4b3096d64ec508a43
Jerry Hogan <jerry.hogan@thisdot.co>
2023-08-09 17:24:42 +0100

    chore: fix top repos api (#1901)
    * chore: fix top repos api
    
    * chore: modified repo card
    
    * chore: fixing failing build

--- cra-rxjs-styled-components/src/routes/user-top-repos.tsx
+++ cra-rxjs-styled-components/src/routes/user-top-repos.tsx
@@ -65,7 +65,8 @@ const NetlifyBadgeContainer = styled.div`
 export default function TopRepos() {
 	const context = useUser();
 	const user = context?.user;
-	const { repositories } = useRepos(user?.login);
+	const isTopRepos: boolean = true;
+	const { repositories } = useRepos(user?.login, isTopRepos);
 	const { gists, loadingGist } = useGists();
 	const topRepositories = [...repositories]
 		.sort((a, b) => b.stargazers_count - a.stargazers_count)
@@ -92,7 +93,7 @@ export default function TopRepos() {
 							) : (
 								<>
 									{topRepositories.map((repo) => (
-										<RepoCard repo={repo} key={repo.id} />
+										<RepoCard repo={repo} key={repo.id} isMainPage />
 									))}
 									<ViewRepositoriesContainer>
 										<ViewRepositoriesLink href={`/${user?.login}`}>
