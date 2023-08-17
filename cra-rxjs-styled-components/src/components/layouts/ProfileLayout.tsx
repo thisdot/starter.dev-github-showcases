66f801b2f6a5f5b12826039f3ef11d22474e4e03
Jerry Hogan <jerry.hogan@thisdot.co>
2023-08-17 12:48:54 +0100

    chore: fix scroll (#1927)
    * chore: fix scroll
    
    * fix: errors in profile styles file
    
    ---------
    
    Co-authored-by: Mattia Magi <mattia.magi91@gmail.com>

--- cra-rxjs-styled-components/src/components/layouts/ProfileLayout.tsx
+++ cra-rxjs-styled-components/src/components/layouts/ProfileLayout.tsx
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
-  height: calc(100% - 70px);
+	height: calc(100% - 70px);
 	padding: 0 1rem;
-  background-color: rgb(255, 255, 255);
-  display: grid;
+	background-color: rgb(255, 255, 255);
+	display: grid;
 	overflow: hidden;
 
-  grid-template-columns: auto;
-    grid-template-areas:
-      'header'
-      'aside'
-      'content'
-	  'footer';
-  }
+	grid-template-columns: auto;
+	grid-template-areas:
+		'header'
+		'aside'
+		'content'
+		'footer';
 
-  @media (min-width: 850px) {
-    grid-template-columns: 384px auto;
-    grid-template-areas: 'aside content' 'footer footer';
-    margin: 20px;
-	height: calc(100% - 110px);
+	@media (min-width: 850px) {
+		grid-template-columns: 384px auto;
+		grid-template-areas: 'aside content' 'footer footer';
+		margin: 20px;
+		height: calc(100% - 110px);
+	}
 `;
 
 export const NetlifyBadgeContainer = styled.div`
