7a4675ecac23aaf7b665300432f1c9963ec9f992
Jerry Hogan <jerry.hogan@thisdot.co>
2023-08-25 12:14:46 +0100

    chore: vite apollo vanilla showcase not supported

--- vite-apollo-vanilla/jest.setup.js
+++
@@ -1,3 +0,0 @@
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
-require("@testing-library/jest-dom");
--- vite-apollo-vanilla/src/lib/css/breakpoints.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  mobile: {},
-  tablet: { '@media': '(min-width: 768px)' },
-  desktop: { '@media': '(min-width: 1024px)' },
-};
